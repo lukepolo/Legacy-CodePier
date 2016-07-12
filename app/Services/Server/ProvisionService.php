65447ade2c1443d881f09659977e56c190bde3eb
Luke Policinski <luke@lukepolo.com>
2016-07-12 23:05:59 +0000

    fixed bad code

--- app/Services/Server/ProvisionService.php
+++ app/Services/Server/ProvisionService.php
@@ -64,69 +64,68 @@ class ProvisionService implements ProvisionServiceContract
 
         $this->totalActions = count(get_class_methods($provisionSystem)) - 1;
 
-        dd(get_class_methods($provisionSystem));
         $provisionSystem->updateSystem();
-        event(new UpdatedSystem($server), $this->getDonePercentage());
+        event(new UpdatedSystem($server, $this->getDonePercentage()));
 
         $provisionSystem->setTimezoneToUTC();
-        event(new TimeZoneSetToUCT($server), $this->getDonePercentage());
+        event(new TimeZoneSetToUCT($server, $this->getDonePercentage()));
 
         $provisionSystem->addCodePierUser($sudoPassword);
-        event(new AddedCodePierUser($server), $this->getDonePercentage());
+        event(new AddedCodePierUser($server, $this->getDonePercentage()));
 
         $provisionSystem->setLocaleToUTF8();
-        event(new LocaleSetToUTF8($server), $this->getDonePercentage());
+        event(new LocaleSetToUTF8($server, $this->getDonePercentage()));
 
         $provisionSystem->createSwap();
-        event(new SwapCreated($server), $this->getDonePercentage());
+        event(new SwapCreated($server, $this->getDonePercentage()));
 
         $provisionSystem->installGit();
-        event(new GitInstalled($server), $this->getDonePercentage());
+        event(new GitInstalled($server, $this->getDonePercentage()));
 
         $provisionSystem->installPHP();
-        event(new PHPInstalled($server), $this->getDonePercentage());
+        event(new PHPInstalled($server, $this->getDonePercentage()));
 
         $provisionSystem->installPhpFpm();
-        event(new PHPFpmInstalled($server), $this->getDonePercentage());
+        event(new PHPFpmInstalled($server, $this->getDonePercentage()));
 
         $provisionSystem->installComposer();
-        event(new ComposerInstalled($server), $this->getDonePercentage());
+        event(new ComposerInstalled($server, $this->getDonePercentage()));
 
         $provisionSystem->installNginx();
-        event(new NginxInstalled($server), $this->getDonePercentage());
+        event(new NginxInstalled($server, $this->getDonePercentage()));
 
         $provisionSystem->installRedis();
-        event(new RedisInstalled($server), $this->getDonePercentage());
+        event(new RedisInstalled($server, $this->getDonePercentage()));
 
         $provisionSystem->installMemcached();
-        event(new MemcachedInstalled($server), $this->getDonePercentage());
+        event(new MemcachedInstalled($server, $this->getDonePercentage()));
 
         $provisionSystem->installSupervisor();
-        event(new SupervisorInstalled($server), $this->getDonePercentage());
+        event(new SupervisorInstalled($server, $this->getDonePercentage()));
 
         $provisionSystem->installBeanstalk();
-        event(new BeanstalkInstalled($server), $this->getDonePercentage());
+        event(new BeanstalkInstalled($server, $this->getDonePercentage()));
 
         $provisionSystem->installMySQL($databasePassword);
-        event(new MySQLInstalled($server), $this->getDonePercentage());
+        event(new MySQLInstalled($server, $this->getDonePercentage()));
 
         $provisionSystem->installMariaDB($databasePassword);
-        event(new MariaDBInstalled($server), $this->getDonePercentage());
+        event(new MariaDBInstalled($server, $this->getDonePercentage()));
 
         $provisionSystem->installNodeJs();
-        event(new NodeJsInstalled($server), $this->getDonePercentage());
+        event(new NodeJsInstalled($server, $this->getDonePercentage()));
 
         $provisionSystem->installGulp();
-        event(new GulpInstalled($server), $this->getDonePercentage());
+        event(new GulpInstalled($server, $this->getDonePercentage()));
 
         $provisionSystem->installBower();
-        event(new BowerInstalled($server), $this->getDonePercentage());
+        event(new BowerInstalled($server, $this->getDonePercentage()));
 
         $provisionSystem->installCertBot();
-        event(new CertBotInstalled($server), $this->getDonePercentage());
+        event(new CertBotInstalled($server, $this->getDonePercentage()));
 
         $provisionSystem->installFirewallRules();
-        event(new FirewallSetup($server), $this->getDonePercentage());
+        event(new FirewallSetup($server, $this->getDonePercentage()));
 
         // TODO - having issues with the laravel installer and envoy installer
 //        $provisionSystem->installLaravelInstaller();
