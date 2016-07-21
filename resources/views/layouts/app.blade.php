98bc04d07ebfb32c75e84a4b566c9fdd742d8cb3
Luke Policinski <luke@lukepolo.com>
2016-07-21 16:24:56 -0400

    google notofications are setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,24 @@
         @endif
 
         <script type="text/javascript">
+
+            if (Notification) {
+                if (Notification.permission !== "granted") {
+                    Notification.requestPermission();
+                }
+
+//                // https://developer.mozilla.org/en-US/docs/Web/API/notification
+//                var notification = new Notification('Server Provisioned', {
+//                    icon: 'https://www.dropbox.com/s/vq20qxb343qkqov/pasted_image_at_2016_07_21_04_03_pm.png?dl=0',
+//                    body : 'Get going!'
+//                });
+//
+//                notification.onclick = function(event) {
+//                    event.preventDefault(); // prevent the browser from focusing the Notification's tab
+//                    window.open('http://codepier.app/server/3', '_blank');
+//                };
+            }
+
             var vue;
 
             moment.tz.setDefault("UTC");
