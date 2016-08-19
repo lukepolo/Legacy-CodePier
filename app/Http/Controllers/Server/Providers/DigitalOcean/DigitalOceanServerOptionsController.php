7b3a546d324ac9c9132866eec34830358b3bedb8
Luke Policinski <Luke@LukePOLO.com>
2016-08-19 14:18:00 -0400

    WIP, starting up the sites

--- app/Http/Controllers/Server/Providers/DigitalOcean/DigitalOceanServerOptionsController.php
+++ app/Http/Controllers/Server/Providers/DigitalOcean/DigitalOceanServerOptionsController.php
@@ -34,8 +34,6 @@ class DigitalOceanServerOptionsController extends Controller
      */
     public function index(Request $request)
     {
-        return response()->json($this->serverService->getServerOptions(ServerProvider::with('serverRegions')->where('provider_name', OauthController::DIGITAL_OCEAN)->firstOrFail()));
-
         return response()->json(ServerProvider::with('serverRegions')->where('provider_name', OauthController::DIGITAL_OCEAN)->firstOrFail()->serverOptions);
     }
 
