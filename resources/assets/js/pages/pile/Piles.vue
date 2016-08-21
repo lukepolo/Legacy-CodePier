6f8c6e94295d6498061321a99f8500ae64cd1e2b
Luke Policinski <Luke@LukePOLO.com>
2016-08-21 00:40:36 -0400

    piles are consisnent now

--- resources/assets/js/pages/pile/Piles.vue
+++ resources/assets/js/pages/pile/Piles.vue
@@ -1,39 +1,43 @@
 <template>
-    <section id="middle" class="section-column">
-        <h3 class="section-header primary">My Piles</h3>
+    <section>
+        <left-nav></left-nav>
+        <section id="middle" class="section-column">
+            <h3 class="section-header primary">My Piles</h3>
+            <div class="section-content">
+                <div class="container">
+                    <h1>Welcome. Let's Get Started.</h1>
+                    <p class="info">Piles are groupings for your servers. We've built defaults for you, but you can edit them to fit your needs.</p>
 
-        <div class="section-content">
-            <div class="container">
-                <h1>Welcome. Let's Get Started.</h1>
-                <p class="info">Piles are groupings for your servers. We've built defaults for you, but you can edit them to fit your needs.</p>
+                    <div class="group-container">
 
-                <div class="group-container">
+                        <pile :pile="pile" :index="index" v-for="(pile, index) in piles"></pile>
 
-                    <pile :pile="pile" :index="index" v-for="(pile, index) in piles"></pile>
+                        <div class="group">
+                            <a v-on:click="newPile()" class="add-pile">
+                                <div class="group-content">
+                                    <span class="icon-layers"></span>
+                                </div>
 
-                    <div class="group">
-                        <a v-on:click="newPile()" class="add-pile">
-                            <div class="group-content">
-                                <span class="icon-layers"></span>
-                            </div>
-
-                            <div class="btn-footer text-center">
-                                <button class="btn btn-primary">Add Pile</button>
-                            </div>
-                        </a>
-                    </div>
-                </div><!-- end group-container -->
+                                <div class="btn-footer text-center">
+                                    <button class="btn btn-primary">Add Pile</button>
+                                </div>
+                            </a>
+                        </div>
+                    </div><!-- end group-container -->
+                </div>
             </div>
-        </div>
+        </section>
     </section>
 </template>
 
 <script>
     import Pile from './components/Pile.vue';
+    import LeftNav from './../../core/LeftNav.vue';
 
     export default {
-        components : {
+        components: {
             Pile,
+            LeftNav
         },
         computed : {
             piles: () => {
