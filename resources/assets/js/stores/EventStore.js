55a1b9a84f9243c53526c7c6c13f3086ecb703a2
Luke Policinski <Luke@LukePOLO.com>
2016-08-22 19:14:27 -0400

    WIP, notifications area has endless pagination

---
+++ resources/assets/js/stores/EventStore.js
@@ -0,0 +1,32 @@
+import Vue from "vue/dist/vue";
+import Vuex from "vuex";
+import {action} from "./helpers";
+
+Vue.use(Vuex);
+
+const eventStore = new Vuex.Store({
+    state: {
+        events: [],
+        events_pagination : null
+    },
+    actions: {
+        getEvents: ({commit}, page) => {
+            Vue.http.get(action('EventController@index', { page : page ? page : 1 })).then((response) => {
+                commit('SET_EVENTS', response.json());
+            }, (errors) => {
+                alert('handle some error')
+            });
+        }
+    },
+    mutations: {
+        SET_EVENTS: (state, events_pagination) => {
+            _.forEach(events_pagination.data, function(event) {
+                state.events.push(event);
+            });
+
+            state.events_pagination = events_pagination;
+        }
+    }
+});
+
+export default eventStore
