c9fe89aaf6dbfa95a6b2e3b6e4bbbdbd85319bc0
Luke Policinski <luke@lukepolo.com>
2016-09-07 22:08:19 +0000

    fixing up config

--- config/database.php
+++ config/database.php
@@ -45,6 +45,13 @@ return [
     */
 
     'connections' => [
+
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+        ],
+
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
@@ -57,6 +64,19 @@ return [
             'prefix'    => '',
             'strict'    => true,
             'engine'    => null,
+        ],
+
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'prefer',
         ],
     ],
 
