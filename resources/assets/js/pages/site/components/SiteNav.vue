5a26746eb7bd598e4093554e0d554bec84bbc4b4
Luke Policinski <luke@lukepolo.com>
2016-09-12 13:49:39 +0000

    removing forever, can just use supervisor

--- resources/assets/js/pages/site/components/SiteNav.vue
+++ resources/assets/js/pages/site/components/SiteNav.vue
@@ -5,7 +5,6 @@
             <router-link :to="{ path : '/site/' + site.id + '/framework-files'}" tag="li" class="btn btn-primary">Framework Files</router-link>
             <router-link :to="{ path : '/site/' + site.id + '/workers'}" tag="li" class="btn btn-primary">Workers</router-link>
             <router-link :to="{ path : '/site/' + site.id + '/ssl-certificates'}" tag="li" class="btn btn-primary">SSLCertificates</router-link>
-            <!--<router-link :to="{ path : '/site/' + site.id + '/files'}" tag="li" class="btn btn-primary">Files</router-link>-->
         </ul>
     </section>
 </template>
