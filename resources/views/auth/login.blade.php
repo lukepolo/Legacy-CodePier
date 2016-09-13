99bc422bb2bca18472949267ace2adc174eb8c64
Janice Falotico <jfalotic@gmail.com>
2016-09-13 10:21:13 -0400

    fix login page styles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,38 +1,65 @@
 @extends('layouts.app')
 
 @section('content')
-        <div>
-            <div class="jcf-form-wrap">
-                <div class="heading">
-                    <h2>Create Account</h2>
-                    <p>Fill out the following fields to create your account.</p>
+    <div class="login-wrap">
+        <div class="img-wrap">
+            <router-link to="/">
+                <img src="/assets/img/kodi_w.svg" alt="CodePier" style="display: block;">
+            </router-link>
+        </div>
+        
+        <div class="jcf-form-wrap" id="login-form">
+            <div class="heading">
+                <h2>Login</h2>
+            </div>
+
+            <form action="#0" method="post" class="validation-form floating-labels">
+                <div class="jcf-input-group">
+                    <input type="email" id="email" name="email" required>
+                    <label for="email"><span class="float-label">Email</span></label>
                 </div>
+                <div class="jcf-input-group">
+                    <input type="password" id="password" name="password" required>
+                    <label for="password"><span class="float-label">Password</span></label>
+                </div>
+                <div class="btn-footer">
+                    <button class="btn">Create Account</button>
+                    <button class="btn btn-primary" type="submit">Login</button>
+                </div>
+            </form>
+        </div><!-- end form-wrap -->
+        
+        
+        <div class="jcf-form-wrap hide" id="login-form">
+            <div class="heading">
+                <h2>Create Account</h2>
+                <p>Fill out the following fields to create your account.</p>
+            </div>
 
-                <form action="#0" method="post" class="validation-form floating-labels">
-                    <div class="jcf-input-group">
-                        <input type="text" id="firstName" name="firstName" required>
-                        <label for="firstName"><span class="float-label">First Name</span></label>
-                    </div>
-                    <div class="jcf-input-group">
-                        <input type="text" id="lastName" name="lastName" required>
-                        <label for="lastName"><span class="float-label">Last Name</span></label>
-                    </div>
-                    <div class="jcf-input-group">
-                        <input type="email" id="email" name="email" required>
-                        <label for="email"><span class="float-label">Email</span></label>
-                    </div>
-                    <div class="jcf-input-group">
-                        <input type="password" id="password" name="password" required>
-                        <label for="password"><span class="float-label">Password</span></label>
-                    </div>
-                    <div class="btn-footer">
-                        <button class="btn">Cancel</button>
-                        <button class="btn btn-primary" type="submit">Sign Up</button>
-                    </div>
-                </form>
-            </div><!-- end form-wrap -->
-
-
+            <form action="#0" method="post" class="validation-form floating-labels">
+                <div class="jcf-input-group">
+                    <input type="text" id="firstName" name="firstName" required>
+                    <label for="firstName"><span class="float-label">First Name</span></label>
+                </div>
+                <div class="jcf-input-group">
+                    <input type="text" id="lastName" name="lastName" required>
+                    <label for="lastName"><span class="float-label">Last Name</span></label>
+                </div>
+                <div class="jcf-input-group">
+                    <input type="email" id="email" name="email" required>
+                    <label for="email"><span class="float-label">Email</span></label>
+                </div>
+                <div class="jcf-input-group">
+                    <input type="password" id="password" name="password" required>
+                    <label for="password"><span class="float-label">Password</span></label>
+                </div>
+                <div class="btn-footer">
+                    <button class="btn">Cancel</button>
+                    <button class="btn btn-primary" type="submit">Sign Up</button>
+                </div>
+            </form>
+        </div><!-- end form-wrap -->
+        
         <h5 class="text-center"> - Or sign in using -</h5>
         <ul class="list-inline text-center">
             <li>
@@ -46,4 +73,8 @@
             </li>
         </ul>
     </div>
+            
+
+
+        
 @endsection
