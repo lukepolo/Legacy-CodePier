34a0a74a2fcf9ec75de0c7f341523b994229d09e
Luke Policinski <luke@lukepolo.com>
2016-10-20 10:48:30 +0000

    if i go back to vue/dist/vue works perfectly

--- resources/assets/js/pages/dashboard/Dashboard.vue
+++ resources/assets/js/pages/dashboard/Dashboard.vue
@@ -1,18 +1,56 @@
 <template>
     <section>
-        here
+        <left-nav></left-nav>
+        <section id="middle" class="section-column">
+            <h3 class="section-header primary">Servers</h3>
+            <div class="section-content">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>Server</th>
+                        <th>Provider</th>
+                        <th>Region</th>
+                        <th>Status</th>
+                        <th>Connection</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="server in servers">
+                        <td>
+                            <router-link :to="{ path: '/server/'+server.id+'/sites' }">
+                                {{ server.name }}
+                            </router-link>
+                        </td>
+                        <td>{{ server.ip }}</td>
+                        <td>{{ server.server_provider.name }}</td>
+                        <td>REGION TODO</td>
+                        <td>{{ server.status }}</td>
+                        <td>
+                            <div class="server-name">
+                                <span :class="{ 'server-success' : server.ssh_connection , 'server-error' : !server.ssh_connection}" class="server-connection" data-toggle="tooltip" data-placement="top" data-container="body" title="Connection Successful"></span>
+                            </div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
     </section>
 </template>
 
 <script>
+    import LeftNav from './../../core/LeftNav.vue';
+
     export default {
+        components: {
+            LeftNav
+        },
         computed: {
             servers() {
                 return serverStore.state.servers;
             }
         },
         created() {
-            alert('test');
             serverStore.dispatch('getServers');
         }
     }
