eed66543f1d311fa54cccf7479de23626e2b8b54
Janice Falotico <jfalotic@gmail.com>
2016-10-21 13:48:52 -0400

    SSL Form Styles

--- resources/assets/js/pages/site/SiteSSLCertificates.vue
+++ resources/assets/js/pages/site/SiteSSLCertificates.vue
@@ -1,10 +1,21 @@
 <template>
     <div v-if="site">
-        <form @submit.prevent="installLetsEncryptCertificate">
-            Domains
-            <input type="text" v-model="domains" name="domains">
-            <button type="submit">Install Let's Encrypt Certificate</button>
-        </form>
+        <div class="jcf-form-wrap">
+            <form @submit.prevent="installLetsEncryptCertificate">
+                <div class="jcf-input-group">
+                    <input type="text" v-model="domains" name="domains">
+                    <label for="domains">
+                        <span class="float-label">Domains</span>
+                    </label>
+                </div>
+
+                <div class="btn-footer">
+                    <button class="btn btn-primary" type="submit">Install Let's Encrypt Certificate</button>
+                </div>
+            </form>
+        </div>
+
+
         <p v-for="ssl_certificate in ssl_certificates">
             {{ ssl_certificate.type }} : {{ ssl_certificate.domains }} : {{ ssl_certificate.cert_path }} :
             {{ ssl_certificate.key_path }}
