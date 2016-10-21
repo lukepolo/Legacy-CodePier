e4df4d178a1cb78896c9a52ab5b42c2789e1e8aa
Luke Policinski <Luke@LukePOLO.com>
2016-10-21 00:26:04 +0000

    adding annimations

---
+++ resources/assets/js/pages/site/SiteArea.vue
@@ -0,0 +1,73 @@
+<style>
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-active {
+        opacity: 0
+    }
+
+    .bounce-enter-active {
+        animation: bounce-in .5s;
+    }
+    .bounce-leave-active {
+        animation: bounce-out .5s;
+    }
+    @keyframes bounce-in {
+        0% {
+            transform: scale(0);
+        }
+        50% {
+            transform: scale(1.5);
+        }
+        100% {
+            transform: scale(1);
+        }
+    }
+    @keyframes bounce-out {
+        0% {
+            transform: scale(1);
+        }
+        50% {
+            transform: scale(1.5);
+        }
+        100% {
+            transform: scale(0);
+        }
+    }
+</style>
+
+<template>
+    <div class="parent">
+        <left-nav></left-nav>
+        <section id="middle" class="section-column">
+
+            <site-header></site-header>
+
+            <transition name="bounce">
+                <router-view name="nav"></router-view>
+            </transition>
+
+            <transition name="bounce">
+                <router-view class="container"></router-view>
+            </transition>
+
+        </section>
+        <servers></servers>
+    </div>
+</template>
+
+<script>
+    import SiteNav from './components/SiteNav.vue';
+    import LeftNav from './../../core/LeftNav.vue';
+    import Servers from './components/Servers.vue';
+    import SiteHeader from './components/SiteHeader.vue';
+
+    export default {
+        components: {
+            SiteNav,
+            LeftNav,
+            Servers,
+            SiteHeader,
+        }
+    }
+</script>
--- resources/assets/js/pages/site/SiteRepository.vue
+++ resources/assets/js/pages/site/SiteRepository.vue
@@ -1,84 +1,65 @@
 <template>
     <section>
-        <left-nav></left-nav>
-        <section id="middle" class="section-column">
-            <site-header></site-header>
-            <div class="section-content" v-if="site">
-                <div class="container">
-                    <site-nav></site-nav>
+        <div class="section-content" v-if="site">
+            <form @submit.prevent="updateSite">
+                <label>Repository</label>
+                <input type="text" v-model="form.repository" name="repository">
 
-                    <form @submit.prevent="updateSite">
-                        <label>Repository</label>
-                        <input type="text" v-model="form.repository" name="repository">
+                <label>Branch</label>
+                <input type="text" v-model="form.branch" name="branch">
 
-                        <label>Branch</label>
-                        <input type="text" v-model="form.branch" name="branch">
+                <label>Web Directory</label>
+                <input type="text" name="web_directory" v-model="form.web_directory">
+                <label>
+                    <input type="checkbox" v-model="form.zerotime_deployment" name="zerotime_deployment"
+                           value="1">
+                    Zerotime Deployment
+                </label>
 
-                        <label>Web Directory</label>
-                        <input type="text" name="web_directory" v-model="form.web_directory">
+                <label>
+                    <input type="checkbox" v-model="form.wildcard_domain" name="wildcard_domain" value="1">
+                    Wildcard Domain
+                </label>
+
+                <div class="form-group">
+                    <div class="radio" v-for="user_repository_provider in user_repository_providers">
                         <label>
-                            <input type="checkbox" v-model="form.zerotime_deployment" name="zerotime_deployment"
-                                   value="1">
-                            Zerotime Deployment
+                            <input name="user_repository_provider_id" type="radio"
+                                   v-model="form.user_repository_provider_id"
+                                   :value="user_repository_provider.id">
+                            {{ user_repository_provider.repository_provider.name }}
                         </label>
-
-                        <label>
-                            <input type="checkbox" v-model="form.wildcard_domain" name="wildcard_domain" value="1">
-                            Wildcard Domain
-                        </label>
-
-                        <div class="form-group">
-                            <div class="radio" v-for="user_repository_provider in user_repository_providers">
-                                <label>
-                                    <input name="user_repository_provider_id" type="radio"
-                                           v-model="form.user_repository_provider_id"
-                                           :value="user_repository_provider.id">
-                                    {{ user_repository_provider.repository_provider.name }}
-                                </label>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <select v-model="form.framework" name="framework">
-                                <option></option>
-                                <optgroup :label="language" v-for="(features, language) in availableLanguages">
-                                    <option v-for="(features, framework) in availableFrameworks[language]"
-                                            :value="language+'.'+framework"> {{ framework }}
-                                    </option>
-                                </optgroup>
-                            </select>
-                        </div>
-
-                        <button type="submit">Update Repository</button>
-                    </form>
-
-                    <template v-if="site.repository && site_servers.length">
-                        <a href="#" @click.prevent="deploySite(site.id)" class="btn btn-primary">Deploy</a>
-                        <a v-if="!site.automatic_deployment_id" href="#" class="btn btn-primary">Start Automatic
-                            Deployments</a>
-                        <a v-else href="#" class="btn btn-primary">Stop Automatic Deployments</a>
-                    </template>
-                    <div @click="deleteSite(site.id)" class="btn btn-xs">Delete Site</div>
+                    </div>
                 </div>
-            </div>
-        </section>
-        <servers></servers>
+
+                <div class="form-group">
+                    <select v-model="form.framework" name="framework">
+                        <option></option>
+                        <optgroup :label="language" v-for="(features, language) in availableLanguages">
+                            <option v-for="(features, framework) in availableFrameworks[language]"
+                                    :value="language+'.'+framework"> {{ framework }}
+                            </option>
+                        </optgroup>
+                    </select>
+                </div>
+
+                <button type="submit">Update Repository</button>
+            </form>
+
+            <template v-if="site.repository && site_servers.length">
+                <a href="#" @click.prevent="deploySite(site.id)" class="btn btn-primary">Deploy</a>
+                <a v-if="!site.automatic_deployment_id" href="#" class="btn btn-primary">Start Automatic
+                    Deployments</a>
+                <a v-else href="#" class="btn btn-primary">Stop Automatic Deployments</a>
+            </template>
+            <div @click="deleteSite(site.id)" class="btn btn-xs">Delete Site</div>
+        </div>
     </section>
 </template>
 
 <script>
 
-    import LeftNav from './../../core/LeftNav.vue';
-    import SiteNav from './components/SiteNav.vue';
-    import Servers from './components/Servers.vue';
-    import SiteHeader from './components/SiteHeader.vue';
     export default {
-        components: {
-            SiteHeader,
-            SiteNav,
-            LeftNav,
-            Servers
-        },
         data() {
             return {
                 form: {
