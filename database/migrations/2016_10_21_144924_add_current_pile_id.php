74f9c6943a05270f6ce67d5e4a8be567febe5e73
Luke Policinski <Luke@LukePOLO.com>
2016-10-21 11:11:44 -0400

    Applied fixes from StyleCI (#59)

--- database/migrations/2016_10_21_144924_add_current_pile_id.php
+++ database/migrations/2016_10_21_144924_add_current_pile_id.php
@@ -13,7 +13,7 @@ class AddCurrentPileId extends Migration
      */
     public function up()
     {
-        Schema::table('users', function(Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->integer('current_pile_id')->nullable();
         });
     }
@@ -25,7 +25,7 @@ class AddCurrentPileId extends Migration
      */
     public function down()
     {
-        Schema::table('users', function(Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('current_pile_id');
         });
     }
