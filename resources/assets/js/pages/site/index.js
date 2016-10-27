578894d5ac86c6aab88ffd112168c2848a93cb8a
Luke Policinski <Luke@LukePOLO.com>
2016-10-27 22:45:22 +0000

    new nav sets in

--- resources/assets/js/pages/site/index.js
+++ resources/assets/js/pages/site/index.js
@@ -1,11 +1,13 @@
 export const SiteArea = require("./SiteArea.vue");
 export const SiteJobs = require("./SiteJobs.vue");
+export const SiteFiles = require("./SiteFiles.vue");
+export const SiteSshKeys = require("./SiteSshKeys.vue");
 export const SiteNav = require("./components/SiteNav.vue");
 export const SiteRepository = require("./SiteRepository.vue");
-export const SiteServerFeatures = require("./SiteServerFeatures.vue");
-export const SiteFiles = require("./SiteFiles.vue");
-export const SiteSSLCertificates = require("./SiteSSLCertificates.vue");
 export const SiteFirewallRules = require('./SiteFirewallRules.vue');
+export const SiteServerFeatures = require("./SiteServerFeatures.vue");
+export const SiteSSLCertificates = require("./SiteSSLCertificates.vue");
+
 
 
 
