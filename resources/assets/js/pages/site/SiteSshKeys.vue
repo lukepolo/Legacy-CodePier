e8ddae0fe46e8e5ecafffee3915b1f07a70754a9
Luke Policinski <Luke@LukePOLO.com>
2016-11-01 07:29:09 +0000

    site ssh keys

--- resources/assets/js/pages/site/SiteSshKeys.vue
+++ resources/assets/js/pages/site/SiteSshKeys.vue
@@ -1,14 +1,46 @@
 <template>
     <div v-if="site">
         Site SSH Keys
-        <template v-for="sshKey in sshKeys">
-            {{ sshKey.id }}
-        </template>
+        <form @submit.prevent="createKey">
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" name="name" v-model="form.name">
+            </div>
+            <div class="form-group">
+                <label>Public Key</label>
+                <textarea name="ssh_key" v-model="form.ssh_key"></textarea>
+            </div>
+            <button type="submit">Install SSH KEY</button>
+        </form>
+
+        <table class="table" v-for="sshKey in sshKeys">
+            <thead>
+                <tr>
+                    <th>Key Name</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{{ sshKey.name }}</td>
+                    <td><a href="#" class="fa fa-remove" @click="deleteKey(sshKey.id)">remove</a></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                form: {
+                    name: null,
+                    ssh_key: null,
+                    site : this.$route ? this.$route.params.site_id : null,
+                }
+            }
+        },
         created() {
             this.fetchData();
         },
@@ -17,7 +49,18 @@
         },
         methods: {
             fetchData() {
+                this.$store.dispatch('getSite', this.$route.params.site_id);
                 this.$store.dispatch('getSiteSshKeys', this.$route.params.site_id);
+            },
+            createKey() {
+                this.$store.dispatch('createSiteSshKey', this.form);
+                this.form = this.$options.data().form;
+            },
+            deleteKey(sshKeyId) {
+                this.$store.dispatch('deleteSiteSshKey', {
+                    ssh_key: sshKeyId,
+                    site: this.form.site,
+                });
             }
         },
         computed: {
