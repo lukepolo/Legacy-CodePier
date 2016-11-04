7258e5ed6fe6a8e2c02249861dfc60f90030f6e6
Luke Policinski <luke@lukepolo.com>
2016-11-04 17:02:01 +0000

    removed duplicate code

--- app/Observers/Site/SiteObserver.php
+++ app/Observers/Site/SiteObserver.php
@@ -38,53 +38,6 @@ class SiteObserver
         foreach ($site->provisionedServers() as $server) {
             $this->siteService->create($server, $site);
         }
-
-        if ($site->deploymentSteps->count() == 0) {
-            $defaultSteps = [
-                [
-                    'step'                         => 'Clone Repository',
-                    'order'                        => '1',
-                    'internal_deployment_function' => 'cloneRepository',
-                    'customizable'                 => false,
-                ],
-                [
-                    'step'                         => 'Install PHP Dependencies',
-                    'order'                        => '2',
-                    'internal_deployment_function' => 'installPhpDependencies',
-                    'customizable'                 => true,
-                ],
-                [
-                    'step' => 'Install Node Dependencies',
-                    'order' => '3',
-                    'internal_deployment_function' => 'installNodeDependencies',
-                    'customizable' => true,
-                ],
-                [
-                    'step'                         => 'Run Migrations',
-                    'order'                        => '4',
-                    'internal_deployment_function' => 'runMigrations',
-                    'customizable'                 => true,
-                ],
-                [
-                    'step'                         => 'Setup Release',
-                    'order'                        => '5',
-                    'internal_deployment_function' => 'setupFolders',
-                    'customizable'                 => false,
-                ],
-                [
-                    'step'                         => 'Clean Up Old Releases',
-                    'order'                        => '6',
-                    'internal_deployment_function' => 'cleanup',
-                    'customizable'                 => true,
-                ],
-            ];
-
-            foreach ($defaultSteps as $defaultStep) {
-                DeploymentStep::firstOrCreate(
-                    array_merge(['site_id' => $site->id], $defaultStep)
-                );
-            }
-        }
     }
 
     public function deleted()
