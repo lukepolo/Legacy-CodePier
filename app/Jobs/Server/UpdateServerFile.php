36e3e211c567344e6071b9b0657134e286b5d834
Luke Policinski <Luke@LukePOLO.com>
2016-11-07 14:23:08 +0000

    Applied fixes from StyleCI

--- app/Jobs/Server/UpdateServerFile.php
+++ app/Jobs/Server/UpdateServerFile.php
@@ -18,7 +18,6 @@ class UpdateServerFile implements ShouldQueue
     private $server;
     private $siteFile;
 
-
     /**
      * Create a new job instance.
      * @param Server $server
@@ -40,7 +39,7 @@ class UpdateServerFile implements ShouldQueue
     public function handle(ServerService $serverService)
     {
         $this->runOnServer(function () use ($serverService) {
-                $serverService->saveFile($this->server, $this->siteFile->file_path, $this->siteFile->content, 'codepier');
+            $serverService->saveFile($this->server, $this->siteFile->file_path, $this->siteFile->content, 'codepier');
         });
 
         return $this->remoteResponse();
