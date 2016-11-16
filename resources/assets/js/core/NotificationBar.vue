cba9ec5f12bb805cdb647bc565c85d2a45a6f6c2
Janice Falotico <jfalotic@gmail.com>
2016-11-16 09:17:20 -0500

    Fixing Event Container

--- resources/assets/js/core/NotificationBar.vue
+++ resources/assets/js/core/NotificationBar.vue
@@ -296,7 +296,7 @@
                                                     <a class="collapsed" data-toggle="collapse" :href="'#deployment_event_' + deployment_event.id" v-if="deployment_event.log && filterArray(deployment_event.log).length">
                                                         <span class="icon-play"></span>
                                                     </a> {{ deployment_event.step.step }} took {{ formatSeconds(deployment_event.runtime) }} seconds
-                                                    <div class="event-details collapse" :id="'deployment_event_'+deployment_event.id">
+                                                    <div class="event-details collapse out" :id="'deployment_event_'+deployment_event.id">
                                                         <pre v-for="log in filterArray(deployment_event.log)" v-if="deployment_event.log">{{ log }}</pre>
                                                     </div>
                                                 </li>
