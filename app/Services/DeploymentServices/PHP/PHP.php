e8d9dde88f6cb88622b9a9b911bca0176ea9af5f
Luke Policinski <luke@lukepolo.com>
2016-11-17 16:53:22 +0000

    start of suggestedDefaults

--- app/Services/DeploymentServices/PHP/PHP.php
+++ app/Services/DeploymentServices/PHP/PHP.php
@@ -19,6 +19,72 @@ class PHP
     private $remoteTaskService;
     private $repositoryProvider;
 
+    public $suggestedProvisioning = [
+        'OsService' => [
+            'Swap' => [
+                'enabled' => 1,
+            ],
+            'parameters' => [
+                'size' => '1G',
+                'swappiness' => 10,
+                'vfsCachePressure' => 50,
+            ]
+        ],
+        'WebService' => [
+            'Nginx' => [
+                'enabled' => 1,
+            ],
+            'CertBot' => [
+                'enabled' => 1
+            ]
+        ],
+        'NodeService' => [
+            'Yarn' => [
+                'enabled' => 1
+            ],
+            'NodeJs' => [
+                'enabled' => 1
+            ]
+        ],
+        'WorkerService' => [
+            'Beanstalk' => [
+                'enabled' => 1
+            ],
+            'Supervisor' => [
+                'enabled' => 1
+            ]
+        ],
+        'DatabaseService' => [
+            'Redis' => [
+                'enabled' => 1
+            ],
+            'MariaDB' => [
+                'enabled' => 1
+            ]
+        ],
+        'Languages\PHP\PHP' => [
+            'PHP7' => [
+                'enabled' => 1
+            ],
+            'PhpFpm' => [
+                'enabled' => 1
+            ],
+            'Composer' => [
+                'enabled' => 1
+            ]
+        ],
+        'MonitoringService' => [
+            'DiskMonitoringScript' => [
+                'enabled' => 1,
+            ]
+        ],
+        'RepositoryService' => [
+            'Git' => [
+                'enabled' => 1
+            ]
+        ]
+    ];
+
     /**
      * @param RemoteTaskService $remoteTaskService
      * @param Server            $server
