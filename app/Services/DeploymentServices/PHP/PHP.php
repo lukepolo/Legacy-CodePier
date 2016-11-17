1b947586ca9eee553d19f58e56d5237e480fb8dc
Luke Policinski <Luke@LukePOLO.com>
2016-11-17 17:22:48 +0000

    Applied fixes from StyleCI

--- app/Services/DeploymentServices/PHP/PHP.php
+++ app/Services/DeploymentServices/PHP/PHP.php
@@ -19,7 +19,7 @@ class PHP
     private $remoteTaskService;
     private $repositoryProvider;
 
-    public $suggestedFeatures= [
+    public $suggestedFeatures = [
         'OsService' => [
             'Swap' => [
                 'enabled' => 1,
