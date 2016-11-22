81933b728cfd3277d1961189ca5304bdb55adef4
Luke Policinski <luke@lukepolo.com>
2016-11-22 22:12:23 +0000

    start of event filters

--- resources/assets/js/core/NotificationBar.vue
+++ resources/assets/js/core/NotificationBar.vue
@@ -38,20 +38,11 @@
                                         <span class="icon"></span>
                                         Select All
                                     </label>
-                                    <label>
+
+                                    <label v-for="pile in piles">
                                         <input type="checkbox">
                                         <span class="icon"></span>
-                                        Dev
-                                    </label>
-                                    <label>
-                                        <input type="checkbox">
-                                        <span class="icon"></span>
-                                        QA
-                                    </label>
-                                    <label>
-                                        <input type="checkbox">
-                                        <span class="icon"></span>
-                                        Prod
+                                        {{ pile.name }}
                                     </label>
                                 </div>
                             </form>
@@ -275,63 +266,7 @@
                 <!--</div>-->
 
                 <section v-for="event in events">
-                    <div class="event">
-                        <div class="event-status" :class="{'event-status-neutral' : !event.status, 'event-status-success' : event.status == 'Completed', 'event-status-error' : event.status == 'Failed', 'icon-spinner' : event.status == 'Running'}"></div>
-                        <div class="event-name">
-                            <a class="collapsed" data-toggle="collapse" :href="'#' + event.id">
-                                <span class="icon-play"></span>
-                            </a> Deployment
-
-                            <div class="event-details collapse" :id="event.id">
-
-                                <template v-for="server_deployment in event.server_deployments">
-
-                                    <div class="event-status" :class="{'event-status-neutral' : (! server_deployment.failed && ! server_deployment.completed && !server_deployment.started), 'event-status-success' : server_deployment.completed, 'event-status-error' : server_deployment.failed, 'icon-spinner' : (!server_deployment.failed && !server_deployment.completed && server_deployment.started) }"></div>
-
-                                    <a class="collapsed" data-toggle="collapse" :href="'#' + event.id + '_server_deployment_' + server_deployment.server.id">
-                                        <span class="icon-play"></span>
-                                    </a>
-
-                                    {{ server_deployment.server.name }} ({{ server_deployment.server.ip }}) - {{ server_deployment.status }}
-
-                                    <div class="event-details collapse" :id="event.id + '_server_deployment_' + server_deployment.server.id">
-
-                                        <ul>
-                                            <template v-for="deployment_event in server_deployment.events">
-                                                <li :class="{'event-error' : deployment_event.failed }">
-
-                                                    <template v-if="!deployment_event.started || (deployment_event.started && deployment_event.completed) || (deployment_event.started && deployment_event.failed)">
-                                                        <div class="event-status" :class="{'event-status-neutral' : (! deployment_event.failed && ! deployment_event.completed), 'event-status-success' : deployment_event.completed, 'event-status-error' : deployment_event.failed}"></div>
-                                                    </template>
-                                                    <template v-else>
-                                                        <span class="icon-spinner"></span>
-                                                    </template>
-
-                                                    <a class="collapsed" :class="{ 'in' : deployment_event.failed }" data-toggle="collapse" :href="'#deployment_event_' + deployment_event.id" v-if="deployment_event.log && filterArray(deployment_event.log).length">
-                                                        <span class="icon-play"></span>
-                                                    </a>
-
-                                                    {{ deployment_event.step.step }}
-                                                    <template v-if="deployment_event.completed">
-                                                        took {{ formatSeconds(deployment_event.runtime) }} seconds
-                                                    </template>
-
-                                                    <div class="event-details collapse" :class="{ 'in' : deployment_event.failed, 'out' : !deployment_event.failed }" :id="'deployment_event_'+deployment_event.id">
-                                                        <pre v-for="log in filterArray(deployment_event.log)" v-if="deployment_event.log">{{ log }}</pre>
-                                                    </div>
-                                                </li>
-                                            </template>
-                                        </ul>
-
-                                    </div>
-                                </template>
-
-                            </div>
-                        </div>
-                        <div class="event-pile"><span class="icon-layers"></span> {{ event.site.pile.name }}</div>
-                        <div class="event-site"><span class="icon-browser"></span> {{ event.site.name }}</div>
-                        <div class="event-commit"><a target="_blank" :href="'https://'+ event.site.user_repository_provider.repository_provider.url + '/' + event.site.repository + '/commit/' + event.git_commit"><span class="icon-github"></span> </a></div>
-                    </div>
+                    <deployment-event :event="event"></deployment-event>
                 </section>
             </div>
         </div>
@@ -342,6 +277,7 @@
 <script>
 
     import ServerEvent from './components/ServerEvent.vue';
+    import DeploymentEvent from './components/DeploymentEvent.vue';
 
     Vue.directive('resizeable', {
         inserted: function (el, bindings) {
@@ -382,38 +318,33 @@
     Vue.directive('watch-scroll', {
         update: function (el, bindings) {
 
-            $(el).unbind('scroll');
-
-            var pagination = bindings.value;
-
-            var nextPage = pagination.current_page + 1;
-
-            if (nextPage <= pagination.last_page) {
-
-                $(el).bind('scroll', function() {
-                    var $el = $(el);
-                    if (el.scrollHeight - $el.scrollTop() - $el.outerHeight() < 1) {
-                        eventStore.dispatch('getEvents', nextPage);
-                    }
-                });
-            }
+//            $(el).unbind('scroll');
+//
+//            var pagination = bindings.value;
+//
+//            var nextPage = pagination.current_page + 1;
+//
+//            if (nextPage <= pagination.last_page) {
+//
+//                $(el).bind('scroll', function() {
+//                    var $el = $(el);
+//                    if (el.scrollHeight - $el.scrollTop() - $el.outerHeight() < 1) {
+//                        eventStore.dispatch('getEvents', nextPage);
+//                    }
+//                });
+//            }
         }
     });
 
     export default {
         components : {
-            ServerEvent
+            ServerEvent,
+            DeploymentEvent,
         },
         created() {
             this.fetchData();
         },
         methods: {
-            filterArray(data) {
-                if(Array.isArray(data)) {
-                    return data.filter(String);
-                }
-              return [];
-            },
             fetchData() {
 
                 var store = this.$store;
@@ -465,9 +396,6 @@
                 });
 
                 store.dispatch('getEvents');
-            },
-            formatSeconds(number) {
-                return parseFloat(number).toFixed(2);
             }
         },
         computed: {
@@ -479,6 +407,9 @@
             },
             events_pagination() {
                 return this.$store.state.eventsStore.events_pagination;
+            },
+            piles() {
+                return this.$store.state.pilesStore.piles;
             }
         },
     }
