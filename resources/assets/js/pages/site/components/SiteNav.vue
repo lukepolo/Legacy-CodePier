bda77ed6da6ab816ca1c98fea408b257c192a3a6
Luke Policinski <luke@lukepolo.com>
2016-11-22 14:38:22 +0000

    servers now fixed to use back button

--- resources/assets/js/pages/site/components/SiteNav.vue
+++ resources/assets/js/pages/site/components/SiteNav.vue
@@ -1,41 +1,37 @@
 <template>
     <ul class="wizard" v-if="site">
+
         <router-link :to="{ name : 'site_repository', params : { site_id : site.id } }" tag="li" class="wizard-item">
             <a>Repository</a>
         </router-link>
-        <template v-if="site">
-            <router-link :to="{ name : 'site_deployment', params : { site_id : site.id } }" tag="li" class="wizard-item">
-                <a>Deployment</a>
-            </router-link>
-        </template>
-        <template v-if="site">
-            <router-link :to="{ name : 'site_files', params : { site_id : site.id } }" tag="li" class="wizard-item">
-                <a>Files</a>
-            </router-link>
-        </template>
-        <template v-if="site_servers">
-            <router-link :to="{ name : 'site_workers', params : { site_id : site.id } }" tag="li" class="wizard-item">
-                <a>Workers</a>
-            </router-link>
-            <router-link :to="{ name : 'site_ssl_certs', params : { site_id : site.id } }" tag="li" class="wizard-item">
-                <a>SSLCertificates</a>
-            </router-link>
-        </template>
-        <template v-if="site">
-            <router-link :to="{ name : 'site_firewall_rules', params : { site_id : site.id } }" tag="li" class="wizard-item">
-                <a>Site Firewall Rules</a>
-            </router-link>
-        </template>
-        <template v-if="site">
-            <router-link :to="{ name : 'site_ssh_keys', params : { site_id : site.id } }" tag="li" class="wizard-item">
-                <a>Site Ssh Keys</a>
-            </router-link>
-        </template>
-        <template v-if="site.repository">
-            <router-link :to="{ name : 'site_server_features', params : { site_id : site.id } }" tag="li" class="wizard-item">
-                <a>Server Features</a>
-            </router-link>
-        </template>
+
+        <router-link :to="{ name : 'site_deployment', params : { site_id : site.id } }" tag="li" class="wizard-item">
+            <a>Deployment</a>
+        </router-link>
+
+        <router-link :to="{ name : 'site_files', params : { site_id : site.id } }" tag="li" class="wizard-item">
+            <a>Files</a>
+        </router-link>
+
+        <router-link :to="{ name : 'site_workers', params : { site_id : site.id } }" tag="li" class="wizard-item">
+            <a>Workers</a>
+        </router-link>
+
+        <router-link :to="{ name : 'site_ssl_certs', params : { site_id : site.id } }" tag="li" class="wizard-item" v-if="site.domain != 'default' ">
+            <a>SSLCertificates</a>
+        </router-link>
+
+        <router-link :to="{ name : 'site_firewall_rules', params : { site_id : site.id } }" tag="li" class="wizard-item">
+            <a>Site Firewall Rules</a>
+        </router-link>
+
+        <router-link :to="{ name : 'site_ssh_keys', params : { site_id : site.id } }" tag="li" class="wizard-item">
+            <a>Site Ssh Keys</a>
+        </router-link>
+
+        <router-link :to="{ name : 'site_server_features', params : { site_id : site.id } }" tag="li" class="wizard-item" v-if="site.repository">
+            <a>Server Features</a>
+        </router-link>
     </ul>
 </template>
 
