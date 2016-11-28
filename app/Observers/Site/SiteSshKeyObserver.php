414bb35d52af3311681e6eb933022d826ce36c95
Luke Policinski <luke@lukepolo.com>
2016-11-28 16:32:24 +0000

    we found a way , just need to implement and add new models etc to make it all work toegher

--- app/Observers/Site/SiteSshKeyObserver.php
+++ app/Observers/Site/SiteSshKeyObserver.php
@@ -4,25 +4,36 @@ namespace App\Observers\Site;
 
 use App\Models\Server\ServerSshKey;
 use App\Models\Site\SiteSshKey;
+use App\Traits\ModelCommandTrait;
 
 class SiteSshKeyObserver
 {
+    use ModelCommandTrait;
+
     /**
      * @param SiteSshKey $siteSshKey
      */
     public function created(SiteSshKey $siteSshKey)
     {
+
         foreach ($siteSshKey->site->provisionedServers as $server) {
             if (! ServerSshKey::where('name', $siteSshKey->name)
                 ->where('ssh_key', $siteSshKey->ssh_key)
                 ->count()
             ) {
-                ServerSshKey::create([
+
+                $serverSshKey = new ServerSshKey([
                     'server_id' => $server->id,
                     'name' => $siteSshKey->name,
                     'ssh_key' => $siteSshKey->ssh_key,
                     'site_ssh_key_id' => $siteSshKey->id,
                 ]);
+
+                $serverSshKey->addHidden([
+                    'command' => $this->makeCommand($serverSshKey, $siteSshKey->site_id)
+                ]);
+
+                $serverSshKey->save();
             }
         }
     }
