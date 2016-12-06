292b64b5329d89976bbecc982829e7c3c07c0d51
Luke Policinski <Luke@LukePOLO.com>
2016-12-06 16:32:24 -0500

    Apply fixes from StyleCI (#188)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -41,7 +41,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if($this->app->environment() != 'production') {
+        if ($this->app->environment() != 'production') {
             $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
