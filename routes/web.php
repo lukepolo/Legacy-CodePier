4d86bf1e688aba6f64f8605e041d5ff25acab718
Luke Policinski <luke@lukepolo.com>
2016-12-09 00:10:46 +0000

    made new route so we can cache routes

--- routes/web.php
+++ routes/web.php
@@ -47,11 +47,7 @@ Route::resource('subscription/plans', 'SubscriptionController');
 */
 
 Route::group(['prefix' => 'webhook'], function () {
-    Route::get('/deploy/{siteHashID}', function ($siteHashID) {
-        dispatch(new \App\Jobs\Site\DeploySite(
-            \App\Models\Site\Site::with('server')->findOrFail(\Hashids::decode($siteHashID)[0])
-        ));
-    })->name('webhook/deploy');
+    Route::get('/deploy/{siteHashID}')->name('webhook/deploy');
 });
 
 /*
