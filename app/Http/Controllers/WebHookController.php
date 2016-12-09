a1c6f20960774b8cd812501d59f8b689a9ee470b
Luke Policinski <luke@lukepolo.com>
2016-12-09 00:10:46 +0000

    made new route so we can cache routes

---
+++ app/Http/Controllers/WebHookController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Site\Site;
+
+class WebHookController extends Controller
+{
+    /**
+     * @param $siteHashID
+     */
+    public function deploy($siteHashID)
+    {
+        dispatch(new \App\Jobs\Site\DeploySite(
+            Site::with('server')->findOrFail(\Hashids::decode($siteHashID)[0])
+        ));
+    }
+}
