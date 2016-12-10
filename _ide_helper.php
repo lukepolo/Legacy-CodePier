cdfae53d607b9531bcc6630daace96b4b06c0d40
Luke Policinski <luke@lukepolo.com>
2016-12-10 11:59:51 +0000

    vendor updates

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.3.26 on 2016-12-09.
+ * Generated for Laravel 5.3.26 on 2016-12-10.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
@@ -899,6 +899,24 @@ if (! function_exists('with')) {
     function with($object)
     {
         return $object;
+    }
+}
+
+
+if (! function_exists('strip_relations')) {
+
+    /**
+     * Strips the relations from a model.
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    function strip_relations(\Illuminate\Database\Eloquent\Model $model)
+    {
+        foreach ($model->getRelations() as $relation => $data) {
+            unset($model->$relation);
+        }
+
+        return $model;
     }
 }
 
@@ -14264,3 +14282,26 @@ if (! function_exists('with')) {
 
 
 
+namespace Illuminate\Support {
+    /**
+     * Methods commonly used in migrations
+     *
+     * @method Fluent after(string $column) Add the after modifier
+     * @method Fluent charset(string $charset) Add the character set modifier
+     * @method Fluent collation(string $collation) Add the collation modifier
+     * @method Fluent comment(string $comment) Add comment
+     * @method Fluent default(mixed $value) Add the default modifier
+     * @method Fluent first() Select first row
+     * @method Fluent index(string $name = null) Add the in dex clause
+     * @method Fluent on(string $column) `on` of a foreign key
+     * @method Fluent onDelete(string $action) `on delete` of a foreign key
+     * @method Fluent onUpdate(string $action) `on update` of a foreign key
+     * @method Fluent primary() Add the primary key modifier
+     * @method Fluent references(string $table) `references` of a foreign key
+     * @method Fluent nullable() Add the nullable modifier
+     * @method Fluent unique(string $name = null) Add unique index clause
+     * @method Fluent unsigned() Add the unsigned modifier
+     * @method Fluent useCurrent() Add the default timestamp value
+     */
+    class Fluent {}
+}
