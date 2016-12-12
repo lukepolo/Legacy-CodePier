b2328bc617db9b525f996638751c1436809921ea
Luke Policinski <luke@lukepolo.com>
2016-12-12 22:19:05 +0000

    ssite ssh key should be fixed

--- app/Models/Server/ServerSshKey.php
+++ app/Models/Server/ServerSshKey.php
@@ -3,15 +3,20 @@
 namespace App\Models\Server;
 
 use App\Traits\ConnectedToUser;
+use App\Traits\Encryptable;
 use Illuminate\Database\Eloquent\Model;
 
 class ServerSshKey extends Model
 {
-    use ConnectedToUser;
+    use ConnectedToUser, Encryptable;
 
     protected $guarded = ['id'];
 
     public static $userModel = 'server';
+
+    protected $encryptable = [
+        'ssh_key',
+    ];
 
     public function server()
     {
--- app/Models/User/UserSshKey.php
+++ app/Models/User/UserSshKey.php
@@ -3,11 +3,16 @@
 namespace App\Models\User;
 
 use App\Traits\ConnectedToUser;
+use App\Traits\Encryptable;
 use Illuminate\Database\Eloquent\Model;
 
 class UserSshKey extends Model
 {
-    use ConnectedToUser;
+    use ConnectedToUser, Encryptable;
 
     protected $guarded = ['id'];
+
+    protected $encryptable = [
+        'ssh_key',
+    ];
 }
