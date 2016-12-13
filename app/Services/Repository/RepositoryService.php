f1f04479f6d2fcf9ffb42b4dde31e667a7087f67
Luke Policinski <luke@lukepolo.com>
2016-12-13 18:56:05 +0000

    we must login via codepier to run this

--- app/Services/Repository/RepositoryService.php
+++ app/Services/Repository/RepositoryService.php
@@ -98,7 +98,7 @@ class RepositoryService implements RepositoryServiceContract
         $sshFile = '~/.ssh/'.$site->id.'_id_rsa';
 
         foreach ($site->provisionedServers as $server) {
-            $this->remoteTaskService->ssh($server);
+            $this->remoteTaskService->ssh($server, 'codepier');
 
             $this->remoteTaskService->writeToFile($sshFile, $site->private_ssh_key);
             $this->remoteTaskService->writeToFile($sshFile.'.pub', $site->public_ssh_key);
