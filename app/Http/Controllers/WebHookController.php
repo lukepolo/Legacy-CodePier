d2c744aeb78710bdacfd3501e11e2ee457605cb7
Luke Policinski <luke@lukepolo.com>
2016-12-13 01:01:22 +0000

    adding response ok

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -8,11 +8,14 @@ class WebHookController extends Controller
 {
     /**
      * @param $siteHashID
+     * @return \Illuminate\Http\JsonResponse
      */
     public function deploy($siteHashID)
     {
         dispatch(new \App\Jobs\Site\DeploySite(
             Site::findOrFail(\Hashids::decode($siteHashID)[0])
         ));
+
+        return response()->json('OK');
     }
 }
