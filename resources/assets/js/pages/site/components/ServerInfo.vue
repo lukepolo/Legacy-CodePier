b6d08ff30d4fd20bee587cebb81c2395950de5bf
Luke Policinski <luke@lukepolo.com>
2016-12-13 23:22:12 +0000

    server stats has defaults

--- resources/assets/js/pages/site/components/ServerInfo.vue
+++ resources/assets/js/pages/site/components/ServerInfo.vue
@@ -19,44 +19,40 @@
                 </section>
             </template>
 
-            <template v-if="server.stats">
-                <h4>Disk Usage</h4>
-                <template v-if="server.stats.disk_usage">
-                    <p v-for="(stats, disk) in server.stats.disk_usage">
-                        {{ disk }} : {{ stats.used }} / {{ stats.available }} ({{ stats.percent }})
-                    </p>
-                </template>
-                <template v-else>
-                    N/A
-                </template>
+            <h4>Disk Usage</h4>
+            <template v-if="server.stats.disk_usage">
+                <p v-for="(stats, disk) in server.stats.disk_usage">
+                    {{ disk }} : {{ stats.used }} / {{ stats.available }} ({{ stats.percent }})
+                </p>
+            </template>
+            <template v-else>
+                N/A
+            </template>
 
-                <h4>Memory</h4>
-                <template v-if="server.stats.memory">
-                    <p v-for="(stats, memory_name) in server.stats.memory">
-                        {{ memory_name }} : {{ stats.used }} / {{ stats.available }}
-                    </p>
-                </template>
-                <template v-else>
-                    N/A
-                </template>
+            <h4>Memory</h4>
+            <template v-if="server.stats.memory">
+                <p v-for="(stats, memory_name) in server.stats.memory">
+                    {{ memory_name }} : {{ stats.used }} / {{ stats.available }}
+                </p>
+            </template>
+            <template v-else>
+                N/A
+            </template>
 
-                <h4>CPU Load</h4>
-                <template v-if="server.stats.loads">
-                    <p>1 / 5 / 10 minutes ago</p>
-                    <template v-for="(load, ago, index) in server.stats.loads">
-                        <span>
-                            {{ load }}%
-                            <template v-if="index != (Object.keys(server.stats.loads).length - 1)">
-                                /
-                            </template>
-                        </span>
-                    </template>
+            <h4>CPU Load</h4>
+            <template v-if="server.stats.loads">
+                <p>1 / 5 / 10 minutes ago</p>
+                <template v-for="(load, ago, index) in server.stats.loads">
+                    <span>
+                        {{ load }}%
+                        <template v-if="index != (Object.keys(server.stats.loads).length - 1)">
+                            /
+                        </template>
+                    </span>
                 </template>
-                <template v-else>
-                    N/A
-                </template>
-
-
+            </template>
+            <template v-else>
+                N/A
             </template>
 
 
