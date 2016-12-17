c86e7f9ac452318c59956b1564aa6490bc42df2a
Luke Policinski <Luke@LukePOLO.com>
2016-12-17 16:34:53 +0000

    Apply fixes from StyleCI

--- database/migrations/2016_12_17_152339_add_indexes.php
+++ database/migrations/2016_12_17_152339_add_indexes.php
@@ -8,40 +8,40 @@ class AddIndexes extends Migration
 {
     const TABLES = [
         'auth_codes' => [
-            'user_id'
+            'user_id',
         ],
         'bitts' => [
-            'user_id'
+            'user_id',
         ],
         'commands' => [
             'site_id',
             'server_id',
-            ['commandable_id', 'commandable_type']
+            ['commandable_id', 'commandable_type'],
         ],
         'deployment_events' => [
             'deployment_step_id',
-            'site_server_deployment_id'
+            'site_server_deployment_id',
         ],
         'deployment_steps' => [
             'site_id',
         ],
         'notification_providers' => [
-            'provider_name'
+            'provider_name',
         ],
         'pile_team' => [
             'pile_id',
             'team_id',
         ],
         'piles' => [
-            'user_id'
+            'user_id',
         ],
         'repository_providers' => [
-            'provider_name'
+            'provider_name',
         ],
         'server_commands' => [
             'command_id',
             'server_id',
-            ['failed', 'completed']
+            ['failed', 'completed'],
         ],
         'server_cron_jobs' => [
             'server_id',
@@ -51,7 +51,7 @@ class AddIndexes extends Migration
         'server_firewall_rules' => [
             'server_id',
             'site_firewall_rule_id',
-            ['port', 'from_ip']
+            ['port', 'from_ip'],
         ],
         'server_network_rules' => [
             'server_id',
@@ -66,14 +66,14 @@ class AddIndexes extends Migration
             'server_provider_id',
         ],
         'server_providers' => [
-            'provider_name'
+            'provider_name',
         ],
         'server_provision_steps' => [
-            'server_id'
+            'server_id',
         ],
         'server_site' => [
             'server_id',
-            'site_id'
+            'site_id',
         ],
         'server_ssh_keys' => [
             'site_ssh_key_id',
@@ -81,14 +81,14 @@ class AddIndexes extends Migration
         ],
         'server_ssl_certificates' => [
             'site_ssl_certificate_id',
-            ['type', 'domains']
+            ['type', 'domains'],
         ],
         'servers' => [
             'user_id',
             ['user_id', 'pile_id'],
             'pile_id',
             'team_id',
-            ['team_id', 'pile_id']
+            ['team_id', 'pile_id'],
         ],
         'site_cron_jobs' => [
             'site_id',
@@ -102,11 +102,11 @@ class AddIndexes extends Migration
         ],
         'site_firewall_rules' => [
             'site_id',
-            ['port', 'from_ip']
+            ['port', 'from_ip'],
         ],
         'site_server_deployments' => [
             'server_id',
-            'site_deployment_id'
+            'site_deployment_id',
         ],
         'site_ssh_keys' => [
             'site_id',
