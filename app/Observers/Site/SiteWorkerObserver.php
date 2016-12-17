b2c4b71cc753ed406165bcf32913487514f41fde
Luke Policinski <luke@lukepolo.com>
2016-12-17 16:29:23 +0000

    adding indexes

--- app/Observers/Site/SiteWorkerObserver.php
+++ app/Observers/Site/SiteWorkerObserver.php
@@ -16,32 +16,21 @@ class SiteWorkerObserver
     public function created(SiteWorker $siteWorker)
     {
         foreach ($siteWorker->site->provisionedServers as $server) {
-            if (! ServerWorker::where('server_id', $server->id)
-                ->where('user', $siteWorker->user)
-                ->where('command', $siteWorker->command)
-                ->where('auto_start', $siteWorker->auto_start)
-                ->where('auto_restart', $siteWorker->auto_restart)
-                ->where('number_of_workers', $siteWorker->number_of_workers)
-                ->count()
-            ) {
-                $serverWorker = new ServerWorker([
-                    'server_id' => $server->id,
-                    'user' => $siteWorker->user,
-                    'command' => $siteWorker->command,
-                    'site_worker_id' => $siteWorker->id,
-                    'auto_start' => $siteWorker->auto_start,
-                    'auto_restart' => $siteWorker->auto_restart,
-                    'number_of_workers' => $siteWorker->number_of_workers,
-                ]);
+            $serverWorker = new ServerWorker([
+                'server_id' => $server->id,
+                'user' => $siteWorker->user,
+                'command' => $siteWorker->command,
+                'site_worker_id' => $siteWorker->id,
+                'auto_start' => $siteWorker->auto_start,
+                'auto_restart' => $siteWorker->auto_restart,
+                'number_of_workers' => $siteWorker->number_of_workers,
+            ]);
 
-                $serverWorker->addHidden([
-                    'command' => $this->makeCommand($siteWorker),
-                ]);
+            $serverWorker->addHidden([
+                'command' => $this->makeCommand($siteWorker),
+            ]);
 
-                $serverWorker->save();
-            } else {
-                $siteWorker->delete();
-            }
+            $serverWorker->save();
         }
     }
 
