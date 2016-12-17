ba0a55d7e62f2c45541f23ec0e0b341c58989bf3
Luke Policinski <Luke@LukePOLO.com>
2016-12-17 11:29:52 -0500

    Apply fixes from StyleCI (#252)

--- database/migrations/2016_12_17_152339_add_indexes.php
+++ database/migrations/2016_12_17_152339_add_indexes.php
@@ -1,47 +1,47 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class AddIndexes extends Migration
 {
-    CONST TABLES = [
+    const TABLES = [
         'auth_codes' => [
-            'user_id'
+            'user_id',
         ],
         'bitts' => [
-            'user_id'
+            'user_id',
         ],
         'commands' => [
             'site_id',
             'server_id',
-            ['commandable_id', 'commandable_type']
+            ['commandable_id', 'commandable_type'],
         ],
         'deployment_events' => [
             'deployment_step_id',
-            'site_server_deployment_id'
+            'site_server_deployment_id',
         ],
         'deployment_steps' => [
             'site_id',
         ],
         'notification_providers' => [
-            'provider_name'
+            'provider_name',
         ],
         'pile_team' => [
             'pile_id',
             'team_id',
         ],
         'piles' => [
-            'user_id'
+            'user_id',
         ],
         'repository_providers' => [
-            'provider_name'
+            'provider_name',
         ],
         'server_commands' => [
             'command_id',
             'server_id',
-            ['failed', 'completed']
+            ['failed', 'completed'],
         ],
         'server_cron_jobs' => [
             'server_id',
@@ -51,7 +51,7 @@ class AddIndexes extends Migration
         'server_firewall_rules' => [
             'server_id',
             'site_firewall_rule_id',
-            ['port', 'from_ip']
+            ['port', 'from_ip'],
         ],
         'server_network_rules' => [
             'server_id',
@@ -66,14 +66,14 @@ class AddIndexes extends Migration
             'server_provider_id',
         ],
         'server_providers' => [
-            'provider_name'
+            'provider_name',
         ],
         'server_provision_steps' => [
-            'server_id'
+            'server_id',
         ],
         'server_site' => [
             'server_id',
-            'site_id'
+            'site_id',
         ],
         'server_ssh_keys' => [
             'site_ssh_key_id',
@@ -81,14 +81,14 @@ class AddIndexes extends Migration
         ],
         'server_ssl_certificates' => [
             'site_ssl_certificate_id',
-            ['type', 'domains']
+            ['type', 'domains'],
         ],
         'servers' => [
             'user_id',
             ['user_id', 'pile_id'],
             'pile_id',
             'team_id',
-            ['team_id', 'pile_id']
+            ['team_id', 'pile_id'],
         ],
         'site_cron_jobs' => [
             'site_id',
@@ -102,49 +102,49 @@ class AddIndexes extends Migration
         ],
         'site_firewall_rules' => [
             'site_id',
-            ['port', 'from_ip']
+            ['port', 'from_ip'],
         ],
         'site_server_deployments' => [
             'server_id',
-            'site_deployment_id'
+            'site_deployment_id',
         ],
         'site_slack_channels' => [
-            'site_id'
+            'site_id',
         ],
         'site_ssh_keys' => [
             'site_id',
         ],
         'site_ssl_certificates' => [
             'site_id',
-            ['type', 'domains']
+            ['type', 'domains'],
         ],
         'site_workers' => [
-            'site_id'
+            'site_id',
         ],
         'sites' => [
             'user_id',
             'pile_id',
-            ['user_id', 'pile_id']
+            ['user_id', 'pile_id'],
         ],
         'slack_channels' => [
             'slackable_id',
-            'slackable_type'
+            'slackable_type',
         ],
         'subscriptions' => [
-            'user_id'
+            'user_id',
         ],
         'taggables' => [
             'taggable_id',
-            'taggable_type'
+            'taggable_type',
         ],
         'teams' => [
-            'owner_id'
+            'owner_id',
         ],
         'user_login_providers' => [
-            ['provider', 'provider_id']
+            ['provider', 'provider_id'],
         ],
         'user_notification_settings' => [
-            'user_id'
+            'user_id',
         ],
         'user_ssh_keys' => [
             'user_id',
@@ -158,14 +158,13 @@ class AddIndexes extends Migration
      */
     public function up()
     {
-        foreach(self::TABLES as $table =>  $indexes) {
-            Schema::table($table, function (Blueprint $tableModifying)  use($indexes) {
-                foreach($indexes as $index) {
+        foreach (self::TABLES as $table =>  $indexes) {
+            Schema::table($table, function (Blueprint $tableModifying) use ($indexes) {
+                foreach ($indexes as $index) {
                     $tableModifying->index($index);
                 }
             });
         }
-
 
         Schema::table('users', function (Blueprint $tableModifying) {
             $tableModifying->dropIndex('users_email_unique');
@@ -181,10 +180,10 @@ class AddIndexes extends Migration
      */
     public function down()
     {
-        foreach(self::TABLES as $table =>  $indexes) {
-            Schema::table($table, function (Blueprint $tableModifying)  use($table, $indexes) {
-                foreach($indexes as $index) {
-                    if(is_array($index)) {
+        foreach (self::TABLES as $table =>  $indexes) {
+            Schema::table($table, function (Blueprint $tableModifying) use ($table, $indexes) {
+                foreach ($indexes as $index) {
+                    if (is_array($index)) {
                         $tableModifying->dropIndex($index);
                         continue;
                     }
