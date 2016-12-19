f1e6652f8d2e067e7cff898d8fa5e9d02b4b710e
Luke Policinski <luke@lukepolo.com>
2016-12-19 21:59:19 +0000

    mergeing

--- resources/assets/js/pages/site/components/ServerInfo.vue
+++ resources/assets/js/pages/site/components/ServerInfo.vue
@@ -19,44 +19,7 @@
                 </section>
             </template>
 
-            <template v-if="server.progress >= 100">
-                <h4>Disk Usage</h4>
-                <template v-if="server.stats && server.stats.disk_usage">
-                    <p v-for="(stats, disk) in server.stats.disk_usage">
-                        {{ disk }} : {{ stats.used }} / {{ stats.available }} ({{ stats.percent }})
-                    </p>
-                </template>
-                <template v-else>
-                    N/A
-                </template>
 
-                <h4>Memory</h4>
-                <template v-if="server.stats && server.stats.memory">
-                    <p v-for="(stats, memory_name) in server.stats.memory">
-                        {{ memory_name }} : {{ stats.used }} / {{ stats.total }}
-                    </p>
-                </template>
-                <template v-else>
-                    N/A
-                </template>
-
-                <h4>CPU Load</h4>
-                <template v-if="server.stats && server.stats.loads">
-                    <p>1 / 5 / 10 mins for {{ server.stats.cpus }} CPUS</p>
-
-                    <template v-for="(load, ago, index) in server.stats.loads">
-                    <span>
-                        {{ load }}%
-                        <template v-if="index != (Object.keys(server.stats.loads).length - 1)">
-                            /
-                        </template>
-                    </span>
-                    </template>
-                </template>
-                <template v-else>
-                    N/A
-                </template>
-            </template>
 
             <div class="dropdown">
                 <button class="btn btn-default btn-xs dropdown-toggle" type="button" data-toggle="dropdown">
