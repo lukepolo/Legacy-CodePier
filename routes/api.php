94d7ceea60c22401b7052a1fc3210e7a68b65b18
Luke Policinski <luke@lukepolo.com>
2016-12-20 01:42:23 +0000

    removing api call , no need since we will update it when their repository is first created

--- routes/api.php
+++ routes/api.php
@@ -159,7 +159,6 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::get('server/languages', 'Server\ServerFeatureController@getLanguages');
     Route::get('server/frameworks', 'Server\ServerFeatureController@getFrameworks');
     Route::get('server/{server}/editable-files', 'Server\ServerFeatureController@getEditableFiles');
-    Route::get('site/{site}/suggested-features', 'Site\SiteFeatureController@getSuggestedFeatures');
     Route::get('site/{site}/framework/editable-files', 'Site\SiteFeatureController@getEditableFrameworkFiles');
 
     Route::group(['prefix' => 'auth'], function () {
