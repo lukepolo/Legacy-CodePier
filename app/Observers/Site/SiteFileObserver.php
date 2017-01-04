3ea3de58c020a83f016e2c5e583b9da19f2c551d
Luke Policinski <Luke@LukePOLO.com>
2017-01-03 21:54:21 -0500

    Apply fixes from StyleCI (#315)

--- app/Observers/Site/SiteFileObserver.php
+++ app/Observers/Site/SiteFileObserver.php
@@ -4,10 +4,8 @@ namespace App\Observers\Site;
 
 use App\Models\File;
 use App\Models\Site\Site;
-use App\Models\Site\SiteFile;
 use App\Traits\ModelCommandTrait;
 use App\Jobs\Server\UpdateServerFile;
-use Illuminate\Database\Eloquent\Model;
 
 class SiteFileObserver
 {
@@ -15,7 +13,7 @@ class SiteFileObserver
 
     public function created(File $model)
     {
-//        dd('created');
+        //        dd('created');
 //        foreach ($siteFile->site->provisionedServers as $server) {
 //            dispatch(
 //                (new UpdateServerFile($server, $siteFile))->onQueue(env('SERVER_COMMAND_QUEUE'))
@@ -30,7 +28,7 @@ class SiteFileObserver
 
     public function updated(File $model)
     {
-//        foreach ($siteFile->site->provisionedServers as $server) {
+        //        foreach ($siteFile->site->provisionedServers as $server) {
 //            dispatch(
 //                (new UpdateServerFile($server, $siteFile))->onQueue(env('SERVER_COMMAND_QUEUE'))
 //            );
