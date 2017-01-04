247404f97f7540eb0111546daeef394bd6891c9a
Luke Policinski <luke@lukepolo.com>
2017-01-04 22:48:15 +0000

    fixing duplicates

--- app/Http/Controllers/Site/SiteFirewallRuleController.php
+++ app/Http/Controllers/Site/SiteFirewallRuleController.php
@@ -4,11 +4,9 @@ namespace App\Http\Controllers\Site;
 
 use App\Events\SiteFirewallRuleCreated;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Site\SiteFirewallRuleRequest;
 use App\Models\FirewallRule;
 use App\Models\Site\Site;
-use App\Models\FirewallRule;
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Site\SiteFirewallRuleRequest;
 
 class SiteFirewallRuleController extends Controller
 {
