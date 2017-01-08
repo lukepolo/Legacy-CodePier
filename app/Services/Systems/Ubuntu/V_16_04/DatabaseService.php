e0d0a2eacda12a45aee9f5cdfd531f053967cf73
Luke Policinski <luke@lukepolo.com>
2017-01-08 18:42:04 +0000

    mongo on startup

--- app/Services/Systems/Ubuntu/V_16_04/DatabaseService.php
+++ app/Services/Systems/Ubuntu/V_16_04/DatabaseService.php
@@ -130,6 +130,7 @@ class DatabaseService
         $this->remoteTaskService->run('echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list');
         $this->remoteTaskService->run('apt-get update');
         $this->remoteTaskService->run('apt-get install -y mongodb-org php-mongodb ');
+        $this->remoteTaskService->run('systemctl enable mongod.service');
         $this->remoteTaskService->run('service mongod start');
 
         $this->addToServiceRestartGroup(SystemService::WEB_SERVICE_GROUP, 'service mongod restart');
