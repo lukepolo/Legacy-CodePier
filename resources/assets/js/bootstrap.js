4f44291c7fba5aef40c120a918084be7fbaded06
Luke Policinski <Luke@LukePOLO.com>
2017-01-13 20:53:20 -0500

    Mix mostly working putting final touches on

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,27 +1,23 @@
+window.laroute = require('./laroute')
+window.$ = window.jQuery = require('jquery')
 window._ = require('lodash')
-
-/**
- * Vue is a modern JavaScript for building interactive web interfaces using
- * reacting data binding and reusable components. Vue's API is clean and
- * simple, leaving you to focus only on building your next great idea.
- */
-
 window.Vue = require('vue')
-
-if (Laravel.env == 'production') {
-  Vue.config.devtools = false
-  Vue.config.silent = true
-}
-
 window.VueRouter = require('vue-router')
-require('vue-resource')
+
+window.ace = require('brace')
+require('brace/mode/javascript')
+require('brace/theme/monokai')
+
+require('bootstrap')
+require('jcf-forms')
+require('moment-timezone')
 
 /**
  * We'll register a HTTP interceptor to attach the "CSRF" header to each of
  * the outgoing requests issued by this application. The CSRF middleware
  * included with Laravel will automatically verify the header's value.
  */
-
+require('vue-resource')
 Vue.use(VueRouter)
 
 import NProgress from 'nprogress'
