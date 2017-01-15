54262a0ed92f697420a25831b7842e206e8e2c6f
Janice Falotico <jfalotic@gmail.com>
2017-01-14 19:17:56 -0500

    new sub nav design

--- resources/assets/js/pages/site/components/SiteNav.vue
+++ resources/assets/js/pages/site/components/SiteNav.vue
@@ -1,36 +1,15 @@
 <template>
     <ul class="wizard" v-if="site">
-
         <router-link :to="{ name : 'site_repository', params : { site_id : site.id } }" tag="li" class="wizard-item">
-            <a>Repository</a>
-        </router-link>
-
-        <router-link :to="{ name : 'site_deployment', params : { site_id : site.id } }" tag="li" class="wizard-item">
-            <a>Deployment</a>
-        </router-link>
-
-        <router-link :to="{ name : 'site_files', params : { site_id : site.id } }" tag="li" class="wizard-item">
-            <a>Files</a>
-        </router-link>
-
-        <router-link :to="{ name : 'site_workers', params : { site_id : site.id } }" tag="li" class="wizard-item">
-            <a>Workers</a>
+            <a>Site Setup</a>
         </router-link>
 
         <router-link :to="{ name : 'site_ssl_certs', params : { site_id : site.id } }" tag="li" class="wizard-item" v-if="site.domain != 'default' ">
-            <a>SSLCertificates</a>
+            <a>Security</a>
         </router-link>
 
-        <router-link :to="{ name : 'site_firewall_rules', params : { site_id : site.id } }" tag="li" class="wizard-item">
-            <a>Site Firewall Rules</a>
-        </router-link>
-
-        <router-link :to="{ name : 'site_ssh_keys', params : { site_id : site.id } }" tag="li" class="wizard-item">
-            <a>Site Ssh Keys</a>
-        </router-link>
-
-        <router-link :to="{ name : 'site_server_features', params : { site_id : site.id } }" tag="li" class="wizard-item" v-if="site.repository">
-            <a>Server Features</a>
+        <router-link :to="{ name : 'site_workers', params : { site_id : site.id } }" tag="li" class="wizard-item">
+            <a>Server Setup</a>
         </router-link>
     </ul>
 </template>
---
+++ resources/assets/js/pages/site/components/sub-navs/SiteSetup.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-if="siteId">
+        <div class="tab-container tab-left">
+            <ul class="nav nav-tabs">
+                <router-link :to="{ name : 'site_repository', params : { site_id : siteId } }" tag="li">
+                    <a>Repository</a>
+                </router-link>
+
+                <router-link :to="{ name : 'site_deployment', params : { site_id : siteId } }" tag="li">
+                    <a>Deployment</a>
+                </router-link>
+
+                <router-link :to="{ name : 'site_files', params : { site_id : siteId } }" tag="li">
+                    <a>Files</a>
+                </router-link>
+            </ul>
+            <div class="tab-content">
+                <div role="tabpanel" class="tab-pane active">
+                    <slot></slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+        computed : {
+            siteId() {
+                return this.$route.params.site_id
+            }
+        }
+    }
+</script>
