fc48d5c40036fc4f10a8fe383e3be174483ae307
Luke Policinski <Luke@LukePOLO.com>
2017-01-17 21:56:49 -0500

    Apply fixes from StyleCI (#411)

--- database/seeds/ServerProvidersSeeder.php
+++ database/seeds/ServerProvidersSeeder.php
@@ -28,7 +28,7 @@ class ServerProvidersSeeder extends Seeder
                 'name'     => 'Custom Provider', // if changed , go to vue user server providers page
                 'features' => [],
                 'class' => \App\Services\Server\Providers\CustomProvider::class,
-            ]
+            ],
         ];
 
         foreach ($providers as $provider => $data) {
