a9937c1943be19f144d90065ee55fdd853032a2b
Luke Policinski <Luke@LukePOLO.com>
2017-01-21 08:50:57 -0500

    fixing issues with login routes

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,18 @@
 |
 */
 
-Route::auth();
-$this->get('login', 'Auth\LoginController@showLoginForm')->name('login')->middleware('auth_code');
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login')->middleware('auth_code');
 
 /*
 |--------------------------------------------------------------------------
