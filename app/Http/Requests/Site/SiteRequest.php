3247f57533404322aa47a4dacdb0d9aaa659f64d
Luke Policinski <Luke@LukePOLO.com>
2017-01-26 13:52:44 -0500

    fixing regex

--- app/Http/Requests/Site/SiteRequest.php
+++ app/Http/Requests/Site/SiteRequest.php
@@ -24,7 +24,7 @@ class SiteRequest extends FormRequest
     public function rules()
     {
         return [
-            'domain' => 'required_unless:domainless,true|url',
+            'domain' => 'required_unless:domainless,true|domain',
             'pile_id' => 'required|integer',
         ];
     }
