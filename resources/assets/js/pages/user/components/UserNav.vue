8ce93cec9b21269f35a701b89110c3c10d707a54
Luke Policinski <Luke@LukePOLO.com>
2017-02-04 15:09:13 -0500

    cleaned up users pages

--- resources/assets/js/pages/user/components/UserNav.vue
+++ resources/assets/js/pages/user/components/UserNav.vue
@@ -2,30 +2,49 @@
     <ul class="nav nav-tabs">
 
         <router-link tag="li" class="wizard-item" :to="{ name : 'my_account' }" exact>
-            <a>My Account</a>
+            <a>
+                My Account
+                <div class="small">Basic personal information</div>
+            </a>
         </router-link>
 
         <router-link tag="li" class="wizard-item" :to="{ name : 'subscription' }">
-            <a>Subscription</a>
+            <a>
+                Subscription
+                <div class="small">Sadly we require a subscription, but there are options for everyone!</div>
+            </a>
         </router-link>
         <!--<router-link tag="li" class="wizard-item" to="/my-profile/oauth">-->
         <!--<a>OAuth</a>-->
         <!--</router-link>-->
         <router-link tag="li" class="wizard-item" :to="{ name : 'user_ssh_keys' }">
-            <a>SSH Keys</a>
+            <a>
+                SSH Keys
+                <div class="small">Global SSH keys to access servers connected to this account</div>
+            </a>
         </router-link>
 
         <router-link tag="li" class="wizard-item" :to="{ name : 'user_server_providers' }">
-            <a>Server Providers</a>
+            <a>
+                Server Providers
+                <div class="small">Connect to your existing server providers : Digital Ocean</div>
+            </a>
         </router-link>
 
         <router-link tag="li" class="wizard-item" :to="{ name : 'user_repository_providers' }">
-            <a>Repository Providers</a>
+            <a>
+                Repository Providers
+                <div class="small">We need basic repository access to allow deployments on your servers</div>
+            </a>
         </router-link>
 
         <router-link tag="li" class="wizard-item" :to="{ name : 'user_notification_providers' }">
-            <a>Notification Settings</a>
+            <a>
+                Notification Settings
+                <div class="small">Manage how we notify you about failed deployments, provisions and much more</div>
+            </a>
         </router-link>
+
     </ul>
 </template>
 <script>
