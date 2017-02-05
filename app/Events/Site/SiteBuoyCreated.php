f2ea619f9cf66a3a2b56c97ea284be05079185be
Luke Policinski <Luke@LukePOLO.com>
2017-02-05 13:30:44 -0500

    Apply fixes from StyleCI (#525)
    [ci skip] [skip ci]

--- app/Events/Site/SiteBuoyCreated.php
+++ app/Events/Site/SiteBuoyCreated.php
@@ -2,10 +2,10 @@
 
 namespace App\Events\Site;
 
-use App\Jobs\Server\Buoys\InstallBuoy;
 use App\Models\Buoy;
 use App\Models\Site\Site;
 use App\Traits\ModelCommandTrait;
+use App\Jobs\Server\Buoys\InstallBuoy;
 use Illuminate\Queue\SerializesModels;
 
 class SiteBuoyCreated
