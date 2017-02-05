0e22ca300954206496d79a551d4f6d81b6065f3a
Luke Policinski <Luke@LukePOLO.com>
2017-02-05 15:42:54 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- database/migrations/2017_02_03_224224_create_schemas_table.php
+++ database/migrations/2017_02_03_224224_create_schemas_table.php
@@ -13,7 +13,7 @@ class CreateSchemasTable extends Migration
      */
     public function up()
     {
-//        Schema::create('schemas', function (Blueprint $table) {
+        //        Schema::create('schemas', function (Blueprint $table) {
 //            $table->increments('id');
 //            $table->string('name');
 //            $table->string('database');
