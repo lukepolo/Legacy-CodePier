c87f053ba6d0bba950f5ba05379caf7e13a87603
Luke Policinski <Luke@LukePOLO.com>
2017-02-06 19:36:16 -0500

    deployments much better, still needs a bit of a clean up

--- resources/assets/js/pages/site/components/DeploymentStepCard.vue
+++ resources/assets/js/pages/site/components/DeploymentStepCard.vue
@@ -1,46 +1,58 @@
 <template>
   <div>
-      <template v-if="deploymentStep.internal_deployment_function">
-          <!-- todo - allow them to delete custom steps
-           -- Note: this should no show up unless it is custom -->
-          <!-- todo jf -- switch out icon -->
-          <!--<a class="text-error pull-right"><span class="icon-cancel"></span></a>-->
+
+      <template v-if="deploymentStep.internal_deployment_function || deploymentStep.editing == false">
+
+          <template v-if="!deploymentStep.internal_deployment_function">
+              <a class="text-error pull-right" @click="deleteStep"><span class="icon-cancel"></span></a>
+              <a class="text-success pull-right" @click="edit"><span class="fa fa-edit"></span></a>
+          </template>
 
           <div class="drag-name">
               {{ deploymentStep.step }}
           </div>
+
           <div class="small">{{ deploymentStep.description }}</div>
+
       </template>
+
       <template v-else>
+
           <div class="jcf-form-wrap">
+
               <form @submit.prevent class="floating-labels">
+
                   <div class="jcf-input-group">
                       <input type="text" name="step" v-model="step">
                       <label for="step">
                           <span class="float-label">Step Name</span>
                       </label>
                   </div>
+
                   <div class="jcf-input-group">
                       <input type="text" name="script" v-model="script">
                       <label for="script">
                           <span class="float-label">Script</span>
                       </label>
                   </div>
+
                   <div class="jcf-input-group">
-                      <div class="input-question">Description</div>
-                      <textarea rows="2"></textarea>
+                      <div class="input-question" >Description</div>
+                      <textarea rows="2" v-model="description"></textarea>
                   </div>
 
                   <div class="btn-footer">
-                      <!-- todo - have save button get it out of edit mode and into a component like the rest -->
-                      <!-- todo - cancel should clear form and close -->
-                      <a class="btn btn-small">Cancel</a>
-                      <a class="btn btn-primary btn-small">Save</a>
+                      <a class="btn btn-small" @click="cancel">Cancel</a>
+                      <a class="btn btn-primary btn-small" @click="save">Save</a>
                   </div>
               </form>
+
           </div>
+
       </template>
+
   </div>
+
 </template>
 
 <script>
@@ -49,7 +61,8 @@
         data() {
             return {
                 step : this.deploymentStep.step,
-                script : this.deploymentStep.script
+                script : this.deploymentStep.script,
+                description : this.deploymentStep.description
             }
         },
         watch : {
@@ -58,6 +71,30 @@
             },
             'script' : function() {
                 this.deploymentStep.script = this.script;
+            }
+        },
+        methods: {
+            edit() {
+                this.deploymentStep.editing = true
+                this.updateStep()
+            },
+            save() {
+                this.deploymentStep.editing = false
+                this.deploymentStep.step = this.step
+                this.deploymentStep.script = this.script
+                this.deploymentStep.description = this.description
+                this.updateStep()
+            },
+            cancel() {
+                this.deploymentStep.editing = false
+                this.updateStep()
+            },
+            updateStep() {
+
+                this.$emit('updateStep', this.deploymentStep)
+            },
+            deleteStep() {
+                this.$emit('deleteStep')
             }
         }
     }
