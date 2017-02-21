dd5af2342a0f1934aa8056f2e8ca0a80662ad777
Luke Policinski <Luke@LukePOLO.com>
2017-02-21 11:53:56 -0500

    user registration now works

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,8 @@
 |
 */
 
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset');
-
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login')->middleware('auth_code');
-
+Auth::routes();
 /*
 |--------------------------------------------------------------------------
 | OAuth Routes
