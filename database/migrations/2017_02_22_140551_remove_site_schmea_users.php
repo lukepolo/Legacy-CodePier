a80abd0902e6cad7da0fe8d18d4bd2015b5b856f
Luke Policinski <Luke@LukePOLO.com>
2017-02-22 09:07:01 -0500

    remove schema users, i dont see a good use for it

---
+++ database/migrations/2017_02_22_140551_remove_site_schmea_users.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveSiteSchmeaUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('schema_users');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
