e63e18b423336d8da8593538825091ed7027ffd5
Luke Policinski <Luke@LukePOLO.com>
2017-02-22 09:07:01 -0500

    remove schema users, i dont see a good use for it

--- resources/assets/js/pages/site/components/DatabaseSection.vue
+++ resources/assets/js/pages/site/components/DatabaseSection.vue
@@ -4,7 +4,7 @@
             <h1>{{ database }}</h1>
 
             <div class="jcf-form-wrap">
-                <form @submit.prevent="createSchema(database)" class="floating-labels">
+                <form @submit.prevent="createSchema" class="floating-labels">
                     <h3>Schemas :</h3>
                     <div class="jcf-input-group">
                         <input type="text" name="name" v-model="schemaForm.name">
@@ -35,46 +35,6 @@
                 </tbody>
             </table>
 
-            <div class="jcf-form-wrap">
-                <form @submit.prevent="createUser(database)" class="floating-labels">
-                    <h3>Users :</h3>
-                    <div class="jcf-input-group">
-                        <input type="text" name="user" v-model="userForm.user">
-                        <label for="user">
-                            <span class="float-label">Name</span>
-                        </label>
-                    </div>
-
-                    <div class="jcf-input-group">
-                        <input type="password" name="user" v-model="userForm.password">
-                        <label for="password">
-                            <span class="float-label">Password</span>
-                        </label>
-                    </div>
-
-                    <div class="btn-footer">
-                        <button class="btn btn-primary" type="submit">Create User</button>
-                    </div>
-
-                </form>
-
-                <br>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>User</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>codepier - super user</td>
-                        </tr>
-                        <tr>
-                            <td>codepier_support - super user</td>
-                        </tr>
-                    </tbody>
-                </table>
-
             </div>
         </div>
     </section>
@@ -87,25 +47,11 @@
             return {
                 schemaForm : {
                     name : null
-                },
-                userForm : {
-                    name : null,
-                    schemas : [],
-                    password : null,
                 }
             }
         },
         methods : {
-            createUser(database) {
-                alert('create user for '+database);
-            },
-            updateUser(database) {
-                alert('create user for '+database);
-            },
-            deleteUser(database) {
-
-            },
-            createSchema(database) {
+            createSchema() {
                 this.$store.dispatch('createSiteSchema', {
                     database : this.database,
                     name : this.schemaForm.name,
