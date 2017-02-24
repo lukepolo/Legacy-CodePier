b810732f7869e71a36519c500bc4e03706574c15
Janice Falotico <jfalotic@gmail.com>
2017-02-24 17:03:25 -0500

    landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,5 +1,121 @@
 @extends('layouts.public')
 
 @section('content')
-    Landing
+    <section id="cover-main" class="cover">
+        <div class="cover--content">
+            <div class="cover--logo-container">
+                <img src="assets/img/codepier.svg">
+            </div>
+            <h1>You Build it. We Deploy it.</h1>
+            <p>You're here to build apps. CodePier is here to help you manage your infrastructure, allow custom provisioning for each application, and eliminate downtime with zerotime deployments, plus, so much more. Come check it out.</p>
+
+
+            <div class="jcf-form-wrap">
+                <form>
+                    <div class="jcf-input-group">
+                        <input type="email" id="email" name="email" required="">
+                        <label for="email"><span class="float-label">Enter your email to join our beta</span></label>
+                    </div>
+                </form>
+            </div>
+
+            <div class="btn-container">
+                <a href="#" class="btn btn-primary btn-large">Join our Beta</a>
+            </div>
+        </div>
+    </section>
+    <section id="section-video" class="section">
+        <div class="section--content">
+            <div class="video">
+                <iframe src="https://player.vimeo.com/video/205614363?color=48ACF0&title=0&byline=0&portrait=0" width="800" height="450" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+            </div>
+        </div>
+    </section>
+    <section id="comparison" class="section">
+        <div class="section--content">
+            <table class="table table--comparison">
+                <tr>
+                    <th class="row1"></th>
+                    <th class="row2"><img src="/assets/img/codepier.svg" style="max-width: 130px;"></th>
+                    <th class="row3">Forge</th>
+                    <th class="row4">Heroku</th>
+                    <th class="row5">Engine Yard</th>
+                </tr>
+                <tr>
+                    <td>Runtime</td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                </tr>
+                <tr>
+                    <td>Scale</td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                </tr>
+                <tr>
+                    <td>Continuous Delivery</td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                </tr>
+                <tr>
+                    <td>Zerotime Deployments</td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                </tr>
+                <tr>
+                    <td>Team Management</td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                </tr>
+                <tr>
+                    <td>Custom Servers</td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                </tr>
+
+                <tr>
+                    <td>Continuous Delivery</td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                </tr>
+                <tr>
+                    <td>Zerotime Deployments</td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                </tr>
+                <tr>
+                    <td>Team Management</td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                </tr>
+                <tr>
+                    <td>Custom Servers</td>
+                    <td><img src="/assets/img/yes.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                    <td><img src="/assets/img/no.svg"></td>
+                </tr>
+            </table>
+            <div class="text-right">
+                <p><em>* this is not a comprehensive list of features</em></p>
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -11,7 +11,7 @@
         <title>CodePier</title>
 
         <!-- Styles -->
-        <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
+        <link href="{{ elixir('css/site.css') }}" rel="stylesheet">
 
         @if(env('APP_ENV') == 'production')
             <script src="https://cdn.ravenjs.com/3.8.1/raven.min.js"></script>
@@ -21,7 +21,59 @@
         @endif
     </head>
     <body>
-        HIYA
+        <header>
+            <ul class="nav left">
+                <li>
+                    <div class="nav--logo-container">
+                        {{--<img src="assets/img/codepier.svg">--}}
+                    </div>
+                </li>
+            </ul>
+            <ul class="nav right">
+                <li><a href="#" class="nav--link">Features</a></li>
+                <li><a href="#" class="nav--link">Pricing</a></li>
+                <li><a href="#" class="nav--link">Documentation</a></li>
+                <li><a href="#" class="nav--link">FAQs</a></li>
+                <li><a href="#" class="nav--link nav--link-highlight">Login</a></li>
+            </ul>
+        </header>
+
+        <div id="main">
+            @yield('content')
+
+            <footer class="footer">
+                <div class="footer--links">
+                    <h4 class="footer--links-heading">Resources</h4>
+                    <ul class="footer--links-list">
+                        <li><a href="#">Getting Started</a></li>
+                        <li><a href="#">Documentation</a></li>
+                        <li><a href="#">FAQs</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Terms & Conditions</a></li>
+                    </ul>
+                </div>
+                <div class="footer--links">
+                    <h4 class="footer--links-heading">About</h4>
+                    <ul class="footer--links-list">
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Our Customers</a></li>
+                        <li><a href="#">Careers</a></li>
+                    </ul>
+                </div>
+                <div class="footer--links">
+                    <h4 class="footer--links-heading">Support</h4>
+                    <ul class="footer--links-list">
+                        <li><a href="https://github.com/CodePier/CodePier-Issues/issues">Report a Bug</a></li>
+                        <li><a href="mailto:codepier.io">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="footer--img">
+                    <img src="/assets/img/boats.svg">
+                </div>
+            </footer>
+        </div>
+
+
         <!-- Scripts -->
         <script src="{{ elixir('js/all.js') }}"></script>
 
