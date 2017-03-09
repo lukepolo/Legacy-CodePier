bff41573eb9ee0ea45f90c7f2f3d8a430cda82a6
Luke Policinski <Luke@LukePOLO.com>
2017-03-09 17:48:05 -0500

    adding install counts

---
+++ database/migrations/2017_03_09_224538_bitts_installs_stars.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BittsInstallsStars extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bitts', function(Blueprint $table) {
+            $table->integer('installs')->default(0);
+            $table->integer('stars')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bitts', function(Blueprint $table) {
+            $table->dropColumn('installs');
+            $table->dropColumn('stars');
+        });
+    }
+}
