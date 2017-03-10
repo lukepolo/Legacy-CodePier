a50cc29f4cb5d6de12c7539a924495f856d2de1c
Luke Policinski <Luke@LukePOLO.com>
2017-03-09 20:29:49 -0500

    more changes for setup pages

---
+++ resources/assets/js/pages/Setup/index.js
@@ -0,0 +1,6 @@
+export const SshKeys = require('./SshKeys/Form.vue')
+export const Workers = require('./Workers/Form.vue')
+export const CronJobs = require('./CronJobs/Form.vue')
+export const Databases = require('./Databases/Form.vue')
+export const FirewallRules = require('./FirewallRules/Form.vue')
+export const SslCertificates = require('./SslCertificates/Form.vue')
