731dd6fceef83c732d2887f4e9c96388429dac09
Luke Policinski <luke@lukepolo.com>
2017-03-13 14:22:36 -0400

    now we can restart services via workers

---
+++ app/Events/Site/SiteRestartServers.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Events\Site;
+
+use App\Jobs\Server\RestartServer;
+use App\Models\Site\Site;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+
+class SiteRestartServers
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Site $site
+     */
+    public function __construct(Site $site)
+    {
+        foreach ($site->provisionedServers as $server) {
+            dispatch(
+                (new RestartServer($server))->onQueue(config('queue.channels.server_commands'))
+            );
+        }
+    }
+}
