a2b57febb07f4784ddcc8e7f667d8f62fe9da44e
Luke Policinski <Luke@LukePOLO.com>
2017-03-19 15:28:51 -0400

    stopping for now 3 hours in feel like im getting closer

--- app/Services/DeploymentServices/Ruby/Ruby.php
+++ app/Services/DeploymentServices/Ruby/Ruby.php
@@ -7,6 +7,17 @@ use App\Services\DeploymentServices\Ruby\Frameworks\RubyOnRails;
 
 class Ruby
 {
+    /**
+     * @description Install the vendors packages.
+     *
+     * @order 200
+     */
+    public function installRubyDependencies()
+    {
+        $this->remoteTaskService->ssh($this->server);
+        return [$this->remoteTaskService->run('cd '.$this->release.'; bundle install --path ~/.gem/'.$this->site->domain)];
+    }
+
     use RubyOnRails;
     use DeployTrait;
 }
