c677f6e29ef663a44902e0e95b155f527a1bdf92
Luke Policinski <Luke@LukePOLO.com>
2017-04-11 13:17:46 -0400

    Apply fixes from StyleCI (#692)
    [ci skip] [skip ci]

--- app/Services/Systems/Ubuntu/V_16_04/Languages/Ruby/Ruby.php
+++ app/Services/Systems/Ubuntu/V_16_04/Languages/Ruby/Ruby.php
@@ -82,7 +82,6 @@ class Ruby
         }
 
         $this->remoteTaskService->appendTextToFile('/home/codepier/.bashrc', 'source /usr/local/rvm/scripts/rvm');
-
     }
 
     public function getNginxConfig(Site $site)
