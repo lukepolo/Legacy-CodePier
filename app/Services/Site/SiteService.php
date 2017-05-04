5fd029b6ab70b05010687ac4c265c40f7ba6d425
Luke Policinski <Luke@LukePOLO.com>
2017-05-03 23:23:37 -0400

    Apply fixes from StyleCI (#707)
    [ci skip] [skip ci]

--- app/Services/Site/SiteService.php
+++ app/Services/Site/SiteService.php
@@ -143,12 +143,9 @@ class SiteService implements SiteServiceContract
                 }
 
                 event(new DeploymentStepCompleted($site, $server, $event, $event->step, collect($deploymentStepResult)->filter()->implode("\n"), microtime(true) - $start));
-
             } catch (FailedCommand $e) {
-
                 event(new DeploymentStepFailed($site, $server, $event, $event->step, $e->getMessage()));
                 throw new DeploymentFailed($e->getMessage());
-
             }
         }
 
