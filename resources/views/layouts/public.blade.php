3253b6bf2e11785f6531b5bfe694d1c3ad3f0586
Janice Falotico <jfalotic@gmail.com>
2017-05-11 11:14:30 -0400

    transparent favicon

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -22,7 +22,7 @@
         <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon/favicon-16x16.png">
         <link rel="manifest" href="/assets/img/favicon/manifest.json">
-        <link rel="mask-icon" href="/assets/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+        <link rel="mask-icon" href="/assets/img/favicon/safari-pinned-tab.svg" color="#48acf0">
         <link rel="shortcut icon" href="/assets/img/favicon/favicon.ico">
         <meta name="msapplication-config" content="/assets/img/favicon/browserconfig.xml">
         <meta name="theme-color" content="#ffffff">
