16594810eaa50da565ede71d64213c3a2648f9ec
Luke Policinski <Luke@LukePOLO.com>
2017-05-12 04:21:32 -0400

    added favicon in

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,16 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <!-- Favicons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/assets/img/favicon/manifest.json">
+        <link rel="mask-icon" href="/assets/img/favicon/safari-pinned-tab.svg" color="#48acf0">
+        <link rel="shortcut icon" href="/assets/img/favicon/favicon.ico">
+        <meta name="msapplication-config" content="/assets/img/favicon/browserconfig.xml">
+        <meta name="theme-color" content="#ffffff">
+
         <title>CodePier</title>
 
         <!-- Styles -->
