3e474115a259d0c34aaabc169105c781d99695a9
Luke Policinski <Luke@LukePOLO.com>
2017-05-12 06:10:31 -0400

    mix is amazing

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,11 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
+mix
+    .options({
+        purifyCss: true
+    })
+    .js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .sass('resources/assets/sass/site.scss', 'public/css')
     .browserSync({
@@ -19,3 +23,25 @@ mix.js('resources/assets/js/app.js', 'public/js')
         host: 'codepier.dev',
         open: 'external'
     })
+    .extract([
+        'vue',
+        'brace',
+        'jquery',
+        'lodash',
+        'progress',
+        'jcf-forms',
+        'pusher-js',
+        'clipboard',
+        'vue-router',
+        'laravel-echo',
+        'filesize-parser',
+        'lodash-inflection',
+    ])
+    .autoload({
+        jquery: ['$', 'window.jQuery'],
+        lodash : '_',
+        clipboard : 'Clipboard',
+        moment : 'moment',
+        'pusher-js' : 'Pusher',
+    })
+    .version()
