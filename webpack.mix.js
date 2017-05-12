6ad4e9f9b11a3a1f448d02d28017132f8c1a2a66
Luke Policinski <Luke@LukePOLO.com>
2017-05-12 05:37:26 -0400

    mix is going well, need to re-write some stuff

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,8 @@ const { mix } = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .sass('resources/assets/sass/site.scss', 'public/css')
-    .browserSync('codepier.dev')
+    .browserSync({
+        proxy : 'codepier.dev',
+        host: 'codepier.dev',
+        open: 'external'
+    })
