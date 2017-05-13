93652a574e384f7206525ad72f96ff47a2435f5c
Luke Policinski <Luke@LukePOLO.com>
2017-05-12 21:57:17 -0400

    store stucture completed, now to refacotr the entire store.....

---
+++ resources/assets/js/store/modules/index.js
@@ -0,0 +1,81 @@
+import piles from './piles'
+import bitts from './bitts'
+import buoys from './buoys'
+import events from './events'
+import system from './system'
+import categories from './admin/categories'
+import notifications from './notifications'
+
+import users from './user/users'
+import user_teams from './user/teams'
+import user_ssh_keys from './user/ssh-keys'
+import user_subscriptions from './user/subscription'
+import user_notifications from './user/notifications'
+
+import sites from './site/sites'
+import site_files from './site/files'
+import site_workers from './site/workers'
+import site_schemas from './site/schemas'
+import site_ssh_keys from './site/ssh-keys'
+import site_cron_jobs from './site/cron-jobs'
+import site_firewalls from './site/firewalls'
+import site_server_features from './site/server-features'
+import site_ssl_certificates from './site/ssl-certificates'
+import site_language_settings from './site/language-settings'
+import site_environment_variables from './site/environment-varaibles'
+
+import servers from './server/servers'
+import server_buoys from './server/buoys'
+import server_files from './server/files'
+import server_schemas from './server/schemas'
+import server_workers from './server/workers'
+import server_services from './server/services'
+import server_ssh_keys from './server/ssh-keys'
+import server_cron_jobs from './server/cron-jobs'
+import server_firewalls from './server/firewalls'
+import server_providers from './server/providers'
+import server_ssl_certificates from './server/ssl-certificates'
+import server_language_settings from './server/language-settings'
+import server_environment_variables from './server/environment-varaibles'
+
+export default {
+    piles,
+    bitts,
+    buoys,
+    events,
+    system,
+    categories,
+    notifications,
+
+    users,
+    user_teams,
+    user_ssh_keys,
+    user_subscriptions,
+    user_notifications,
+
+    sites,
+    site_files,
+    site_workers,
+    site_schemas,
+    site_ssh_keys,
+    site_cron_jobs,
+    site_firewalls,
+    site_server_features,
+    site_ssl_certificates,
+    site_language_settings,
+    site_environment_variables,
+
+    servers,
+    server_buoys,
+    server_files,
+    server_schemas,
+    server_workers,
+    server_services,
+    server_ssh_keys,
+    server_cron_jobs,
+    server_firewalls,
+    server_providers,
+    server_ssl_certificates,
+    server_language_settings,
+    server_environment_variables
+}
