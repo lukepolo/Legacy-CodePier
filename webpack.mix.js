39eb04f4815ea6bf596950ff07f68306cb2b9930
Luke Policinski <Luke@LukePOLO.com>
2017-05-15 19:53:03 -0400

    fixing deployment issues along with site observer ,and mix now doenst do versioning in dev

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,17 +12,9 @@ const { mix } = require('laravel-mix');
  */
 
 mix
-    .options({
-        purifyCss: true
-    })
     .js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .sass('resources/assets/sass/public.scss', 'public/css')
-    .browserSync({
-        proxy : 'codepier.dev',
-        host: 'codepier.dev',
-        open: 'external'
-    })
     .extract([
         'vue',
         'brace',
@@ -44,4 +36,13 @@ mix
         moment : 'moment',
         'pusher-js' : 'Pusher',
     })
-    .version()
+
+if (mix.config.inProduction) {
+    mix.version()
+} else {
+    mix.browserSync({
+        proxy : 'codepier.dev',
+        host: 'codepier.dev',
+        open: 'external'
+    })
+}
