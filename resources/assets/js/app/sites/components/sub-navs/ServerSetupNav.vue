10cc5b993fe029f9329c902a5bae31f7eec7aa8c
Luke Policinski <luke@lukepolo.com>
2017-05-21 18:52:19 -0400

    some basic updates janice ill be styling soon

--- resources/assets/js/app/sites/components/sub-navs/ServerSetupNav.vue
+++ resources/assets/js/app/sites/components/sub-navs/ServerSetupNav.vue
@@ -1,66 +1,64 @@
 <template>
-    <div v-if="siteId">
-        <div class="tab-container tab-left">
-            <ul class="nav nav-tabs">
+    <div class="tab-container tab-left">
+        <ul class="nav nav-tabs">
 
-                <router-link :to="{ name : 'site_environment_variables', params : { site_id : siteId } }" tag="li" exact>
+            <router-link :to="{ name : 'site_environment_variables', params : { site_id : siteId } }" tag="li" exact>
+                <a>
+                    Environment Variables
+                    <div class="small">Add environment variables for you applications</div>
+                </a>
+            </router-link>
+
+            <router-link :to="{ name : 'site_cron_jobs', params : { site_id : siteId } }" tag="li" exact>
+                <a>
+                    Cron Jobs
+                    <div class="small">Configure cron jobs</div>
+                </a>
+            </router-link>
+
+            <router-link :to="{ name : 'site_workers', params : { site_id : siteId } }" tag="li" exact>
+                <a>
+                    Workers
+                    <div class="small">Configure workers / daemons services</div>
+                </a>
+            </router-link>
+
+            <router-link :to="{ name : 'site_server_files', params : { site_id : siteId } }" tag="li">
+                <a>
+                    Server Files
+                    <div class="small">Customize your server files to suit your app</div>
+                </a>
+            </router-link>
+
+            <router-link :to="{ name : 'site_language_settings', params : { site_id : siteId } }" tag="li">
+                <a>
+                    Language Settings
+                    <div class="small">Customize your server to handle bigger uploads etc.</div>
+                </a>
+            </router-link>
+
+            <template v-if="site && !site.repository">
+                <router-link :to="{ name : 'site_repository', params : { site_id : site.id } }" tag="li">
                     <a>
-                        Environment Variables
-                        <div class="small">Add environment variables for you applications</div>
+                        Server Features
+                        <div class="small">You must setup your site's repository first</div>
+                        <div class="small">Advanced server setup, highly customized servers</div>
                     </a>
                 </router-link>
-
-                <router-link :to="{ name : 'site_cron_jobs', params : { site_id : siteId } }" tag="li" exact>
+            </template>
+            <template v-else>
+                <router-link :to="{ name : 'site_server_features', params : { site_id : siteId } }" tag="li">
                     <a>
-                        Cron Jobs
-                        <div class="small">Configure cron jobs</div>
+                        Server Features
+                        <div class="small">Advanced server setup, highly customized servers</div>
                     </a>
                 </router-link>
+            </template>
 
-                <router-link :to="{ name : 'site_workers', params : { site_id : siteId } }" tag="li" exact>
-                    <a>
-                        Workers
-                        <div class="small">Configure workers / daemons services</div>
-                    </a>
-                </router-link>
-
-                <router-link :to="{ name : 'site_server_files', params : { site_id : siteId } }" tag="li">
-                    <a>
-                        Server Files
-                        <div class="small">Customize your server files to suit your app</div>
-                    </a>
-                </router-link>
-
-                <router-link :to="{ name : 'site_language_settings', params : { site_id : siteId } }" tag="li">
-                    <a>
-                        Language Settings
-                        <div class="small">Customize your server to handle bigger uploads etc.</div>
-                    </a>
-                </router-link>
-
-                <template v-if="site && !site.repository">
-                    <router-link :to="{ name : 'site_repository', params : { site_id : site.id } }" tag="li">
-                        <a>
-                            Server Features
-                            <div class="small">You must setup your site's repository first</div>
-                            <div class="small">Advanced server setup, highly customized servers</div>
-                        </a>
-                    </router-link>
-                </template>
-                <template v-else>
-                    <router-link :to="{ name : 'site_server_features', params : { site_id : siteId } }" tag="li">
-                        <a>
-                            Server Features
-                            <div class="small">Advanced server setup, highly customized servers</div>
-                        </a>
-                    </router-link>
-                </template>
-
-            </ul>
-            <div class="tab-content">
-                <div role="tabpanel" class="tab-pane active">
-                    <slot></slot>
-                </div>
+        </ul>
+        <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active">
+                <slot></slot>
             </div>
         </div>
     </div>
