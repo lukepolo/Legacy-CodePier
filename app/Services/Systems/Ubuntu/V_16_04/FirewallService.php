f248a709c55a65c907723e069d60ae75ab4137f4
Luke Policinski <Luke@LukePOLO.com>
2017-06-05 20:09:14 -0400

    firewall rules reverting to old school

--- app/Services/Systems/Ubuntu/V_16_04/FirewallService.php
+++ app/Services/Systems/Ubuntu/V_16_04/FirewallService.php
@@ -15,32 +15,32 @@ class FirewallService
 
         $this->remoteTaskService->run('DEBIAN_FRONTEND=noninteractive apt-get install iptables-persistent -y');
 
-        $this->remoteTaskService->writeToFile('/etc/opt/iptables', '
+        $this->remoteTaskService->writeToFile('/opt/codepier/iptables', '
     #!/bin/sh
-    iptables --wait --flush
-    iptables --wait --delete-chain
-    iptables --wait --table nat --flush
-    iptables --wait --table nat --delete-chain
-    iptables --wait --table mangle --flush
-    iptables --wait --table mangle --delete-chain
     
-    # default accept
-    iptables --wait --policy FORWARD ACCEPT
-    iptables --wait --policy OUTPUT ACCEPT
+    iptables --wait -F
+    iptables --wait -X
+    iptables --wait -t nat -F
+    iptables --wait -t nat -X
+    iptables --wait -t mangle -F
+    iptables --wait -t mangle -X
     
-    # allow loopback device (127.0.0.1)
-    iptables --wait --append INPUT --in-interface lo --jump ACCEPT
+    iptables --wait -P INPUT ACCEPT
+    iptables --wait -P FORWARD ACCEPT
+    iptables --wait -P OUTPUT ACCEPT
+    
+    iptables --wait -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+    iptables --wait -I INPUT 1 -i lo -j ACCEPT
     
     # SSH
-    iptables --wait --append INPUT --policy tcp --match tcp --dport '.$this->server->port.' --jump ACCEPT
+    iptables --wait -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
     
     # DO NOT REMOVE - Custom Rules
     
-    # default drop
-    iptables --wait --policy INPUT DROP
+    iptables --wait -P INPUT DROP
 ');
 
-        $this->remoteTaskService->run('chmod 775 /etc/opt/iptables');
+        $this->remoteTaskService->run('chmod 775 /opt/codepier/iptables');
         $this->rebuildFirewall();
     }
 
@@ -49,7 +49,7 @@ class FirewallService
         $this->connectToServer();
 
         $this->remoteTaskService->findTextAndAppend(
-            '/etc/opt/iptables',
+            '/opt/codepier/iptables',
             '# DO NOT REMOVE - Custom Rules',
             $this->getRule($firewallRule)
         );
@@ -62,7 +62,7 @@ class FirewallService
         $this->connectToServer();
 
         $this->remoteTaskService->removeLineByText(
-            '/etc/opt/iptables',
+            '/opt/codepier/iptables',
             $this->getRule($firewallRule)
         );
 
@@ -73,12 +73,12 @@ class FirewallService
     {
         if ($firewallRule->port !== '*') {
             if ($firewallRule->from_ip) {
-                $rule = "iptables --wait --append INPUT -s $firewallRule->from_ip --policy tcp --match tcp --dport $firewallRule->port --jump ACCEPT";
+                $rule = "iptables --wait -A INPUT -s $firewallRule->from_ip -p tcp -m tcp --dport $firewallRule->port -j ACCEPT";
             } else {
-                $rule = "iptables --wait --append INPUT --policy tcp --match tcp --dport $firewallRule->port --jump ACCEPT";
+                $rule = "iptables --wait -A INPUT -p tcp -m tcp --dport $firewallRule->port -j ACCEPT";
             }
         } else {
-            $rule = "iptables --wait --in-interface INPUT --policy tcp -s $firewallRule->from_ip --jump ACCEPT";
+            $rule = "iptables --wait -A INPUT -s $firewallRule->from_ip -j ACCEPT";
         }
 
         return $rule;
@@ -88,7 +88,7 @@ class FirewallService
     {
         $this->connectToServer();
 
-        $this->remoteTaskService->run('/etc/opt/./iptables');
+        $this->remoteTaskService->run('/opt/codepier/./iptables');
         $this->remoteTaskService->run('netfilter-persistent save');
         $this->remoteTaskService->run('netfilter-persistent reload');
 
