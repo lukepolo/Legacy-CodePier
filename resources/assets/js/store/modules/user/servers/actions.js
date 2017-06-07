56cbbb86501338d5b4ed7407939a21f7fed9c717
Luke Policinski <Luke@LukePOLO.com>
2017-06-07 19:56:06 -0400

    prodjs and fixing repository

--- resources/assets/js/store/modules/user/servers/actions.js
+++ resources/assets/js/store/modules/user/servers/actions.js
@@ -83,10 +83,10 @@ export const listenTo = ({ commit, state, dispatch }, server) => {
                 })
 
                 commit('user_server_provisioning/setCurrentStep', {
-                        request : data.serverCurrentProvisioningStep
-                    }, {
-                        root: true
-                    })
+                    request: data.serverCurrentProvisioningStep
+                }, {
+                    root: true
+                })
             })
             .listen('Server\\ServerSshConnectionFailed', (data) => {
                 commit('user_servers/update', {
