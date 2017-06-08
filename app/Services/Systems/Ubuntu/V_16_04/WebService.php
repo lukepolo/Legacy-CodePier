f4e9e735839aa8f72f33d34a0ce24b31c27bce0e
Luke Policinski <luke@lukepolo.com>
2017-06-08 08:24:14 -0400

    new ssl certs

--- app/Services/Systems/Ubuntu/V_16_04/WebService.php
+++ app/Services/Systems/Ubuntu/V_16_04/WebService.php
@@ -118,6 +118,7 @@ upstream '.$upstreamName.' {
             })->implode('ip', "\n").'
 }');
 
+            $strictTransport = '';
             $location = '
 location / {
     include proxy_params;
@@ -125,6 +126,7 @@ location / {
 }
 ';
         } else {
+            $strictTransport = 'add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;';
             $location = 'root /home/codepier/'.$site->domain.($site->zerotime_deployment ? '/current' : null).'/'.$site->web_directory.';';
         }
 
@@ -136,6 +138,8 @@ location / {
             $activeSsl = $site->activeSsl();
 
             $this->remoteTaskService->writeToFile(self::NGINX_SERVER_FILES.'/'.$site->domain.'/server/listen', '
+            
+gzip off; 
 server_name '.($site->wildcard_domain ? '.' : '').$site->domain.';
 listen 443 ssl http2 '.($site->domain == 'default' ? 'default_server' : null).';
 listen [::]:443 ssl http2 '.($site->domain == 'default' ? 'default_server' : null).';
@@ -145,15 +149,28 @@ listen [::]:443 ssl http2 '.($site->domain == 'default' ? 'default_server' : nul
 ssl_certificate_key '.ServerService::SSL_FILES.'/'.$activeSsl->id.'/server.key;
 ssl_certificate '.ServerService::SSL_FILES.'/'.$activeSsl->id.'/server.crt;
 
-ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-ssl_prefer_server_ciphers on;
+ssl_protocols TLSv1.2;
+
 ssl_dhparam /etc/nginx/dhparam.pem;
-ssl_ciphers \'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA\';
-ssl_session_timeout 1d;
-ssl_session_cache shared:SSL:50m;
+ssl_ecdh_curve secp384r1;
+
+ssl_prefer_server_ciphers on;
+ssl_ciphers ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256;
+
+'.$strictTransport.'
+
+ssl_session_cache shared:SSL:10m;
+ssl_session_timeout 10m;
+ssl_prefer_server_ciphers on;
+
 ssl_stapling on;
 ssl_stapling_verify on;
-add_header Strict-Transport-Security max-age=15768000;
+resolver 8.8.8.8 8.8.4.4 valid=300s;
+resolver_timeout 10s;
+
+add_header X-Frame-Options DENY;
+add_header X-Content-Type-Options nosniff;
+
 ');
 
             $this->remoteTaskService->writeToFile(self::NGINX_SERVER_FILES.'/'.$site->domain.'/before/ssl_redirect.conf', '
