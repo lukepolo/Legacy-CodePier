9bd0d0ff58b920f1ad6426f9d0a4fff918f47a56
Luke Policinski <Luke@LukePOLO.com>
2017-06-11 02:21:59 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -58,7 +58,7 @@ class SiteController extends Controller
     {
         $isDomain = false;
 
-        if(preg_match('/^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$/', $request->get('domain')) > 0) {
+        if (preg_match('/^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$/', $request->get('domain')) > 0) {
             $isDomain = true;
         }
 
