2b0144306c789ec3a043b3da89c244abeb6adf11
Luke Policinski <Luke@LukePOLO.com>
2017-06-10 21:57:07 -0400

    check if we need to copy over the example

--- app/Services/DeploymentServices/PHP/Frameworks/Laravel.php
+++ app/Services/DeploymentServices/PHP/Frameworks/Laravel.php
@@ -13,6 +13,11 @@ trait Laravel
      */
     public function laravelCreateSymbolicEnv()
     {
+        if ($this->remoteTaskService->isFileEmpty($this->siteFolder.'/.env') && $this->remoteTaskService->hasFile($this->release.'/.env.example')) {
+            $this->remoteTaskService->run('cp '.$this->release.'/.env.example '.$this->release.'/.env');
+        }
+
+
         if ($this->zerotimeDeployment) {
             return $this->remoteTaskService->run('ln -sfn '.$this->siteFolder.'/.env '.$this->release.'/.env');
         }
