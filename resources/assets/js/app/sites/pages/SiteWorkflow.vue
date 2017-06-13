77164f5bd0bdda9ae3c3593c8b19795b18e78cec
Luke Policinski <Luke@LukePOLO.com>
2017-06-13 18:09:23 -0400

    readability

--- resources/assets/js/app/sites/pages/SiteWorkflow.vue
+++ resources/assets/js/app/sites/pages/SiteWorkflow.vue
@@ -8,23 +8,11 @@
 
             <form @submit.prevent="saveWorkflow" class="floating-labels">
 
-                <div class="jcf-input-group input-checkbox">
-
-                    <div class="input-question">Normal Something not sure what to call it</div>
-
-                    <label>
-
-                        <tooltip message="" size="medium">
-                            <span class="fa fa-info-circle"></span>
-                        </tooltip>
-
-                        <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_files">
-                        <span class="icon"></span>
-                        Do you need to update your site files?
-                    </label>
-                </div>
 
                 <div class="jcf-input-group input-checkbox">
+
+                    <div class="input-question">Typical Additional Information Needed</div>
+
                     <label>
 
                         <tooltip message="" size="medium">
@@ -34,34 +22,6 @@
                         <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_databases">
                         <span class="icon"></span>
                         Do you need a database?
-
-                    </label>
-                </div>
-
-                <div class="jcf-input-group input-checkbox">
-                    <label>
-
-                        <tooltip message="These are keys that are not already in your account" size="medium">
-                            <span class="fa fa-info-circle"></span>
-                        </tooltip>
-
-                        <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_ssh_keys">
-                        <span class="icon"></span>
-                        Do you need to setup additional ssh keys?
-
-                    </label>
-                </div>
-
-                <div class="jcf-input-group input-checkbox">
-                    <label>
-
-                        <tooltip message="You can install a free Lets Encrypt certificate or include your own" size="medium">
-                            <span class="fa fa-info-circle"></span>
-                        </tooltip>
-
-                        <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_ssl_certs">
-                        <span class="icon"></span>
-                        Do you need to setup SSL certificates?
 
                     </label>
                 </div>
@@ -97,6 +57,37 @@
                 <div class="jcf-input-group input-checkbox">
                     <label>
 
+                        <tooltip message="" size="medium">
+                            <span class="fa fa-info-circle"></span>
+                        </tooltip>
+
+                        <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_files">
+                        <span class="icon"></span>
+                        Do you need to update your site files?
+                    </label>
+                </div>
+
+                <div class="jcf-input-group input-checkbox">
+                    <label>
+
+                        <tooltip message="You can install a free Lets Encrypt certificate or include your own" size="medium">
+                            <span class="fa fa-info-circle"></span>
+                        </tooltip>
+
+                        <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_ssl_certs">
+                        <span class="icon"></span>
+                        Do you need to setup SSL certificates?
+
+                    </label>
+                </div>
+
+
+                <div class="jcf-input-group input-checkbox">
+
+                    <div class="input-question">Unusual Changes Needed</div>
+
+                    <label>
+
                         <tooltip message="Except for ports 80 and 443" size="medium">
                             <span class="fa fa-info-circle"></span>
                         </tooltip>
@@ -104,6 +95,21 @@
                         <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_firewall_rules">
                         <span class="icon"></span>
                         Do you need to open firewall ports ?
+
+                    </label>
+                </div>
+
+                <div class="jcf-input-group input-checkbox">
+
+                    <label>
+
+                        <tooltip message="These are keys that are not already in your account" size="medium">
+                            <span class="fa fa-info-circle"></span>
+                        </tooltip>
+
+                        <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_ssh_keys">
+                        <span class="icon"></span>
+                        Do you need to setup additional ssh keys?
 
                     </label>
                 </div>
@@ -131,13 +137,14 @@
 
                         <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_server_features">
                         <span class="icon"></span>
-                        Do you need additional software for your server? (Advanced Users Only)
+                        Do you need additional software for your server?<br>
+                        <small>Advanced Users Only</small>
 
                     </label>
                 </div>
 
                 <div class="btn-footer">
-                    <button class="btn btn-primary" type="submit" :disabled="form.diff().length === 0">Update Workflow</button>
+                    <button class="btn btn-primary" type="submit">Update Workflow</button>
                 </div>
 
             </form>
@@ -151,8 +158,10 @@
             return {
                 form : this.createForm({
                     workflow : [
-                        'site_files',
                         'site_databases',
+                        'site_workers',
+                        'site_cron_jobs',
+                        'site_files',
                         'site_ssl_certs'
                     ]
                 })
