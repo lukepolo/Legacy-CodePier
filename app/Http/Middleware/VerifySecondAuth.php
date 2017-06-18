570309a5383b880093f529916f4ee06982f072c0
Luke Policinski <Luke@LukePOLO.com>
2017-06-18 15:54:40 -0400

    Apply fixes from StyleCI (#837)
    [ci skip] [skip ci]

--- app/Http/Middleware/VerifySecondAuth.php
+++ app/Http/Middleware/VerifySecondAuth.php
@@ -3,14 +3,12 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Session;
-use App\Http\Controllers\Auth\SecondAuthController;
 
 class VerifySecondAuth
 {
     public function handle($request, Closure $next)
     {
-        if (!second_authed()) {
+        if (! second_authed()) {
             return redirect(action('Auth\SecondAuthController@show'));
         }
 
