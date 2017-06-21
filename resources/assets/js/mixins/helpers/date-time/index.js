4648312f82805f78ea04cf180a81124bb06b0499
Luke Policinski <Luke@LukePOLO.com>
2017-06-21 19:13:07 -0400

    dont use timeago method

--- resources/assets/js/mixins/helpers/date-time/index.js
+++ resources/assets/js/mixins/helpers/date-time/index.js
@@ -2,18 +2,6 @@ export const now = () => {
     return moment()
 }
 
-export const timeAgo = (time) => {
-    time = moment(time)
-
-    const currentTime = moment()
-
-    if (currentTime.diff(time, 'hour') < 5) {
-        return time.fromNow()
-    }
-
-    return time.format('M-D-YY h:mm A')
-}
-
 export const parseDate = (date) => {
     return moment(date)
 }
