7845ae50e96be49685cb4e126915fafc833e5e9d
Janice Falotico <jfalotic@gmail.com>
2017-06-25 17:49:44 -0400

    pile update & workflow page

--- resources/assets/js/app/sites/pages/SiteWorkflow.vue
+++ resources/assets/js/app/sites/pages/SiteWorkflow.vue
@@ -1,158 +1,151 @@
 <template>
     <section>
-        <h3>
-            Now that you have entered in your information we will build you a workflow to make it easy as possible to make your application come to life.
-        </h3>
+        <h2 class="heading">
+            Select the additional items needed to create your site.
+        </h2>
 
-        <div class="jcf-form-wrap">
+        <form @submit.prevent="saveWorkflow" class="floating-labels">
+            <div class="grid-2">
+                <div class="grid-item">
+                    <div class="flyform--group">
+                        <label>Common Items Needed</label>
+                    </div>
 
-            <form @submit.prevent="saveWorkflow" class="floating-labels">
+                    <div class="flyform--group-checkbox">
+                        <label>
+                            <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_databases">
+                            <span class="icon"></span>
+                            Create a Database
 
+                            <tooltip message="" size="medium">
+                                <span class="fa fa-info-circle"></span>
+                            </tooltip>
+                        </label>
+                    </div>
 
-                <div class="jcf-input-group input-checkbox">
+                    <div class="flyform--group-checkbox">
+                        <label>
+                            <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_workers">
+                            <span class="icon"></span>
+                            Set up Workers
 
-                    <div class="input-question">Typical Additional Information Needed</div>
+                            <tooltip message="" size="medium">
+                                <span class="fa fa-info-circle"></span>
+                            </tooltip>
+                        </label>
+                    </div>
 
-                    <label>
+                    <div class="flyform--group-checkbox">
+                        <label>
+                            <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_cron_jobs">
+                            <span class="icon"></span>
+                            Set up Cron Jobs
 
-                        <tooltip message="" size="medium">
-                            <span class="fa fa-info-circle"></span>
-                        </tooltip>
+                            <tooltip message="" size="medium">
+                                <span class="fa fa-info-circle"></span>
+                            </tooltip>
+                        </label>
+                    </div>
 
-                        <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_databases">
-                        <span class="icon"></span>
-                        Do you need a database?
+                    <div class="flyform--group-checkbox">
+                        <label>
+                            <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_files">
+                            <span class="icon"></span>
+                            Update Your Site Files
 
-                    </label>
-                </div>
+                            <tooltip message="" size="medium">
+                                <span class="fa fa-info-circle"></span>
+                            </tooltip>
+                        </label>
+                    </div>
 
-                <div class="jcf-input-group input-checkbox">
-                    <label>
+                    <div class="flyform--group-checkbox">
+                        <label>
+                            <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_ssl_certs">
+                            <span class="icon"></span>
+                            Set up SSL Certificates
 
-                        <tooltip message="" size="medium">
-                            <span class="fa fa-info-circle"></span>
-                        </tooltip>
-
-                        <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_workers">
-                        <span class="icon"></span>
-                        Do you need to setup workers?
-
-                    </label>
-                </div>
-
-                <div class="jcf-input-group input-checkbox">
-                    <label>
-
-                        <tooltip message="" size="medium">
-                            <span class="fa fa-info-circle"></span>
-                        </tooltip>
-
-                        <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_cron_jobs">
-                        <span class="icon"></span>
-                        Do you need to setup cron jobs?
-
-                    </label>
-                </div>
-
-                <div class="jcf-input-group input-checkbox">
-                    <label>
-
-                        <tooltip message="" size="medium">
-                            <span class="fa fa-info-circle"></span>
-                        </tooltip>
-
-                        <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_files">
-                        <span class="icon"></span>
-                        Do you need to update your site files?
-                    </label>
-                </div>
-
-                <div class="jcf-input-group input-checkbox">
-                    <label>
-
-                        <tooltip message="You can install a free Lets Encrypt certificate or include your own" size="medium">
-                            <span class="fa fa-info-circle"></span>
-                        </tooltip>
-
-                        <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_ssl_certs">
-                        <span class="icon"></span>
-                        Do you need to setup SSL certificates?
-
-                    </label>
+                            <tooltip message="You can install a free Lets Encrypt certificate or include your own" size="medium">
+                                <span class="fa fa-info-circle"></span>
+                            </tooltip>
+                        </label>
+                    </div>
                 </div>
 
 
-                <div class="jcf-input-group input-checkbox">
+                <div class="grid-item">
+                    <div class="flyform--group">
+                        <label>Uncommon Items Needed</label>
+                    </div>
 
-                    <div class="input-question">Unusual Changes Needed</div>
+                    <div class="flyform--group-checkbox">
+                        <label>
+                            <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_firewall_rules">
+                            <span class="icon"></span>
+                            Open Firewall Ports
 
-                    <label>
+                            <tooltip message="Except for ports 80 and 443" size="medium">
+                                <span class="fa fa-info-circle"></span>
+                            </tooltip>
+                        </label>
+                    </div>
 
-                        <tooltip message="Except for ports 80 and 443" size="medium">
-                            <span class="fa fa-info-circle"></span>
-                        </tooltip>
+                    <div class="flyform--group-checkbox">
+                        <label>
+                            <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_ssh_keys">
+                            <span class="icon"></span>
+                            Set up Additional SSH Keys
 
-                        <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_firewall_rules">
-                        <span class="icon"></span>
-                        Do you need to open firewall ports ?
+                            <tooltip message="These are keys that are not already in your account" size="medium">
+                                <span class="fa fa-info-circle"></span>
+                            </tooltip>
+                        </label>
+                    </div>
 
-                    </label>
+                    <div class="flyform--group-checkbox">
+                        <label>
+                            <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_environment_variables">
+                            <span class="icon"></span>
+                            Set up Environment Variables
+
+                            <tooltip message="" size="medium">
+                                <span class="fa fa-info-circle"></span>
+                            </tooltip>
+                        </label>
+                    </div>
+
+                    <div class="flyform--group-checkbox">
+                        <label>
+                            <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_server_features">
+                            <span class="icon"></span>
+                            Additional Software for Your Server
+
+                            <tooltip message="Your site has already been customized based on your repository" size="medium">
+                                <span class="fa fa-info-circle"></span>
+                            </tooltip>
+
+                            <br>
+                            <small>Advanced Users Only</small>
+                        </label>
+                    </div>
+
+
                 </div>
+            </div>
 
-                <div class="jcf-input-group input-checkbox">
-
-                    <label>
-
-                        <tooltip message="These are keys that are not already in your account" size="medium">
-                            <span class="fa fa-info-circle"></span>
-                        </tooltip>
-
-                        <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_ssh_keys">
-                        <span class="icon"></span>
-                        Do you need to setup additional ssh keys?
-
-                    </label>
-                </div>
-
-                <div class="jcf-input-group input-checkbox">
-                    <label>
-
-                        <tooltip message="" size="medium">
-                            <span class="fa fa-info-circle"></span>
-                        </tooltip>
-
-                        <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_environment_variables">
-                        <span class="icon"></span>
-                        Do you need to setup environment variables?
-
-                    </label>
-                </div>
-
-                <div class="jcf-input-group input-checkbox">
-                    <label>
-
-                        <tooltip message="Your site has already been customized based on your repository" size="medium">
-                            <span class="fa fa-info-circle"></span>
-                        </tooltip>
-
-                        <input type="checkbox" v-model="form.workflow" name="workflow[]" value="site_server_features">
-                        <span class="icon"></span>
-                        Do you need additional software for your server?<br>
-                        <small>Advanced Users Only</small>
-
-                    </label>
-                </div>
-
-                <p>
-                    You can opt out workflow in your <router-link :to="{ name: 'my_account' }">profile</router-link>
-                </p>
-                <div class="btn-footer">
-                    <button class="btn btn-primary" type="submit">Update Workflow</button>
+            <div class="flyform--footer">
+                <div class="flyform--footer-btns">
                     <button @click.prevent="skipWorkflow" class="btn btn-danger" type="submit">Skip</button>
+                    <button class="btn btn-primary" type="submit">Continue</button>
                 </div>
 
-            </form>
-        </div>
+                <div class="flyform--footer-links">
+                    You can opt out of using the workflow in your <router-link :to="{ name: 'my_account' }">profile</router-link>.
+                </div>
+            </div>
+
+        </form>
+
     </section>
 </template>
 
