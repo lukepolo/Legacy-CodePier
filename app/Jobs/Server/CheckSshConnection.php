fb6762fc5f3679985e10f4d8843aba9d2958565b
Luke Policinski <Luke@LukePOLO.com>
2017-07-11 19:22:16 -0400

    linode working , except ....a werid php error

--- app/Jobs/Server/CheckSshConnection.php
+++ app/Jobs/Server/CheckSshConnection.php
@@ -2,16 +2,16 @@
 
 namespace App\Jobs\Server;
 
-use Carbon\Carbon;
-use App\Models\Server\Server;
-use Illuminate\Bus\Queueable;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Contracts\Server\ServerServiceContract;
-use App\Events\Server\ServerSshConnectionFailed;
-use App\Events\Server\ServerProvisionStatusChanged;
 use App\Contracts\Server\ServerServiceContract as ServerService;
+use App\Events\Server\ServerProvisionStatusChanged;
+use App\Events\Server\ServerSshConnectionFailed;
+use App\Models\Server\Server;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 
 class CheckSshConnection implements ShouldQueue
 {
@@ -43,15 +43,14 @@ class CheckSshConnection implements ShouldQueue
             event(new ServerProvisionStatusChanged($this->server, 'Queue for Provisioning', 0));
             dispatch((new ProvisionServer($this->server))->onQueue(config('queue.channels.server_provisioning')));
         } else {
-            //            if ($this->server->created_at->addMinutes(10) > Carbon::now()) {
+            if ($this->server->created_at->addMinutes(10) > Carbon::now()) {
                 dispatch((new self($this->server))->delay(10)->onQueue(config('queue.channels.server_provisioning')));
+                return;
+            }
 
-            return;
-//            }
-//
-//            event(
-//                new ServerSshConnectionFailed($this->server, 'Cannot connect to server. Server provisioning failed.')
-//            );
+            event(
+                new ServerSshConnectionFailed($this->server, 'Cannot connect to server. Server provisioning failed.')
+            );
         }
     }
 }
