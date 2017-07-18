fd2b622aa9ccbeae9e9135c2d1ce47a57c4e495e
Janice Falotico <jfalotic@gmail.com>
2017-07-17 20:18:17 -0400

    lifelines on overview.
    need help clearing/removing form after submission, and change status icon if last seen is longer than threshold

--- resources/assets/js/app/sites/components/Lifeline.vue
+++ resources/assets/js/app/sites/components/Lifeline.vue
@@ -1,17 +1,46 @@
 <template>
-    <section>
-        {{ lifeLine.name }} - {{ lifeLine.url }}<br>
-        <template v-if="lifeLine.last_seen">
-            <time-ago :time="lifeLine.last_seen"></time-ago>
-        </template>
-        <template v-else>
-            Never Seen
-        </template>
-        <br>
-        <confirm dispatch="user_site_life_lines/destroy" confirm_class="btn btn-small" :params="{ site : site.id, life_line : lifeLine.id }">
-            delete
-        </confirm>
-    </section>
+    <div class="lifeline">
+
+        <div class="flyform--group">
+            <div class="flex">
+                <div class="flex--grow flex--shrink">
+                    <label>{{ lifeLine.name }}</label>
+                    <textarea class="flex--grow" rows="3" readonly>{{ lifeLine.url }}</textarea>
+
+                    <div class="flex">
+                        <div class="flex--grow flex--shrink">
+                            <template v-if="lifeLine.last_seen">
+                                <div class="status status--success"></div>
+                                <time-ago :time="lifeLine.last_seen"></time-ago>
+                            </template>
+
+                            <template v-else>
+                                <div class="status status--neutral"></div>
+                                Never Seen
+                            </template>
+                        </div>
+
+                        <small>Check every {{ lifeLine.threshold }} minutes</small>
+                    </div>
+                </div>
+
+                <div>
+                    <tooltip message="Copy to Clipboard" class="flyform--btn-right">
+                        <clipboard :data="lifeLine.url"></clipboard>
+                    </tooltip>
+
+                    <br>
+
+                    <tooltip message="Delete" class="flyform--btn-right">
+                        <confirm dispatch="user_site_life_lines/destroy" confirm_class="btn btn-small" :params="{ site : site.id, life_line : lifeLine.id }">
+                            <span class="icon-trash text-error"></span>
+                        </confirm>
+                    </tooltip>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
--- resources/assets/js/app/sites/components/Lifelines.vue
+++ resources/assets/js/app/sites/components/Lifelines.vue
@@ -1,18 +1,47 @@
 <template>
-    <section>
+    <div class="grid--item">
+        <h3 class="heading text-center">Lifelines</h3>
 
-        <form @submit.prevent="createLifeline">
-            Name : <input type="text" v-model="form.name">
-            <br>
-            Should check-in every <input type="text" v-model="form.threshold"> minutes
-            <br>
-            <button type="submit">Create Life Line</button>
-        </form>
+        <life-line v-for="lifeLine in lifeLines" :key="lifeLine.id" :lifeLine="lifeLine" :site="site"></life-line>
 
-        <p v-for="lifeLine in lifeLines">
-           <life-line :lifeLine="lifeLine" :site="site"></life-line>
-        </p>
-    </section>
+        <template v-if="!showLifelineForm">
+            <hr style="margin-top: 40px">
+            <form @submit.prevent="createLifeline">
+                <div class="flyform--group">
+                    <input type="text" v-model="form.name" placeholder=" ">
+                    <label>Lifeline Name</label>
+                </div>
+
+                <div class="flyform--group">
+                    <label>Lifeline should check in every</label>
+                    <div class="flyform--group-postfix">
+                        <input type="number" v-model="form.threshold" placeholder=" ">
+                        <label></label>
+                        <template>
+                            <div class="flyform--group-postfix-label">
+                                minutes
+                            </div>
+                        </template>
+                    </div>
+                </div>
+
+                <div class="flyform-footer">
+                    <div class="flyform--footer-btns">
+                        <span class="btn btn-small" @click="showLifelineForm= !showLifelineForm">Cancel</span>
+                        <button type="submit" class="btn btn-small btn-primary">Create Lifeline</button>
+                    </div>
+                </div>
+            </form>
+        </template>
+
+        <template v-else>
+            <div class="flyform--footer">
+                <div class="flyform--footer-btns">
+                    <span class="btn btn-primary" @click="showLifelineForm= !showLifelineForm">Add Lifeline</span>
+                </div>
+            </div>
+        </template>
+    </div>
 </template>
 
 <script>
@@ -23,10 +52,11 @@
         },
         data() {
             return {
+                showLifelineForm : true,
                 form: this.createForm({
                     name : null,
-                    threshold : null,
-                    site : this.$route.params.site_id
+                    threshold : 5,
+                    site : this.$route.params.site_id,
                 })
             }
         },
