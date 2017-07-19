b00d874124b74e36143a2404fc34066d6810e903
Janice Falotico <jfalotic@gmail.com>
2017-07-18 23:30:39 -0400

    server provider stuff

--- resources/assets/js/app/servers/pages/ServerForm.vue
+++ resources/assets/js/app/servers/pages/ServerForm.vue
@@ -6,146 +6,109 @@
 
             <div class="section-content">
                 <div class="container">
-                    <div class="jcf-form-wrap">
-                        <form @submit.prevent="createServer()" class="validation-form floating-labels">
+                    <form @submit.prevent="createServer()" class="validation-form floating-labels">
 
-                            <template v-if="siteId">
-                                <input type="hidden" name="site" :value="siteId">
-                            </template>
-                            <template v-else>
-                                <input type="hidden" name="pile_id" :value="pile">
-                            </template>
+                        <template v-if="siteId">
+                            <input type="hidden" name="site" :value="siteId">
+                        </template>
+                        <template v-else>
+                            <input type="hidden" name="pile_id" :value="pile">
+                        </template>
 
-                            <template v-if="$route.params.type">
-                                <input type="hidden" name="type" :value="$route.params.type">
-                            </template>
+                        <template v-if="$route.params.type">
+                            <input type="hidden" name="type" :value="$route.params.type">
+                        </template>
 
-                            <div class="input-group input-radio">
-                                <div class="input-question">Server Provider</div>
-                                <template v-if="user_server_providers.length">
-                                    <template v-for="user_server_provider in user_server_providers">
-                                        <label>
-                                            <input
-                                                @change="getProviderData(user_server_provider.server_provider_id)"
-                                                type="radio"
-                                                name="server_provider_id"
-                                                :value="user_server_provider.server_provider_id"
-                                                v-model="server_provider"
-                                            >
-                                            <span class="icon"></span>
-                                            {{ getServerProviderName(user_server_provider.server_provider_id) }}
-                                        </label>
-                                    </template>
-                                </template>
-                                <template v-else>
-                                    You can connect other server providers through your
-                                    <router-link :to="{ name : 'user_server_providers' }">
-                                        profile
-                                    </router-link>
-                                </template>
 
-                                <label>
-                                    <input type="radio" @click="is_custom=true" value="" v-model="server_provider">
-                                    <span class="icon"></span>
-                                    Custom Server
-                                    <template v-if="is_custom">
-                                        <input type="hidden" name="custom" value="true">
-                                    </template>
-                                    <small>
-                                        This must be a fresh Ubuntu 16.04 system
-                                    </small>
-                                </label>
+                        <server-provider-selector :server_provider_id.sync="server_provider_id" :is_custom.sync="is_custom"></server-provider-selector>
 
+                        <template v-if="is_custom || server_provider_id">
+
+                            <div class="jcf-input-group">
+                                <input type="text" id="server_name" name="server_name" required>
+                                <label for="server_name"><span class="float-label">Name</span></label>
                             </div>
 
-                            <template v-if="is_custom || server_provider">
+                            <div class="jcf-input-group" v-if="is_custom">
+                                <input type="number" name="port" required value="22">
+                                <label for="port">
+                                    <tooltip message="We will use this port ssh connections" size="medium">
+                                        <span class="fa fa-info-circle"></span>
+                                    </tooltip>
+                                    <span class="float-label">SSH Port</span>
+                                </label>
+                            </div>
+
+                            <template v-if="server_provider_id && server_options.length && server_regions.length">
 
                                 <div class="jcf-input-group">
-                                    <input type="text" id="server_name" name="server_name" required>
-                                    <label for="server_name"><span class="float-label">Name</span></label>
+                                    <div class="input-question">Size</div>
+
+                                    <div class="select-wrap">
+                                        <select name="server_option">
+                                            <option v-for="option in server_options" :value="option.id">
+                                                {{ option.memory }} MB RAM
+                                                 - {{ option.cpus }} CPUS
+                                                 - {{ option.space }} SSD
+                                                 - ${{ option.priceHourly }} / Hour
+                                                 - ${{ option.priceMonthly }} / Month
+                                            </option>
+                                        </select>
+                                    </div>
                                 </div>
 
-                                <div class="jcf-input-group" v-if="is_custom">
-                                    <input type="number" name="port" required value="22">
-                                    <label for="port">
-                                        <tooltip message="We will use this port ssh connections" size="medium">
+                                <div class="jcf-input-group">
+                                    <div class="input-question">Region</div>
+
+                                    <div class="select-wrap">
+                                        <select name="server_region">
+                                            <option v-for="region in server_regions" :value="region.id">{{ region.name }}</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="input-group input-checkbox" v-if="server_provider_features.length">
+                                    <div class="input-question">Features</div>
+                                    <template v-for="feature in server_provider_features">
+                                        <label>
+                                            <input
+                                                type="checkbox"
+                                                name="server_provider_features[]"
+                                                :value="feature.id"
+                                            >
+                                            <span class="icon"></span>{{ 'Enable ' + feature.feature }}
+                                            <small>{{ feature.cost }}</small>
+                                        </label>
+                                    </template>
+                                </div>
+                            </template>
+
+                            <div class="jcf-input-group">
+                                <label for="web_directory">
+                                    <h3 v-if="$route.params.site_id">
+                                        <tooltip message="We have configured your site based on your apps language and framework, thus you do not need to modify the server if you do not want to" size="medium">
                                             <span class="fa fa-info-circle"></span>
                                         </tooltip>
-                                        <span class="float-label">SSH Port</span>
-                                    </label>
-                                </div>
+                                        Your server has been customized for your application<br>
+                                        <small>
+                                            <a @click="customize_server = !customize_server">(customize)</a>
+                                        </small>
+                                    </h3>
+                                    <h3 v-else>
+                                        Setup your server :
+                                    </h3>
+                                </label>
+                            </div>
 
-                                <template v-if="server_provider && server_options.length && server_regions.length">
+                            <server-features :update="false" v-show="customize_server"></server-features>
 
-                                    <div class="jcf-input-group">
-                                        <div class="input-question">Size</div>
+                            <br><br><br>
+                            <div class="btn-footer">
+                                <button type="submit" class="btn btn-primary">Create Server</button>
+                            </div>
 
-                                        <div class="select-wrap">
-                                            <select name="server_option">
-                                                <option v-for="option in server_options" :value="option.id">
-                                                    {{ option.memory }} MB RAM
-                                                    - {{ option.cpus }} CPUS
-                                                    - {{ option.space }} SSD
-                                                    - ${{ option.priceHourly }} / Hour
-                                                    - ${{ option.priceMonthly }} / Month
-                                                </option>
-                                            </select>
-                                        </div>
-                                    </div>
-
-                                    <div class="jcf-input-group">
-                                        <div class="input-question">Region</div>
-
-                                        <div class="select-wrap">
-                                            <select name="server_region">
-                                                <option v-for="region in server_regions" :value="region.id">{{ region.name }}</option>
-                                            </select>
-                                        </div>
-                                    </div>
-
-                                    <div class="input-group input-checkbox" v-if="server_provider_features.length">
-                                        <div class="input-question">Features</div>
-                                        <template v-for="feature in server_provider_features">
-                                            <label>
-                                                <input
-                                                    type="checkbox"
-                                                    name="server_provider_features[]"
-                                                    :value="feature.id"
-                                                >
-                                                <span class="icon"></span>{{ 'Enable ' + feature.feature }}
-                                                <small>{{ feature.cost }}</small>
-                                            </label>
-                                        </template>
-                                    </div>
-                                </template>
-
-                                <div class="jcf-input-group">
-                                    <label for="web_directory">
-                                        <h3 v-if="$route.params.site_id">
-                                            <tooltip message="We have configured your site based on your apps language and framework, thus you do not need to modify the server if you do not want to" size="medium">
-                                                <span class="fa fa-info-circle"></span>
-                                            </tooltip>
-                                            Your server has been customized for your application<br>
-                                            <small>
-                                                <a @click="customize_server = !customize_server">(customize)</a>
-                                            </small>
-                                        </h3>
-                                        <h3 v-else>
-                                            Setup your server :
-                                        </h3>
-                                    </label>
-                                </div>
-
-                                <server-features :update="false" v-show="customize_server"></server-features>
-
-                                <br><br><br>
-                                <div class="btn-footer">
-                                    <button type="submit" class="btn btn-primary">Create Server</button>
-                                </div>
-
-                            </template>
-                        </form>
-                    </div>
+                        </template>
+                    </form>
                 </div>
             </div>
         </section>
@@ -153,32 +116,31 @@
 </template>
 
 <script>
-    import LeftNav from '../../../components/LeftNav.vue'
-    import {ServerFeatures} from '../../setup/pages'
 
+    import {ServerFeatures} from '../../setup/pages'
+    import LeftNav from '../../../components/LeftNav.vue'
+    import ServerProviderSelector from './../components/ServerProviderSelector.vue'
     export default {
         components: {
             LeftNav,
-            ServerFeatures
-        },
-        created() {
-            this.fetchData()
-        },
-        watch: {
-            '$route': 'fetchData'
+            ServerFeatures,
+            ServerProviderSelector
         },
         data() {
          return {
             is_custom : false,
-            server_provider : null,
+            server_provider_id : null,
             customize_server : !this.$route.params.site_id,
          }
         },
+        watch : {
+            'server_provider_id' : function() {
+                if(this.server_provider_id) {
+                    this.getProviderData(this.server_provider_id)
+                }
+            }
+        },
         methods: {
-            fetchData() {
-                this.$store.dispatch('server_providers/get')
-                this.$store.dispatch('user_server_providers/get', this.$store.state.user.user.id)
-            },
             getProviderData(server_provider_id) {
                 this.is_custom = false
                 let provider = _.find(this.server_providers, { id : server_provider_id}).provider_name
@@ -198,14 +160,6 @@
                         }
                     }
                 })
-            },
-            getServerProviderName(server_provider_id) {
-                if(this.server_providers) {
-                    let server_provider = _.find(this.server_providers, { id : server_provider_id})
-                    if(server_provider) {
-                        return server_provider.name
-                    }
-                }
             }
         },
         computed: {
@@ -223,9 +177,6 @@
             },
             server_providers() {
                 return this.$store.state.server_providers.providers
-            },
-            user_server_providers() {
-                return this.$store.state.user_server_providers.providers
             },
             server_provider_features() {
                 return this.$store.state.server_providers.features
--- resources/assets/js/app/sites/components/SiteHeader.vue
+++ resources/assets/js/app/sites/components/SiteHeader.vue
@@ -9,93 +9,6 @@
 
         <div class="section-header--btn-right">
 
-            <template v-if="site.user_repository_provider_id">
-                <template v-if="workFlowCompleted === true">
-
-                    <template v-if="!site.automatic_deployment_id">
-
-                    <span class="dropdown" @click="createDeployHook">
-                        <tooltip message="Enable Auto Deploy" placement="bottom-left" class="btn btn-default">
-                            <span class="icon-cloud-auto-deploy"></span>
-                        </tooltip>
-                    </span>
-
-                    </template>
-
-                    <template v-else>
-
-                    <span class="dropdown" @click="removeDeployHook">
-                        <tooltip message="Remove Auto Deploy" placement="bottom-left" class="btn btn-default">
-                            <span class="icon-cloud-auto-deploy active"></span>
-                        </tooltip>
-                    </span>
-
-                    </template>
-
-                </template>
-            </template>
-
-
-            <drop-down tag="span">
-
-                <tooltip slot="header" message="Site SSH Key" placement="bottom-left" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                    <span class="icon-web"></span>
-                </tooltip>
-
-                <div slot="content" class="dropdown-menu dropdown-content nowrap">
-
-                    <div class="jcf-form-wrap">
-
-                        <div class="jcf-input-group">
-
-                            <div class="input-question">
-                                <confirm-dropdown dispatch="user_site_ssh_keys/refreshPublicKey" :params="site.id">Public SSH Key &nbsp;<a href="#"><span class="fa fa-refresh"></span></a></confirm-dropdown>
-                            </div>
-
-                            <textarea rows="10" readonly>{{ site.public_ssh_key }}</textarea>
-
-                            <div class="text-right">
-                                <clipboard :data="site.public_ssh_key"></clipboard>
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </drop-down>
-
-            <drop-down tag="span">
-
-                <tooltip slot="header" message="Deploy Hook URL" placement="bottom-left" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                    <span class="icon-webhooks"></span>
-                </tooltip>
-
-                <div slot="content" class="dropdown-menu nowrap">
-
-                    <div class="jcf-form-wrap">
-
-                        <div class="jcf-input-group">
-
-                            <div class="input-question">
-                                <confirm-dropdown dispatch="user_site_deployments/refreshDeployKey" :params="site.id">Deploy Hook URL &nbsp;<a href="#"><span class="fa fa-refresh"></span></a></confirm-dropdown>
-                            </div>
-
-                            <textarea  rows="3" readonly :value="deployHook"></textarea>
-
-                            <div class="text-right">
-                                <clipboard :data="deployHook"></clipboard>
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </drop-down>
-
             <drop-down tag="span" v-if="siteServers">
 
                 <button slot="header" class="btn btn-default btn-xs dropdown-toggle" type="button" data-toggle="dropdown">
@@ -124,23 +37,6 @@
 
 <script>
     export default {
-        data() {
-          return {
-              webhook : false,
-              sshKey: false,
-          }
-        },
-        methods: {
-            createDeployHook() {
-                return this.$store.dispatch('user_site_deployments/createDeployHook', this.$route.params.site_id)
-            },
-            removeDeployHook() {
-                this.$store.dispatch('user_site_deployments/removeDeployHook', {
-                    site : this.$route.params.site_id,
-                    hook : this.site.automatic_deployment_id
-                });
-            },
-        },
         computed: {
             site() {
                 return this.$store.state.user_sites.site;
@@ -151,12 +47,7 @@
                 if(siteServers && siteServers.length) {
                     return siteServers
                 }
-            },
-            deployHook() {
-                if(this.site) {
-                    return location.protocol+'//'+location.hostname + Vue.action('WebHookController@deploy', { siteHashID : this.site.hash })
-                }
-            },
+            }
         }
     }
 </script>
