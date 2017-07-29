cf0006875d5699244178423a59c5e201760997e4
Luke Policinski <luke@lukepolo.com>
2017-07-29 10:27:38 -0500

    fixing removing deploy hook

--- app/Http/Requests/EnvironmentVariableRequest.php
+++ app/Http/Requests/EnvironmentVariableRequest.php
@@ -28,4 +28,11 @@ class EnvironmentVariableRequest extends FormRequest
             'value' => 'required',
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'environment_variable' => 'Please enter a valid environment variable name. Must not start with a number, contain special characters or spaces.'
+        ];
+    }
 }
