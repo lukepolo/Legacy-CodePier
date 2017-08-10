bf1f80ff1b3062a9452ad507568c59f34e4ebeef
Luke Policinski <Luke@LukePOLO.com>
2017-08-09 21:50:40 -0400

    StyleJS fixes (#934)
    StyleJS Auto Squash and Merge PR-934

--- resources/assets/js/store/modules/user/site/cron-jobs/actions.js
+++ resources/assets/js/store/modules/user/site/cron-jobs/actions.js
@@ -14,7 +14,10 @@ export const store = (context, data) => {
 
 export const patch = (context, data) => {
   return Vue.request(data).put(
-    Vue.action("SiteSiteCronJobController@update", { site: data.site, cron_job : data.cron_job }),
+    Vue.action("SiteSiteCronJobController@update", {
+      site: data.site,
+      cron_job: data.cron_job
+    }),
     "user_site_cron_jobs/update"
   );
 };
