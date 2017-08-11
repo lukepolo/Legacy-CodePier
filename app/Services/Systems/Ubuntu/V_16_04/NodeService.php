a25f6504ac46910df8d244c1e875f67ace57625c
Luke Policinski <Luke@LukePOLO.com>
2017-08-11 17:16:28 -0400

    little chnages

--- app/Services/Systems/Ubuntu/V_16_04/NodeService.php
+++ app/Services/Systems/Ubuntu/V_16_04/NodeService.php
@@ -22,17 +22,12 @@ class NodeService
 
         $this->remoteTaskService->run('curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash');
 
-        $this->remoteTaskService->appendTextToFile('/home/codepier/.bashrc', '
+        $this->remoteTaskService->appendTextToFile('/etc/profile', '
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 ');
 
-        $this->remoteTaskService->appendTextToFile('/root/.bashrc', '
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-');
 
         $this->remoteTaskService->run('nvm install '.$version);
         $this->remoteTaskService->run('nvm alias default '.$version);
