3b637803624f7fff5e5cc8b52390e96b12bf9c14
Luke Policinski <Luke@LukePOLO.com>
2017-08-22 19:32:53 -0400

    fixing git url

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -21,6 +21,7 @@ if (app.$store.state.user.user.is_subscribed) {
   app.$store.dispatch("user_commands/get");
   app.$store.dispatch("user_ssh_keys/get");
   app.$store.dispatch("user_piles/get");
+  app.$store.dispatch('repository_providers/get')
 }
 
 app.$store.dispatch("user_teams/get");
