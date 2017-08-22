3b8d48bc275e803b808ca0f56c7e59f45f7e8431
Luke Policinski <Luke@LukePOLO.com>
2017-08-22 19:28:20 -0400

    we now show a message about why the workflow was activated

--- resources/assets/js/mixins/helpers/site/index.js
+++ resources/assets/js/mixins/helpers/site/index.js
@@ -19,13 +19,17 @@ export const workFlowCompleted = function() {
   let site = this.$store.state.user_sites.site;
 
   if (site && site.repository && site.workflow) {
-    let workflows = _.sortBy(
+    let workflows = _.filter(_.sortBy(
       _.map(site.workflow, function(flow, step) {
-        flow.step = step;
+        if(_.isObject(flow)) {
+            flow.step = step;
+        }
         return flow;
       }),
       "order"
-    );
+    ), function(workflow) {
+      return _.isObject(workflow)
+    });
 
     let currentWorkflow = _.find(workflows, function(flow) {
       return flow.completed === false;
