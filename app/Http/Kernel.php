40bdaf1a9b24f89cce22b17ea9ee91ca12b61a51
Luke Policinski <luke@lukepolo.com>
2017-08-25 12:28:43 -0400

    update middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,10 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\TrimStrings::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\TrustProxies::class,
     ];
 
     /**
@@ -25,7 +29,6 @@ class Kernel extends HttpKernel
     protected $middlewareGroups = [
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
-            \App\Http\Middleware\TrimStrings::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\Session\Middleware\AuthenticateSession::class,
@@ -33,7 +36,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-            \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
             \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
             \App\Http\Middleware\GitCommit::class,
             \App\Http\Middleware\SentryContext::class,
@@ -59,11 +61,16 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+
+        /** Passport */
         'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
         'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
+
+        /** Custom */
         'role' => \App\Http\Middleware\CheckRole::class,
-        'auth.provisioning-key' => \App\Http\Middleware\VerifyProvisioningKey::class,
-        'second_auth' => \App\Http\Middleware\VerifySecondAuth::class,
         'subscribed' =>  \App\Http\Middleware\IsSubscribed::class,
+        'second_auth' => \App\Http\Middleware\VerifySecondAuth::class,
+        'auth.provisioning-key' => \App\Http\Middleware\VerifyProvisioningKey::class,
+
     ];
 }
