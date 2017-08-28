78e666ed8b0ed80b7322fb5a95fa773352be89f1
Luke Policinski <Luke@LukePOLO.com>
2017-08-28 18:20:54 -0400

    fixing server workers

--- resources/assets/js/store/modules/user/server/daemons/actions.js
+++ resources/assets/js/store/modules/user/server/daemons/actions.js
@@ -1,30 +1,30 @@
-export const get = (context, site) => {
+export const get = (context, server) => {
   return Vue.request().get(
-    Vue.action("SiteSiteDaemonsController@index", { site: site }),
-    "user_site_daemons/setAll"
+    Vue.action("ServerServerDaemonsController@index", { server: server }),
+    "user_server_daemons/setAll"
   );
 };
 
 export const store = (context, data) => {
   return Vue.request(data).post(
-    Vue.action("SiteSiteDaemonsController@store", { site: data.site }),
-    "user_site_daemons/add"
+    Vue.action("ServerServerDaemonsController@store", { server: data.server }),
+    "user_server_daemons/add"
   );
 };
 
 export const patch = data => {
   return Vue.request(data).put(
-    Vue.action("SiteSiteDaemonsController@update", { site: data.site }),
-    "user_site_daemons/update"
+    Vue.action("ServerServerDaemonsController@update", { server: data.server }),
+    "user_server_daemons/update"
   );
 };
 
 export const destroy = (context, data) => {
   return Vue.request(data).delete(
-    Vue.action("SiteSiteDaemonsController@destroy", {
-      site: data.site,
+    Vue.action("ServerServerDaemonsController@destroy", {
+      server: data.server,
       daemon: data.daemon
     }),
-    "user_site_daemons/remove"
+    "user_server_daemons/remove"
   );
 };
