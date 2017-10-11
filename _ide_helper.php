db9b7092889e96574b80209c1605ad108a35469a
Luke Policinski <Luke@LukePOLO.com>
2017-10-10 20:28:55 -0400

    need no dev when installing on production

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.5.14 on 2017-10-10.
+ * Generated for Laravel 5.5.14 on 2017-10-11.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
