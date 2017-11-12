16ddf5ee76201b97aad9dd8410e7b13112d82467
Janice Falotico <jfalotic@gmail.com>
2017-11-11 23:17:21 -0500

    format for database section

--- resources/assets/js/app/setup/components/DatabaseSection.vue
+++ resources/assets/js/app/setup/components/DatabaseSection.vue
@@ -1,19 +1,103 @@
 <template>
     <section>
+
+        <form @submit.prevent="createSchema" class="flyform--submit">
+            <div class="flyform--group">
+                <input type="text" name="name" v-model="schemaForm.name" placeholder=" ">
+                <label for="name">Schema Name</label>
+            </div>
+
+            <div class="flyform--footer-btns">
+                <button class="btn btn-primary btn-small" type="submit">Create Schema</button>
+            </div>
+        </form>
+
+        <br><br>
+
+        <div class="list">
+            <div class="list--item list--item-icons list--item-heading">
+                <div class="list--item-text">
+                    -- schema name --
+                </div>
+                <div class="list--icon">
+                    <span class="icon-group_add"></span>
+                </div>
+                <div class="list--icon">
+                    <div class="icon-trash"></div>
+                </div>
+            </div>
+
+            <div class="list--item list--item-icons list--item-subgroup">
+                <div class="list--item-text">
+                    -- user name --
+                </div>
+
+                <div class="list--icon">
+                    <div class="icon-trash"></div>
+                </div>
+            </div>
+
+            <form>
+                <div class="flyform--submit">
+                    <div class="flyform--group">
+                        <input type="text" name="name" placeholder=" ">
+                        <label for="name">Name</label>
+                    </div>
+
+                    <div class="flyform--group">
+                        <input type="password" name="password" placeholder=" ">
+                        <label for="password">Password</label>
+                    </div>
+
+                    <div class="flyform--footer-btns">
+                        <button class="btn btn-primary btn-small" type="submit">Create User</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <div class="list">
+            <div class="list--item list--item-icons list--item-heading">
+                <div class="list--item-text">
+                    -- schema name --
+                </div>
+
+                <div class="list--icon">
+                    <span class="icon-group_add"></span>
+                </div>
+                <div class="list--icon">
+                    <div class="icon-trash"></div>
+                </div>
+            </div>
+
+
+            <form>
+                <div class="flyform--submit">
+                    <div class="flyform--group">
+                        <input type="text" name="name" placeholder=" ">
+                        <label for="name">Name</label>
+                    </div>
+
+                    <div class="flyform--group">
+                        <input type="password" name="password" placeholder=" ">
+                        <label for="password">Password</label>
+                    </div>
+
+                    <div class="flyform--footer-btns">
+                        <button class="btn btn-primary btn-small" type="submit">Create User</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+
+
+        <br><br><br><br><br>
         <h3><span class="h--label">{{ database }}</span></h3>
 
         <div class="grid-2">
 
-            <form @submit.prevent="createSchema" class="floating-labels">
-                <div class="flyform--group">
-                    <input type="text" name="name" v-model="schemaForm.name" placeholder=" ">
-                    <label for="name">Schema Name</label>
-                </div>
 
-                <div class="flyform--footer-btns">
-                    <button class="btn btn-primary btn-small" type="submit">Create Schema</button>
-                </div>
-            </form>
 
             <table class="table" v-if="schemas.length">
                 <thead>
