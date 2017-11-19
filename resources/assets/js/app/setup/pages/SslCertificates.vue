7c6241b7571b1d17cebac9e7f243f48057384f4b
Luke Policinski <Luke@LukePOLO.com>
2017-11-19 13:42:52 -0500

    ssl certifcates new form done

--- resources/assets/js/app/setup/pages/SslCertificates.vue
+++ resources/assets/js/app/setup/pages/SslCertificates.vue
@@ -1,6 +1,76 @@
 <template>
     <section>
-        <form @submit.prevent="installCertificate">
+
+        <div class="flex flex--center">
+            <h3 class="flex--grow">
+                SSL Certificates
+            </h3>
+
+            <tooltip message="Add SSL Certificate">
+                <span class="btn btn-small btn-primary" :class="{ 'btn-disabled' : this.shouldShowForm }" @click="showForm = true">
+                    <span class="icon-plus"></span>
+                </span>
+            </tooltip>
+        </div>
+
+        <table class="table" v-if="sslCertificates.length">
+            <thead>
+                <tr>
+                    <th>Domains</th>
+                    <th>Type</th>
+                    <th>Cert Path</th>
+                    <th>Key Path</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="sslCertificate in sslCertificates" :key="sslCertificate.id">
+                    <td>{{ sslCertificate.domains }}</td>
+                    <td>{{ sslCertificate.type }}</td>
+                    <td class="break-word">{{ sslCertificate.cert_path }}</td>
+                    <td class="break-word">{{ sslCertificate.key_path }}</td>
+                    <td class="table--action">
+                        <template v-if="isRunningCommandFor(sslCertificate.id)">
+                            {{ isRunningCommandFor(sslCertificate.id).status }}
+
+                            <tooltip message="Delete">
+                                <span class="table--action-delete">
+                                    <a @click="deleteSslCertificate(sslCertificate.id)"><span class="icon-trash"></span></a>
+                                </span>
+                            </tooltip>
+                        </template>
+                        <template v-else>
+                            <template v-if="sslCertificate.failed">
+                                <tooltip message="Retry Install">
+                                    <span class="table--action-retry">
+                                        <a @click="retryInstall(sslCertificate.domains)"><span class="icon-refresh"></span></a>
+                                    </span>
+                                </tooltip>
+                            </template>
+                            <template v-else>
+                                <tooltip message="Deactivate" v-if="sslCertificate.active">
+                                    <span class="table--action-deactivate">
+                                        <a @click="deactivateSslCertificate(sslCertificate.id)"><span class="icon-cancel"></span></a>
+                                    </span>
+                                </tooltip>
+                                <tooltip message="Activate" v-else>
+                                    <span class="table--action-activate">
+                                        <a @click="activateSslCertificate(sslCertificate.id)"><span class="icon-check_circle"></span></a>
+                                    </span>
+                                </tooltip>
+                            </template>
+                            <tooltip message="Delete">
+                                <span class="table--action-delete">
+                                    <a @click="deleteSslCertificate(sslCertificate.id)"><span class="icon-trash"></span></a>
+                                </span>
+                            </tooltip>
+                        </template>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <form @submit.prevent="installCertificate" v-if="shouldShowForm">
 
             <div class="flyform--group">
                 <label>Certificate Type</label>
@@ -44,72 +114,12 @@
 
             <div class="flyform--footer">
                 <div class="flyform--footer-btns">
+                    <button class="btn" v-if="sslCertificates.length" @click.prevent="resetForm">Cancel</button>
                     <button class="btn btn-primary" type="submit">Add Certificate</button>
                 </div>
             </div>
 
         </form>
-
-        <div v-if="ssl_certificates.length">
-            <h3>SSL Certificates</h3>
-
-            <table class="table">
-                <thead>
-                <tr>
-                    <th>Domains</th>
-                    <th>Type</th>
-                    <th>Cert Path</th>
-                    <th>Key Path</th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="ssl_certificate in ssl_certificates" :key="ssl_certificate.id">
-                    <td>{{ ssl_certificate.domains }}</td>
-                    <td>{{ ssl_certificate.type }}</td>
-                    <td class="break-word">{{ ssl_certificate.cert_path }}</td>
-                    <td class="break-word">{{ ssl_certificate.key_path }}</td>
-                    <td class="table--action">
-                        <template v-if="isRunningCommandFor(ssl_certificate.id)">
-                            {{ isRunningCommandFor(ssl_certificate.id).status }}
-
-                            <tooltip message="Delete">
-                                <span class="table--action-delete">
-                                    <a @click="deleteSslCertificate(ssl_certificate.id)"><span class="icon-trash"></span></a>
-                                </span>
-                            </tooltip>
-                        </template>
-                        <template v-else>
-                            <template v-if="ssl_certificate.failed">
-                                <tooltip message="Retry Install">
-                                    <span class="table--action-retry">
-                                        <a @click="retryInstall(ssl_certificate.domains)"><span class="icon-refresh"></span></a>
-                                    </span>
-                                </tooltip>
-                            </template>
-                            <template v-else>
-                                <tooltip message="Deactivate" v-if="ssl_certificate.active">
-                                    <span class="table--action-deactivate">
-                                        <a @click="deactivateSslCertificate(ssl_certificate.id)"><span class="icon-cancel"></span></a>
-                                    </span>
-                                </tooltip>
-                                <tooltip message="Activate" v-else>
-                                    <span class="table--action-activate">
-                                        <a @click="activateSslCertificate(ssl_certificate.id)"><span class="icon-check_circle"></span></a>
-                                    </span>
-                                </tooltip>
-                            </template>
-                            <tooltip message="Delete">
-                                <span class="table--action-delete">
-                                    <a @click="deleteSslCertificate(ssl_certificate.id)"><span class="icon-trash"></span></a>
-                                </span>
-                            </tooltip>
-                        </template>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
 
         <input type="hidden" v-if="site">
     </section>
@@ -119,6 +129,8 @@
     export default {
         data() {
             return {
+                loaded : false,
+                showForm : false,
                 form  : this.createForm({
                     type : null,
                     domains : null,
@@ -136,13 +148,16 @@
         methods: {
             fetchData() {
                 if(this.siteId) {
-                    this.$store.dispatch('user_site_ssl_certificates/get', this.siteId);
+                    this.$store.dispatch('user_site_ssl_certificates/get', this.siteId).then(() => {
+                        this.loaded = true;
+                    });
                 }
 
                 if(this.serverId) {
-                    this.$store.dispatch('user_server_ssl_certificates/get', this.serverId);
+                    this.$store.dispatch('user_server_ssl_certificates/get', this.serverId).then(() => {
+                      this.loaded = true;
+                    });
                 }
-
             },
             installCertificate() {
                 if(this.siteId) {
@@ -243,6 +258,7 @@
                 } else {
                     this.form.type = 'existing'
                 }
+                this.showForm = false;
             }
         },
         computed: {
@@ -267,9 +283,12 @@
                 }
                 return serverId
             },
-            ssl_certificates() {
+            sslCertificates() {
                 return this.$store.state.user_site_ssl_certificates.ssl_certificates;
-            }
+            },
+            shouldShowForm() {
+                return (this.loaded && this.sslCertificates.length === 0) || this.showForm;
+            },
         }
     }
 </script>
