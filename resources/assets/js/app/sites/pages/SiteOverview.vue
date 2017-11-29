d58ac2ae23b30f43692ac713840078d2f1c66473
Janice Falotico <jfalotic@gmail.com>
2017-11-28 20:20:46 -0500

    Recent deployments & lifeline edits

--- resources/assets/js/app/sites/pages/SiteOverview.vue
+++ resources/assets/js/app/sites/pages/SiteOverview.vue
@@ -180,26 +180,41 @@
             </drop-down>
         </div>
 
-        <div class="grid-2">
+        <div class="grid-2 grid-gap-large">
             <div class="grid--item">
-                <h3 class="text-center">Recent Deployments</h3>
+                <h3 class="text-center heading">Recent Deployments</h3>
 
                 <div v-if="!recentDeployments">
                     <div class="placeholder text-center">Recent deployments will show up here once you have deployed your site.</div>
                 </div>
                 <template v-else>
+                    <div class="list">
                     <template v-for="recentDeployment in recentDeployments">
-                        {{ recentDeployment.status }} <time-ago :time="recentDeployment.created_at"></time-ago>
-                        <br>
-                        <small>
-                            took ({{ diff(recentDeployment.created_at, recentDeployment.updated_at) }})
-                        </small>
+                        <div class="list--item list--item-icons">
+                            <div>
+                                {{ recentDeployment.status }} <time-ago :time="recentDeployment.created_at"></time-ago>
 
-                        <confirm dispatch="user_site_deployments/rollback" confirm_class="btn btn-small" :params="{ siteDeployment : recentDeployment.id, site : site.id } " v-if="recentDeployment.status === 'Completed'">
-                            Rollback
-                        </confirm>
-                        <br>
+                                <div>
+                                    <small>took ({{ diff(recentDeployment.created_at, recentDeployment.updated_at) }})</small>
+                                </div>
+                            </div>
+
+                            <confirm dispatch="user_site_deployments/rollback" confirm_position="btns-only" confirm_class="btn-link" :params="{ siteDeployment : recentDeployment.id, site : site.id } " v-if="recentDeployment.status === 'Completed'">
+                                <tooltip message="Rollback">
+                                    <span class="icon-refresh2"></span>
+                                </tooltip>
+                            </confirm>
+                        </div>
+
+
+                        <div class="flex deployment">
+                            <div class="flex--grow deployment--text">
+                            </div>
+                            <div class="deployment--btns">
+                            </div>
+                        </div>
                     </template>
+                    </div>
                 </template>
             </div>
 
