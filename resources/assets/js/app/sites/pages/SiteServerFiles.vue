9222c1ca5f5cc2b6ab789a766485fc4031535d76
Luke Policinski <Luke@LukePOLO.com>
2017-12-06 21:59:24 -0500

    fixing site server files

--- resources/assets/js/app/sites/pages/SiteServerFiles.vue
+++ resources/assets/js/app/sites/pages/SiteServerFiles.vue
@@ -1,47 +1,56 @@
 <template>
     <section>
-        <template v-if="possibleFiles && site">
-            <site-file :site="site" :file="file" v-for="file in possibleFiles" :key="file" :running="isRunningCommandFor(file)"></site-file>
+        <template v-if="site && files">
+            <site-file :site="site" :file="file" v-for="file in files" :key="file.id" :running="isRunningCommandFor(file)"></site-file>
         </template>
     </section>
 </template>
 
 <script>
 
-    import {
-        SiteFile,
-    } from '../components';
+  import {
+    SiteFile,
+  } from '../components'
 
-    export default {
-        components : {
-            SiteFile,
-        },
-        created() {
-        },
-        watch: {
-            '$route': 'fetchData'
-        },
-        methods: {
-            isRunningCommandFor(file) {
-                if(this.siteFiles) {
-                    let foundFile =_.find(this.siteFiles, { file_path : file });
-                    if(foundFile) {
-                        return this.isCommandRunning('App\\Models\\File', foundFile.id);
-                    }
-                }
-                return false;
-            }
-        },
-        computed: {
-            runningCommands() {
-                return this.$store.state.commands.running_commands;
-            },
-            site() {
-                return this.$store.state.user_sites.site;
-            },
-            siteFiles() {
-                return this.$store.state.user_site_files.files;
-            }
-        },
-    }
+  export default {
+    components: {
+      SiteFile,
+    },
+    created () {
+      this.fetchData()
+    },
+    watch: {
+      '$route': 'fetchData'
+    },
+    methods: {
+      fetchData () {
+        this.$store.dispatch('user_site_files/get', this.$route.params.site_id)
+      },
+      isRunningCommandFor (file) {
+        if (this.siteFiles) {
+          let foundFile = _.find(this.siteFiles, {file_path: file})
+          if (foundFile) {
+            return this.isCommandRunning('App\\Models\\File', foundFile.id)
+          }
+        }
+        return false
+      }
+    },
+    computed: {
+      runningCommands () {
+        return this.$store.state.commands.running_commands
+      },
+      site () {
+        return this.$store.state.user_sites.site
+      },
+      files () {
+        return this.siteFiles.filter((file) => {
+            return (!file.custom && !file.framework_file);
+        });
+      },
+      siteFiles () {
+        return this.$store.state.user_site_files.files
+      }
+    },
+  }
 </script>
