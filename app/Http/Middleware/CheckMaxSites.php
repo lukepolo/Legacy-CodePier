1c2d5ba6730f5e948272fded0efaf6a119880e10
Luke Policinski <Luke@LukePOLO.com>
2017-12-12 22:43:55 -0500

    Apply fixes from StyleCI (#1045)
    [ci skip] [skip ci]

--- app/Http/Middleware/CheckMaxSites.php
+++ app/Http/Middleware/CheckMaxSites.php
@@ -15,26 +15,27 @@ class CheckMaxSites
      */
     public function handle($request, Closure $next)
     {
-        if (!$request->user()->subscribed()) {
-            if($request->user()->servers->count() > 1) {
+        if (! $request->user()->subscribed()) {
+            if ($request->user()->servers->count() > 1) {
                 return $this->toManyServersResponse(1);
             }
         }
 
         $stripePlan = $request->user()->subscription()->stripe_plan;
 
-        if(str_contains($stripePlan, 'firstmate')) {
-            if($request->user()->servers->count() > 30) {
+        if (str_contains($stripePlan, 'firstmate')) {
+            if ($request->user()->servers->count() > 30) {
                 return $this->toManyServersResponse(30);
             }
-        } else if(!str_contains($stripePlan, 'captain')) {
+        } elseif (! str_contains($stripePlan, 'captain')) {
             return response()->json('We don\'t recognize your subscription, please contact support.', 500);
         }
 
         return $next($request);
     }
 
-    private function toManyServersResponse($maxNumber) {
+    private function toManyServersResponse($maxNumber)
+    {
         return response()->json("You have to many servers for your plan (max $maxNumber), please archive some servers.", 401);
     }
 }
