fa6fdbd6eb44922b51ef31de56a9883bda52ea01
Luke Policinski <Luke@LukePOLO.com>
2017-12-14 15:54:29 -0500

    Apply fixes from StyleCI (#1059)
    [ci skip] [skip ci]

--- app/Jobs/Server/UpdateServerLanguageSetting.php
+++ app/Jobs/Server/UpdateServerLanguageSetting.php
@@ -8,7 +8,6 @@ use Illuminate\Bus\Queueable;
 use App\Models\LanguageSetting;
 use App\Traits\ServerCommandTrait;
 use Illuminate\Queue\SerializesModels;
-use App\Exceptions\ServerCommandFailed;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
