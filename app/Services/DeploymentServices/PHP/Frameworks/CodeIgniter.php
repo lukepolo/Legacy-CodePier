2a88a070cdf3ad5fb3d2ed64115d3e2cbe93f9b4
Luke Policinski <Luke@LukePOLO.com>
2017-12-27 18:01:05 -0500

    initial commit for code igniter

---
+++ app/Services/DeploymentServices/PHP/Frameworks/CodeIgniter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services\DeploymentServices\PHP\Frameworks;
+
+trait CodeIgniter
+{
+
+    /**
+     * @description Creates a symbolic link for the cache and logs folders
+     *
+     * @zerotime-deployment
+     *
+     * @order 150
+     */
+    public function symfonyCreateSymbolicFolders()
+    {
+        $output = [];
+
+        if ($this->zerotimeDeployment) {
+            $output[] = $this->remoteTaskService->run('cp -r '.$this->release.'/application/logs '.$this->siteFolder);
+            $output[] = $this->remoteTaskService->run('rm '.$this->release.'/application/logs -rf');
+            $output[] = $this->remoteTaskService->run('ln -sfn '.$this->siteFolder.'/application/logs '.$this->release);
+
+            $output[] = $this->remoteTaskService->run('cp -r '.$this->release.'/application/logs '.$this->siteFolder);
+            $output[] = $this->remoteTaskService->run('rm '.$this->release.'/application/logs -rf');
+            $output[] = $this->remoteTaskService->run('ln -sfn '.$this->siteFolder.'/application/logs '.$this->release);
+        }
+
+        return $output;
+    }
+
+}
