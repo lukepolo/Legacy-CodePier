c50fbffeee9ba9ad3fbb1bfc058116678588edf4
Luke Policinski <Luke@LukePOLO.com>
2017-12-30 00:14:38 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/User/UserConfirmController.php
+++ app/Http/Controllers/User/UserConfirmController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\User;
 use App\Models\User\User;
 use App\Mail\ConfirmWelcome;
 use Vinkla\Hashids\Facades\Hashids;
-use Illuminate\Support\Facades\Mail;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
 
 class UserConfirmController extends Controller
 {
@@ -15,17 +15,19 @@ class UserConfirmController extends Controller
      * @param $code
      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function update($code) {
+    public function update($code)
+    {
         $userId = Hashids::connection('users')->decode($code);
-        if(empty($userId)) {
+        if (empty($userId)) {
             return redirect('/')->withErrors('Sorry, this confirmation is invalid');
         }
         $user = User::findOrFail($userId[0]);
 
-        if(!$user->confirmed) {
+        if (! $user->confirmed) {
             $user->update([
-                'confirmed' => true
+                'confirmed' => true,
             ]);
+
             return redirect('/')->withSuccess('You have confirmed your email');
         }
 
@@ -35,13 +37,12 @@ class UserConfirmController extends Controller
     /**
      * @return \Illuminate\Http\JsonResponse
      */
-    public function store() {
-
+    public function store()
+    {
         $user = \Auth::user();
 
         Mail::to($user)->send(new ConfirmWelcome($user));
 
         return response()->json('OK');
     }
-
 }
