a2ff6f6ce31228c3e6455c8bc26cc67a260a4e16
Luke Policinski <Luke@LukePOLO.com>
2018-01-09 20:04:38 -0500

    Subscriptions are difficult

--- resources/assets/js/components/Back.vue
+++ resources/assets/js/components/Back.vue
@@ -3,3 +3,13 @@
         <div class="btn pull-left" @click="back"><span class="icon-arrow-left"></span> Back</div>
     </div>
 </template>
+
+<script>
+    export default {
+      methods : {
+        back () {
+          window.history.back();
+        }
+      }
+    }
+</script>
