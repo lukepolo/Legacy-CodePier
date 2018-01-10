14c62ade9cfc90c4e8d510ca9d63cb90888c4daa
Luke Policinski <Luke@LukePOLO.com>
2018-01-09 20:52:30 -0500

    solving unique of hahses for sites hashes

--- resources/assets/js/app/sites/pages/SiteOverview.vue
+++ resources/assets/js/app/sites/pages/SiteOverview.vue
@@ -317,7 +317,9 @@ export default {
       this.$store.dispatch("user_site_deployments/removeDeployHook", {
         site: this.$route.params.site_id,
         hook: this.site.automatic_deployment_id
-      });
+      }).catch(() => {
+        this.site.automatic_deployment_id =  false;
+      })
     },
     fetchData() {
       this.getDns();
@@ -381,7 +383,7 @@ export default {
           location.protocol +
           "//" +
           location.hostname +
-          Vue.action("WebHookController@deploy", { siteHashID: this.site.hash })
+          Vue.action("WebHookController@deploy", { siteHashId : this.site.hash })
         );
       }
     },
