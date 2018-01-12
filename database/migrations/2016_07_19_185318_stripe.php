82437ef140d0c8fa1eb55c56db151dd3f8e9449b
Luke Policinski <Luke@LukePOLO.com>
2018-01-11 20:06:11 -0500

    Only frontend changes left for billing of its active plan

--- database/migrations/2016_07_19_185318_stripe.php
+++ database/migrations/2016_07_19_185318_stripe.php
@@ -24,6 +24,7 @@ class Stripe extends Migration
             $table->string('name');
             $table->string('stripe_id');
             $table->string('stripe_plan');
+            $table->string('active_plan')->nullable();
             $table->unsignedInteger('quantity');
             $table->timestamp('trial_ends_at')->nullable();
             $table->timestamp('ends_at')->nullable();
