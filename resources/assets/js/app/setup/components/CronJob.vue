cb15f6650813e5b277b560a6c259b53d05fcb586
Luke Policinski <Luke@LukePOLO.com>
2018-01-12 11:33:16 -0500

    Most things fixed, cronjob failing

--- resources/assets/js/app/setup/components/CronJob.vue
+++ resources/assets/js/app/setup/components/CronJob.vue
@@ -10,7 +10,11 @@
 
         <td class="table--action">
 
-            <server-selection :server_ids.sync="form.server_ids" :server_types.sync="form.server_types"></server-selection>
+            <server-selection
+                :title="cronJob.job"
+                :server_ids.sync="form.server_ids"
+                :server_types.sync="form.server_types"
+            ></server-selection>
 
             <div class="btn btn-success" v-if="form.diff().length" @click="updateCronJob(cronJob)">update</div>
 
