ae8a60d636d60ae14fde874f2d30984caeee315a
Luke Policinski <Luke@LukePOLO.com>
2018-01-12 19:24:15 -0500

    removal of all jquery on events bar

--- resources/assets/js/components/event-components/SystemEventFilter.vue
+++ resources/assets/js/components/event-components/SystemEventFilter.vue
@@ -168,7 +168,7 @@ export default {
         return "All";
       }
 
-      return site_deployments.concat(commands).join(', ');
+      return site_deployments.concat(commands).join(", ");
     }
   }
 };
