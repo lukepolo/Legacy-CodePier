b41bef836203fbd92733f2cdef913af9f03cdc23
Luke Policinski <Luke@LukePOLO.com>
2018-01-16 20:54:23 -0500

    fix custom provider

--- resources/assets/js/app/sites/pages/SiteRepository.vue
+++ resources/assets/js/app/sites/pages/SiteRepository.vue
@@ -3,12 +3,7 @@
         <form @submit.prevent="updateSite">
 
             <repository-provider-selector :provider.sync="form.user_repository_provider_id">
-
-                <input
-                    type="radio"
-                    v-model="form.user_repository_provider_id"
-                    value="custom"
-                >
+                <input type="radio" checked v-if="form.custom_provider">
 
                 <div class="providers--item providers--item-custom" @click="form.custom_provider = true">
                     <div class="providers--item-header">
@@ -173,8 +168,7 @@ export default {
         this.form.branch = site.branch;
         this.form.repository = site.repository;
         this.form.web_directory = site.web_directory;
-        this.form.user_repository_provider_id =
-          site.user_repository_provider_id;
+        this.form.user_repository_provider_id = site.user_repository_provider_id;
 
         if (this.form.repository && !this.form.user_repository_provider_id) {
           this.form.custom_provider = true;
@@ -201,7 +195,7 @@ export default {
         repository: this.form.repository,
         web_directory: this.form.web_directory,
         custom_provider: this.form.custom_provider,
-        user_repository_provider_id: this.form.user_repository_provider_id
+        user_repository_provider_id: !this.form.custom_provider ? this.form.user_repository_provider_id : null
       });
     },
     getProviderByUrl(providerUrl) {
