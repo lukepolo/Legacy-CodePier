367ff7dac43039b33b151b5f8a83438ff289e7ad
Janice Falotico <jfalotic@gmail.com>
2018-01-16 20:28:51 -0500

    hero section

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -2,35 +2,39 @@
 
 @section('content')
     <section id="section--hero" class="cover">
+        <div class="cover--img">
+            <img src="{{ asset('/assets//img/Boat_Dock.svg') }}">
+        </div>
         <div class="cover--content">
-            <div class="cover--logo">
-                <img src="{{ asset('assets/img/CP_Logo_TX-onWhite.svg') }}">
-            </div>
-            <h1>Your dock for D<span class="small-caps">ev</span>Ops</h1>
+            {{--<div class="cover--logo">--}}
+                {{--<img src="{{ asset('assets/img/CP_Logo_TX-onWhite.svg') }}">--}}
+            {{--</div>--}}
+            <h3>You build it,</h3>
+            <h1>We Deploy It.</h1>
 
             <p class="subheading">Build, Deploy & Mange your applications easily, all on your own servers.</p>
 
             <div class="cover--btns">
-                <a href="/login" class="btn btn-primary btn-large">Join our Beta</a>
+                <a href="/login" class="btn btn-primary btn-large">Start Now!</a>
             </div>
         </div>
     </section>
 
-    <section id="section--video" class="section">
-        <div class="section--content">
-            <div class="video">
-                <div class="video--item">
-                    <div class="video--item-embed">
-                        <div id='deploy_app_url'></div>
-                    </div>
-                </div>
-                <div class="video--controls">
-                    <div data-video="205611637" class="video--controls-item active">Provision</div>
-                    <div data-video="205614363" class="video--controls-item">Deploy</div>
-                </div>
-            </div>
-        </div>
-    </section>
+    {{--<section id="section--video" class="section">--}}
+        {{--<div class="section--content">--}}
+            {{--<div class="video">--}}
+                {{--<div class="video--item">--}}
+                    {{--<div class="video--item-embed">--}}
+                        {{--<div id='deploy_app_url'></div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+                {{--<div class="video--controls">--}}
+                    {{--<div data-video="205611637" class="video--controls-item active">Provision</div>--}}
+                    {{--<div data-video="205614363" class="video--controls-item">Deploy</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</section>--}}
 @endsection
 
 @push('scripts')
