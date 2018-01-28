f4de320b7f22e54edafcdda01f2a744ebf892c8b
Luke Policinski <Luke@LukePOLO.com>
2018-01-28 12:12:56 -0500

    Setting get Ip, need to test on live

--- routes/provision.php
+++ routes/provision.php
@@ -1,6 +1,7 @@
 <?php
 
 Route::get('', 'Server\CustomServerProvisioningController@provision');
+Route::get('ip', 'Server\CustomServerProvisioningController@getIp');
 Route::post('start/{provisioning_key}', 'Server\CustomServerProvisioningController@start');
 Route::get('keys/{provisioning_key}/public', 'Server\CustomServerProvisioningController@returnPublicKey');
 Route::get('keys/{provisioning_key/private}', 'Server\CustomServerProvisioningController@returnPrivateKey');
