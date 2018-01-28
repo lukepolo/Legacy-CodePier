e02793a29ddcb5b2232ab513569fb2eb76b2f391
Luke Policinski <Luke@LukePOLO.com>
2018-01-28 14:53:11 -0500

    moving things

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -109,26 +109,24 @@
         <script type="text/javascript" src="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js"></script>
         @stack('scripts')
 
-        @if(config('app.env') == 'production')
-            <script type="text/javascript">
-              $crisp=[];CRISP_WEBSITE_ID="144f48f7-3604-4483-a8e1-107106d86484";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.im/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
-                  window.CRISP_READY_TRIGGER = function() {
-                    if (!$crisp.is("chat:opened") === true) {
-                      $crisp.push(["do", "chat:hide"])
-                    }
-                  };
-                @if(\Auth::check())
+        <script type="text/javascript">
+          $crisp=[];CRISP_WEBSITE_ID="144f48f7-3604-4483-a8e1-107106d86484";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.im/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+              window.CRISP_READY_TRIGGER = function() {
+                if (!$crisp.is("chat:opened") === true) {
+                  $crisp.push(["do", "chat:hide"])
+                }
+              };
+            @if(\Auth::check())
 
-                    $crisp.push(["set", "user:email", "{{ auth()->user()->email }}"]);
-                    $crisp.push(["set", "user:nickname", "({{ auth()->user()->id }} ) {{ auth()->user()->name }} "]);
-                @endif
-              $(document).on("click", "#getHelp", function(e) {
-                e.preventDefault();
-                $crisp.push(["do", "chat:show"])
-                $crisp.push(["do", "chat:open"])
-              });
-            </script>
-        @endif
+                $crisp.push(["set", "user:email", "{{ auth()->user()->email }}"]);
+                $crisp.push(["set", "user:nickname", "({{ auth()->user()->id }} ) {{ auth()->user()->name }} "]);
+            @endif
+          $(document).on("click", "#getHelp", function(e) {
+            e.preventDefault();
+            $crisp.push(["do", "chat:open"])
+            $crisp.push(["do", "chat:show"])
+          });
+        </script>
 </body>
 </html>
 
