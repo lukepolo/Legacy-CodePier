e0fdd08de10fb7552e089a387ceeae672ae09cb6
Luke Policinski <Luke@LukePOLO.com>
2018-01-30 18:35:33 -0500

    repsoitory validation

--- app/Http/Requests/Site/SiteRepositoryRequest.php
+++ app/Http/Requests/Site/SiteRepositoryRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests\Site;
 
+use App\Providers\RepositoriesServiceProvider;
+use App\Rules\Repository;
 use Illuminate\Foundation\Http\FormRequest;
 
 class SiteRepositoryRequest extends FormRequest
@@ -27,7 +29,7 @@ class SiteRepositoryRequest extends FormRequest
             'type' => 'required|string',
             'branch' => 'required|string',
             'framework' => 'nullable|string',
-            'repository'=> 'string',
+            'repository'=> [new Repository],
             'web_directory' => 'nullable|string',
             'user_repository_provider_id' => 'required_unless:custom_provider,true|nullable|integer',
         ];
