643231fb2b76bf87257aeee5368c2973daedf80e
Luke Policinski <Luke@LukePOLO.com>
2018-01-30 18:23:14 -0500

    removal of wtf

--- app/Events/Site/DeploymentStepStarted.php
+++ app/Events/Site/DeploymentStepStarted.php
@@ -63,8 +63,6 @@ class DeploymentStepStarted implements ShouldBroadcastNow
      */
     public function broadcastWith()
     {
-        \Log::info('wtf');
-
         return [
             'site_deployment' => strip_relations($this->siteDeployment),
             'server_deployment' => strip_relations($this->serverDeployment),
