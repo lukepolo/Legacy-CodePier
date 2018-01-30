2cdc86a2583868a80eab965e97313d7ba8c2b9a1
Luke Policinski <Luke@LukePOLO.com>
2018-01-29 19:30:39 -0500

    Apply fixes from StyleCI (#1181)
    [ci skip] [skip ci]

--- app/Models/Server/Server.php
+++ app/Models/Server/Server.php
@@ -71,14 +71,15 @@ class Server extends Model
         'database_password',
     ];
 
-    public function getServerFeaturesAttribute() {
+    public function getServerFeaturesAttribute()
+    {
         $serverFeatures = json_decode($this->attributes['server_features'], true);
 
-        $serverFeatures['NodeService'] = collect($serverFeatures['NodeService'])->sortBy(function($options, $feature) {
+        $serverFeatures['NodeService'] = collect($serverFeatures['NodeService'])->sortBy(function ($options, $feature) {
             return $feature === 'NodeJs' ? 0 : 1;
         })->all();
 
-        $serverFeatures['Languages\PHP\PHP'] = collect($serverFeatures['Languages\PHP\PHP'])->sortBy(function($options, $feature) {
+        $serverFeatures['Languages\PHP\PHP'] = collect($serverFeatures['Languages\PHP\PHP'])->sortBy(function ($options, $feature) {
             return $feature === 'PHP' ? 0 : 1;
         })->all();
 
