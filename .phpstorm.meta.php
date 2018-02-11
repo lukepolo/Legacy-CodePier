4f7679e7be142faa80d7592b84f45b18aad220b3
Luke Policinski <luke.policinski@egov.com>
2018-02-11 15:51:52 -0500

    more removals an updates

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-02-11 20:49:14.
+    * Generated on 2018-02-11 20:51:34.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
