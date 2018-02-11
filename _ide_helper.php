9658b709ce702c424abebce5d621b331c7db07ad
Luke Policinski <luke.policinski@egov.com>
2018-02-11 15:51:52 -0500

    more removals an updates

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.3 on 2018-02-11 20:49:13.
+ * Generated for Laravel 5.6.3 on 2018-02-11 20:51:34.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
