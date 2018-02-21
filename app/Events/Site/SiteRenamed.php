0082ca047be7785cec841fffd7c68be2a01ea20c
Luke Policinski <Luke@LukePOLO.com>
2018-02-21 16:41:37 -0500

    fixing symbolic links when renaming

--- app/Events/Site/SiteRenamed.php
+++ app/Events/Site/SiteRenamed.php
@@ -2,6 +2,8 @@
 
 namespace App\Events\Site;
 
+use App\Jobs\Server\UpdateServerFile;
+use App\Jobs\Site\DeploySite;
 use App\Models\Site\Site;
 use App\Traits\ModelCommandTrait;
 use App\Jobs\Site\RenameSiteDomain;
@@ -31,10 +33,18 @@ class SiteRenamed
             $siteCommand = $this->makeCommand($site, $site, 'Renaming site '.$oldDomain.' to '.$newDomain);
 
             foreach ($availableServers as $server) {
-                dispatch(
-                    (new RenameSiteDomain($server, $site, $newDomain, $oldDomain, $siteCommand))
-                        ->onQueue(config('queue.channels.server_commands'))
-                );
+
+                foreach($site->files->filter(function($value) {
+                    return $value->framework_file || $value->custom;
+                }) as $siteFile) {
+                    $chainedCommands[] = (new UpdateServerFile($server, $siteFile, $siteCommand))->onQueue(config('queue.channels.server_commands'));
+                }
+
+                $chainedCommands[] = (new DeploySite($site))->onQueue(config('queue.channels.site_deployments'));
+
+                RenameSiteDomain::withChain($chainedCommands)
+                    ->dispatch($server, $site, $newDomain, $oldDomain, $siteCommand)
+                    ->onQueue(config('queue.channels.server_commands'));
             }
         }
     }
