5dc1cff3575c55b21f4b610cca182df655554eb9
Janice Falotico <jfalotic@gmail.com>
2018-02-25 14:39:58 -0500

    language update

--- resources/views/all-features.blade.php
+++ resources/views/all-features.blade.php
@@ -22,7 +22,7 @@
                     <small>(Supports Node / Javascript Deployments)</small>
                 </h3>
                 <ul>
-                    <li>SPA (single page applications)</li>
+                    <li>SPA <small>(single page applications)</small></li>
                 </ul>
                 <h3>Ruby - COMING SOON!</h3>
                 <br>
@@ -34,10 +34,10 @@
             <h2>Account Features</h2>
             <ul>
                 <li>Two-Factor Authentication</li>
-                <li>Customizable Notification Settings for email and slack</li>
+                <li>Customizable Notification Settings for Email and Slack</li>
                 <li>Global SSH Keys</li>
-                <li>Teams - Coming Soon!</li>
-                <li>API - Coming Soon!</li>
+                <li>Teams <em>- COMING SOON!</em></li>
+                <li>API <em>- COMING SOON!</em></li>
             </ul>
 
 
@@ -53,7 +53,7 @@
                 <li>Github</li>
                 <li>GitLab</li>
                 <li>BitBucket</li>
-                <li>Custom - When using the site's SSH Key Provided</li>
+                <li>Custom <small>- When using the site's SSH key provided</small></li>
             </ul>
 
             <h2>Server Providers</h2>
@@ -61,27 +61,27 @@
                 <li>Digital Ocean</li>
                 <li>Linode</li>
                 <li>Vultr</li>
-                <li>Custom - Any Ubuntu 16.04 Based System</li>
+                <li>Custom <small>- Any Ubuntu 16.04 based system</small></li>
             </ul>
 
             <h2>Site Features</h2>
             <ul>
                 <li>Site Priority Design</li>
-                <li>Database Backups - COMING SOON!</li>
-                <li>Lifelines - URL's to be triggered to see the health of services</li>
-                <li>Current DNS IP of A record</li>
-                <li>Custom site notifications for slack</li>
+                <li>Database Backups <em>- COMING SOON!</em></li>
+                <li>Lifelines <small>- URL's to be triggered to see the health of services</small></li>
+                <li>Current DNS IP of a Record</li>
+                <li>Custom Site Notifications for Slack</li>
                 <li>Wildcard Domains</li>
-                <li>Easy renaming of sites</li>
+                <li>Easy Renaming of Sites</li>
                 <li>Site Files</li>
-                <li>Restart of services : web / databases / workers & daemons</li>
-                <li>Provisioning of different server types : full stack, web, database, worker, load balancer</li>
+                <li>Restart Services <small>- web / databases / workers and daemons</small></li>
+                <li>Provisioning Different Server Types <small>- full stack, web, database, worker and load balancer</small></li>
             </ul>
 
             <h2>Deployments</h2>
             <ul>
                 <li>Auto Deploy</li>
-                <li>1 Click Deploys</li>
+                <li>1 Click Deployment</li>
                 <li>Customizable Deployments</li>
                 <li>Customized Deployment Scripts</li>
                 <li>Rollbacks</li>
@@ -89,43 +89,43 @@
 
             <h2>Server Features</h2>
             <ul>
-                <li>Secured by only allowing logins with SSH key authentication only</li>
-                <li>Server Monitoring w/Notifications for : CPU , Memory, Disk Usage</li>
+                <li>Secured by Bnly Allowing Logins with SSH Key Authentication</li>
+                <li>Server Monitoring with Notifications for CPU, Memory and Disk Usage</li>
             </ul>
 
             <h2>Setup Features for Sites / Servers</h2>
             <ul>
-                <li>Databases and users</li>
+                <li>Databases and Users</li>
                 <li>Site SSH Keys</li>
                 <li>Firewall Rules</li>
                 <li>Free SSL Certificates</li>
-                <li>Free Wildcard Certificates - COMING SOON!</li>
+                <li>Free Wildcard Certificates <em>- COMING SOON!</em></li>
                 <li>Environment Variables</li>
                 <li>Cron Jobs</li>
                 <li>Daemons</li>
                 <li>Workers</li>
                 <li>Server Files</li>
-                <li>Language settings</li>
+                <li>Language Settings</li>
                 <li>Server Features</li>
             </ul>
 
             <h2>Installable Server Features</h2>
             <ul>
-                <h3>Datasbase Features</h3>
+                <h3>Database Features</h3>
                 <ul>
                     <li>MariaDB</li>
                     <li>Memcached</li>
                     <li>MySQL</li>
                     <li>PostgresSQL</li>
                     <li>Redis</li>
-                    <li>SqlLite</li>
+                    <li>SQLite</li>
                     <li>MongoDB</li>
                 </ul>
                 <h3>Firewall Features</h3>
                 <ul>
                     <li>Fail2ban</li>
                 </ul>
-                <h3>Moniotiring Features</h3>
+                <h3>Monitoring Features</h3>
                 <ul>
                     <li>Disk Monitoring Script</li>
                     <li>Server Load Monitoring Script</li>
@@ -160,7 +160,7 @@
                 <h3>PHP Features</h3>
                 <ul>
                     <li>PHP 7.0+</li>
-                    <li>PHPFpm</li>
+                    <li>PHP-FPM</li>
                     <li>Composer</li>
                     <li>Blackfire</li>
                     <br>
@@ -173,18 +173,18 @@
             <h2>Server Distributions</h2>
             <ul>
                 <li>Ubuntu 16.04 LTS</li>
-                <li>Ubuntu 18.04 LTS - COMING SOON!</li>
+                <li>Ubuntu 18.04 LTS <em>- COMING SOON!</em></li>
             </ul>
 
             <h2>Events Bar</h2>
             <ul>
-                <li>All Events are reactive, no refreshing needed!</li>
-                <li>Dig deep into events, see errors and timing of events</li>
-                <li>1440p Monitors get extra features for events bar!</li>
-                <li>Can open events as a new window</li>
+                <li>All events are reactive; no refreshing needed!</li>
+                <li>Dig deep into events, see errors and timing.</li>
+                <li>1440p Monitors allow for events to always be visible in sidebar.</li>
+                <li>Can open events as a new window.</li>
             </ul>
 
-            <h2>Buoys (1 Click Installs)</h2>
+            <h2>Buoys <small>(1 Click Installs)</small></h2>
             <ul>
                 <li>Sentry</li>
                 <li>Gitlab</li>
@@ -193,7 +193,7 @@
 
             <h2>Misc. Features</h2>
             <ul>
-                <li>Bitts (custom shell scripts) - COMING SOON!</li>
+                <li>Bitts <small>(custom shell scripts)</small> <em>- COMING SOON!</em></li>
                 <li>Live Support with a Developer</li>
             </ul>
 
