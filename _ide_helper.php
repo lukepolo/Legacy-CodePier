8c4a97375f9316776103882004266ad7520d5574
Luke Policinski <Luke@LukePOLO.com>
2018-02-25 15:08:59 -0500

    phpcs

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.5 on 2018-02-22 22:44:28.
+ * Generated for Laravel 5.6.5 on 2018-02-25 20:03:15.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
