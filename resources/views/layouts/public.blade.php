e1c647d8d2603ef1dfbb1650419b8e750e25f680
Luke Policinski <luke.policinski@egov.com>
2018-02-25 22:16:48 -0500

    rename to site

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -7,14 +7,14 @@
 
         <!-- Open Graph Protocol Social Networks -->
         <meta property="og:title" content="CodePier">
-        <meta property="og:description" content="You're here to build apps. CodePier is here to help you manage your infrastructure, allow custom provisioning for each application, and eliminate downtime with zero downtime deployments, plus, so much more.">
+        <meta property="og:description" content="You're here to build apps. CodePier is here to help you manage your infrastructure, allow custom provisioning for each site, and eliminate downtime with zero downtime deployments, plus, so much more.">
         <meta property="og:image" content="{{ asset('/assets/img/social_img.png') }}">
         <meta property="og:url" content="{{ url('/') }}/">
         <meta property="og:site_name" content="CodePier">
 
         <!-- Twitter -->
         <meta name="twitter:title" content="CodePier">
-        <meta name="twitter:description" content="You're here to build apps. CodePier is here to help you manage your infrastructure, allow custom provisioning for each application, and eliminate downtime with zero downtime deployments, plus, so much more.">
+        <meta name="twitter:description" content="You're here to build apps. CodePier is here to help you manage your infrastructure, allow custom provisioning for each site, and eliminate downtime with zero downtime deployments, plus, so much more.">
         <meta name="twitter:image" content="{{ asset('/assets/img/social_img.png') }}">
         <meta name="twitter:card" content="summary_large_image">
         <meta name="twitter:image:alt" content="CodePier | You Build It. We Deploy It.">
