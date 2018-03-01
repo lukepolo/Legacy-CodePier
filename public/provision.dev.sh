74dbea426b3865b37cf37adf16afb30fb853f464
Luke Policinski <Luke@LukePOLO.com>
2018-03-01 18:34:09 -0500

    new script to detect os and admin

--- public/provision.dev.sh
+++ public/provision.dev.sh
@@ -1,20 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 clear
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root. Please switch to the root user and run the command again."
+
+CODEPIER_URL=http://3e31d5d0.ngrok.io
+
+NL='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[38;05;151m'
+
+echo "Hey there! We are starting to to setup your server for provisioning."
+
+echo "Checking Privileges"
+if [ "$(whoami)" != "root" ] ; then
+   echo -e "$RED This script must be run as root. Please switch to the root user and run the command again. $NL"
    exit 1
 fi
-echo "Hey there! Welcome to the CodePier automated custom server provisioning tool!"
-echo "We're in the process of getting things setup, so that we can begin the provisioning process of your new custom server."
-echo "This should only take a moment or two..."
-echo "--"
+echo -e "$GREEN OK $NL"
+
+echo "Checking System Compatibility"
+
+server_os=`curl -s $CODEPIER_URL/os/$1`
+
+os=$(cat /etc/*release | grep ^PRETTY_NAME | tr -d 'PRETTY_NAME="')
+if ! [[ $os =~ $server_os ]]; then
+  echo -e "$RED Required OS - Ubuntu 16.04 $NL"
+  exit 1
+fi
+echo -e "$GREEN OK $NL"
+
 ip=`wget -qO- https://provision.codepier.io/ip ; echo`
 echo "We have detected your external IP as: $ip. Sending it off so that we know how to reach your server..."
-curl -s --data "ip=$ip" provision.codepier.dev/start/$1
-echo "--"
+curl -s --data "ip=$ip" $CODEPIER_URL/start/$1
+echo -e "$GREEN OK $NL"
+
 echo "Installing CodePier SSH key so that we can connect to your server..."
-public_key=`curl -s provision.codepier.dev/keys/$1/public`
-private_key=`curl -s provision.codepier.dev/keys/$1/private`
+
+public_key=`curl -s $CODEPIER_URL/keys/$1/public`
+private_key=`curl -s $CODEPIER_URL/keys/$1/private`
+
 mkdir ~/.ssh -p
 if [ ! -e ~/.ssh/id_rsa ]; then
     echo "$private_key" > ~/.ssh/id_rsa
@@ -33,6 +55,7 @@ if [ ! -e ~/.ssh/authorized_keys ]; then
 else
     echo "$public_key" >> ~/.ssh/authorized_keys
 fi
+echo -e "$GREEN OK $NL"
 echo "--"
-curl -s -X GET provision.codepier.dev/end/$1
-echo "We've completed the preliminary provisioning process! Please head back to CodePier.io, and watch the rest of the progress there! :)"
+curl -s -X GET $CODEPIER_URL/end/$1
+echo -e "$GREEN We've completed the preliminary provisioning process! Please head back to CodePier.io, and watch the rest of the progress there! $NL"
