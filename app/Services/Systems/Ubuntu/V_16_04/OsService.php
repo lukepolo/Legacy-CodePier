2d1ade15be892f458bc398830fd04e06a188d12b
Luke Policinski <Luke@LukePOLO.com>
2018-03-04 19:49:40 -0500

    putting back the delay in just in case we get odd bugs

--- app/Services/Systems/Ubuntu/V_16_04/OsService.php
+++ app/Services/Systems/Ubuntu/V_16_04/OsService.php
@@ -13,9 +13,8 @@ class OsService
     {
         $this->connectToServer();
 
-        $this->remoteTaskService->run('DEBIAN_FRONTEND=noninteractive apt-get update');
-        // Sleep no longer works , this is a hack , will monitor it for provision failures
-        $this->remoteTaskService->run('sudo rm /var/lib/dpkg/lock');
+        // https://github.com/docker/machine/issues/3358 - sleeping for 45 - 30 wasn't enough.........
+        $this->remoteTaskService->run('sleep 45; DEBIAN_FRONTEND=noninteractive apt-get update');
         $this->remoteTaskService->run('DEBIAN_FRONTEND=noninteractive apt-get -y upgrade');
 
         $this->remoteTaskService->run('DEBIAN_FRONTEND=noninteractive apt-get -y install zip unzip libpq-dev software-properties-common apt-transport-https');
