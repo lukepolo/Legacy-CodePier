6b976fbcbfbe04651d362d7022fec37c2bfb71c8
Luke Policinski <Luke@LukePOLO.com>
2018-03-06 20:27:53 -0500

    if its a different pile they can

--- app/Http/Requests/Site/SiteRequest.php
+++ app/Http/Requests/Site/SiteRequest.php
@@ -30,7 +30,8 @@ class SiteRequest extends FormRequest
                 'required',
                 new Domain,
                 Rule::unique('sites', 'name')->where(function ($query) {
-                    return $query->where('user_id', \Auth::user()->id);
+                    return $query->where('user_id', \Auth::user()->id)
+                        ->where('pile_id', $this->get('pile_id'));
                 }),
             ],
             'pile_id' => 'required|integer',
