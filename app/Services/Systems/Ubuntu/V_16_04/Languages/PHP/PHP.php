0d206e0558e8461d1d8b95a4eeba09e47b68cdf2
Luke Policinski <Luke@LukePOLO.com>
2018-03-06 21:32:25 -0500

    fixing plain php installs

--- app/Services/Systems/Ubuntu/V_16_04/Languages/PHP/PHP.php
+++ app/Services/Systems/Ubuntu/V_16_04/Languages/PHP/PHP.php
@@ -168,9 +168,7 @@ class PHP
         fastcgi_pass unix:/var/run/php/php'.$phpVersion.'-fpm.sock;
         include fastcgi_params;
         
-        fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
-        fastcgi_param DOCUMENT_ROOT $realpath_root;
-        internal;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
     }
 ';
         if (! empty($site->framework)) {
