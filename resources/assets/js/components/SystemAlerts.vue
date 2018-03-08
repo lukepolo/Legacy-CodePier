37b492669e10c19021d7e031b939212df040729d
Luke Policinski <Luke@LukePOLO.com>
2018-03-07 19:03:58 -0500

    New connection statuses

--- resources/assets/js/components/SystemAlerts.vue
+++ resources/assets/js/components/SystemAlerts.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-      <div v-if="current_version !== version" class="text-center">
-          <div>
-              Hello, we've got a new version of CodePier ready for you! Please <a href="">refresh now</a>.
-          </div>
-      </div>
-      <div class="banner banner-info" v-if="!user.confirmed">
-          You have not confirmed your email. You will be unable to create servers until confirmed. <a class="btn btn-small" @click="resendConfirmation()">Resend Now</a>
-      </div>
+    <div v-if="!online" class="banner banner-danger">
+        It appears you are having some connection issues ....
+    </div>
+    <div v-if="!socketConnection" class="banner banner-danger">
+        We are unable to connect you with CodePier's servers, you may not receive updates properly ....
+    </div>
+    <div v-if="current_version !== version" class="banner banner-info">
+        <div>
+            Hello, we've got a new version of CodePier ready for you! Please <a href="" class="btn btn-small">refresh now</a>
+        </div>
+    </div>
+    <div class="banner banner-info" v-if="!user.confirmed">
+        You have not confirmed your email. You will be unable to create servers until confirmed. <a class="btn btn-small" @click="resendConfirmation()">Resend Now</a>
+    </div>
   </div>
 </template>
 
@@ -15,12 +21,30 @@
 export default {
   data() {
     return {
+      online: true,
+      socketConnection: true,
       current_version: Laravel.version,
     };
+  },
+  created() {
+    window.addEventListener("offline", () => {
+      Vue.set(this, "online", false);
+    });
+
+    window.addEventListener("online", () => {
+      Vue.set(this, "online", true);
+    });
+
+    this.interval = setInterval(() => {
+      this.checkSocketConnection();
+    }, 1000);
   },
   methods: {
     resendConfirmation() {
       this.$store.dispatch("user/resendConfirmation");
+    },
+    checkSocketConnection() {
+      Vue.set(this, "socketConnection", Echo.connector.socket.connected);
     },
   },
   computed: {
