0c539ef780db61de9580b3d6221444fde55fd518
Luke Policinski <Luke@LukePOLO.com>
2018-03-22 21:07:23 -0400

    backups frontend started

--- resources/assets/js/app/sites/pages/SiteOverview.vue
+++ resources/assets/js/app/sites/pages/SiteOverview.vue
@@ -239,23 +239,20 @@
                 </div>
             </drop-down>
 
-            <template>
-                <div class="grid--item">
-                    <div class="providers--item">
-                        <div class="providers--item-header">
-                            <div class="providers--item-icon">
-                                <span class="icon-database"></span>
-                            </div>
+            <router-link :to="{ name : 'site_backups', params : { site : this.site.id } }" tag="div" class="grid--item">
+                <div class="providers--item">
+                    <div class="providers--item-header">
+                        <div class="providers--item-icon">
+                            <span class="icon-database"></span>
                         </div>
-                        <div class="providers--item-footer">
-                            <div class="providers--item-footer-connect">
-                                <h4>Enable Database Backups</h4>
-                                <small>Coming Soon!</small>
-                            </div>
+                    </div>
+                    <div class="providers--item-footer">
+                        <div class="providers--item-footer-connect">
+                            <h4>Database Backups</h4>
                         </div>
                     </div>
                 </div>
-            </template>
+            </router-link>
         </div>
 
         <div class="grid-2 grid-gap-large">
