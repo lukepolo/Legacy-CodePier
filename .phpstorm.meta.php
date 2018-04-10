c0fd66885d2c285bb088b2443eb221b3e6a6b57c
Luke Policinski <Luke@LukePOLO.com>
2018-04-10 18:58:35 -0400

    updating vendors

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-04-08 12:33:01.
+    * Generated on 2018-04-10 22:58:26.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
