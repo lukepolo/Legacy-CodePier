0b9a802201e608d63180d8837129144114a4e848
Janice Falotico <jfalotic@gmail.com>
2018-04-10 21:04:28 -0400

    initial progress and some scss cleanup

--- resources/assets/js/components/SiteProgress.vue
+++ resources/assets/js/components/SiteProgress.vue
@@ -1,5 +1,47 @@
 <template>
-  <div>
-    Progress stuff here
-  </div>
+<div class="progress">
+    <div class="progress--item">
+        <!-- completed -->
+        <div class="progress--item-number">
+            1
+        </div>
+        <div class="progress--item-name">
+            Create Site
+        </div>
+    </div>
+
+    <div class="progress--item-divider"></div>
+
+    <div class="progress--item">
+        <div class="progress--item-number">
+            2
+        </div>
+        <div class="progress--item-name">
+            Site Setup
+        </div>
+    </div>
+
+    <div class="progress--item-divider"></div>
+
+    <div class="progress--item">
+        <div class="progress--item-number">
+            3
+        </div>
+        <div class="progress--item-name">
+            Provision Infrastructure
+        </div>
+    </div>
+
+    <div class="progress--item-divider"></div>
+
+    <div class="progress--item">
+        <div class="progress--item-number">
+            4
+        </div>
+        <div class="progress--item-name">
+            Deploy
+        </div>
+    </div>
+
+</div>
 </template>
