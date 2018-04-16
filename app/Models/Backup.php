b6cb5f2662a2b205a96d2b48679ea8421b3d93a8
Luke Policinski <Luke@LukePOLO.com>
2018-04-16 19:50:46 -0400

    shows size on ui

--- app/Models/Backup.php
+++ app/Models/Backup.php
@@ -10,6 +10,26 @@ class Backup extends Model
 {
     protected $guarded = ['id'];
 
+    protected $appends = [
+        'size'
+    ];
+
+    /**
+     * Get the administrator flag for the user.
+     *
+     * @return bool
+     */
+    public function getSizeAttribute()
+    {
+        // https://gist.github.com/liunian/9338301#gistcomment-1970620
+        if (! empty($this->attributes['size'])) {
+            $bytes = $this->attributes['size'];
+            $i = floor(log($bytes, 1024));
+            return round($bytes / pow(1024, $i), [0,0,2,2,3][$i]).' '.['B','kB','MB','GB','TB'][$i];
+        }
+    }
+
+
     /*
     |--------------------------------------------------------------------------
     | Relations
