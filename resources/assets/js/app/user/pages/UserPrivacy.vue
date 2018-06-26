3acfdae886e208a772a89b4b759227108c89913e
Janice Falotico <jfalotic@gmail.com>
2018-06-26 19:56:13 -0400

    formatting for privacy page

--- resources/assets/js/app/user/pages/UserPrivacy.vue
+++ resources/assets/js/app/user/pages/UserPrivacy.vue
@@ -1,9 +1,31 @@
 <template>
-    <div>
-        Privacy
-        <button>Download Data</button>
-        <button>Delete my account forever</button>
-        <input type="checkbox"> Stop using my data for Marketing Purposes
-        <input type="checkbox"> Stop Processing my data
+<div>
+    <div class="flyform--group">
+        <label>Allow Data Usage for Marketing Purposes</label>
+        <div class="toggleSwitch">
+            No &nbsp;
+            <div class="toggleSwitch--button toggleSwitch--button-switch right"></div> &nbsp; Yes
+            <!-- toggle class "right" for on/off -->
+        </div>
+        <small>This data is only used to send you emails from CodePier.</small>
     </div>
+
+    <div class="flyform--group">
+        <label>Allow Data Processing</label>
+        <div class="toggleSwitch">
+            No &nbsp;
+            <div class="toggleSwitch--button toggleSwitch--button-switch right"></div> &nbsp; Yes
+            <!-- toggle class "right" for on/off -->
+        </div>
+        <small>Data processing is only used for bug reporing and support chat.</small>
+    </div>
+
+    <div class="flyform--footer">
+        <div class="flyform--footer-btns">
+            <button class="btn--link btn--link-danger">Delete My Account</button>
+            <button class="btn">Download My Data</button>
+        </div>
+    </div>
+
+</div>
 </template>
