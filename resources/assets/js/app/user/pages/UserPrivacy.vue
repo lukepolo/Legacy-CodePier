c5fc40b820f2f365eb191cc04b02bbcea16283c4
Luke Policinski <luke@lukepolo.com>
2018-06-26 19:19:19 -0400

    ALl options for privacy settings up

---
+++ resources/assets/js/app/user/pages/UserPrivacy.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        Privacy
+        <button>Download Data</button>
+        <button>Delete my account forever</button>
+        <input type="checkbox"> Stop using my data for Marketing Purposes
+        <input type="checkbox"> Stop Processing my data
+    </div>
+</template>
