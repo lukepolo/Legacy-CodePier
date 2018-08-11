ea485b231642763019066da8703e02bff397157e
Luke Policinski <luke@lukepolo.com>
2018-08-11 17:39:41 -0400

    Oauth Logging in now works

--- resources/js/routes/middleware/Auth.ts
+++ resources/js/routes/middleware/Auth.ts
@@ -1,24 +1,22 @@
 import ConfigInterface from "varie/lib/config/ConfigInterface";
 import StateServiceInterface from "varie/lib/state/StateServiceInterface";
+import LocalStorage from "@app/services/LocalStorage";
 
 export default function(to, from, next) {
-  // // TODO - we need access to the token in a diff. place earlier
-  // if (
-  //   localStorage &&
-  //   !localStorage.getItem("token") &&
-  //   !$app.make<ConfigInterface>("$config").get("app.hasToken")
-  // ) {
-  //   next({
-  //     name: "login",
-  //   });
-  //   return false;
-  // }
-  // let $store = $app.make<StateServiceInterface>("$store").getStore();
-  //
-  // if (!$store.state.auth.user) {
-  //   $store.dispatch("auth/me").then(() => {
-  //     return true;
-  //   });
-  // }
+  if (
+    !$app.make<LocalStorage>("LocalStorage").get("token")
+  ) {
+    next({
+      name: "login",
+    });
+    return false;
+  }
+  let $store = $app.make<StateServiceInterface>("$store").getStore();
+
+  if (!$store.state.auth.user) {
+    $store.dispatch("auth/me").then(() => {
+      return true;
+    });
+  }
   return true;
 }
