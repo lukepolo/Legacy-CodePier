5dbee2b2b50d97fa0cc0008f1259cca8307ca35a
Luke Policinski <luke@lukepolo.com>
2018-08-11 19:39:57 -0400

    you can now logout, refacotr cookie

--- resources/js/app/services/CookieStorage.ts
+++ resources/js/app/services/CookieStorage.ts
@@ -1,23 +1,71 @@
 import { injectable } from "inversify";
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie/Simple_document.cookie_framework
+
 @injectable()
 export default class CookieStorage {
-  get(item) {
-    var nameEQ = item + "=";
-    var ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == " ") c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  get(name) {
+    if (!name) {
+      return null;
     }
-    return null;
+    return (
+      decodeURIComponent(
+        document.cookie.replace(
+          new RegExp(
+            "(?:(?:^|.*;)\\s*" +
+              encodeURIComponent(name).replace(/[\-\.\+\*]/g, "\\$&") +
+              "\\s*\\=\\s*([^;]*).*$)|^.*$",
+          ),
+          "$1",
+        ),
+      ) || null
+    );
   }
 
-  set(item, value) {
-    document.cookie = item + "=" + (value || "") + "; path=/";
+  set(name, value, days = 1, path = "/", domain?, secure?) {
+    let expires = new Date();
+    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+    if (!name || /^(?:expires|max\-age|path|domain|secure)$/i.test(name)) {
+      return false;
+    }
+    document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(
+      value,
+    )}; expires=${expires.toUTCString()}${domain ? "; domain=" + domain : ""}${
+      path ? "; path=" + path : ""
+    }${secure ? "; secure" : ""}`;
+    return true;
   }
 
-  remove(item) {
-    document.cookie = item + "=; Max-Age=-99999999;";
+  remove(name, path?, domain?) {
+    if (!this.hasItem(name)) {
+      return false;
+    }
+    document.cookie =
+      encodeURIComponent(name) +
+      "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" +
+      (domain ? "; domain=" + domain : "") +
+      (path ? "; path=" + path : "");
+    return true;
+  }
+
+  hasItem(name) {
+    if (!name || /^(?:expires|max\-age|path|domain|secure)$/i.test(name)) {
+      return false;
+    }
+    return new RegExp(
+      "(?:^|;\\s*)" +
+        encodeURIComponent(name).replace(/[\-\.\+\*]/g, "\\$&") +
+        "\\s*\\=",
+    ).test(document.cookie);
+  }
+
+  keys() {
+    let aKeys = document.cookie
+      .replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "")
+      .split(/\s*(?:\=[^;]*)?;\s*/);
+    for (let nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) {
+      aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]);
+    }
+    return aKeys;
   }
 }
