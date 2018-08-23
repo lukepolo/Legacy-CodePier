cf40c82b78563eb53f061cc7da3bba3e3887864d
Luke Policinski <luke@lukepolo.com>
2018-08-22 20:30:56 -0400

    sites now have ports

--- app/Models/Site/Site.php
+++ app/Models/Site/Site.php
@@ -37,6 +37,8 @@ class Site extends Model
 {
     use UsedByTeams, Notifiable, SoftDeletes, ConnectedToUser, Encryptable, HasServers;
 
+    const STARTING_PORT = 8080;
+
     protected $guarded = [
         'id',
         'public_ssh_key',
