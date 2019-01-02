4f1baeb2df2accddae79e58f1928496da6ec3042
Luke Policinski <luke@lukepolo.com>
2019-01-02 18:41:50 -0500

    Ubuntu 18.04

--- app/Services/Systems/Ubuntu/V_18_04/NodeService.php
+++ app/Services/Systems/Ubuntu/V_18_04/NodeService.php
@@ -11,12 +11,12 @@ class NodeService
     /**
      * @description NodeJs is a JavaScript runtime built on Chrome's V8 JavaScript engine. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient. Node.js' package ecosystem, npm, is the largest ecosystem of open source libraries in the world.
      *
-     * @options 6.14.4, 8.12.0, 10.13.0, 11.1.0
+     * @options 6.14.4, 8.12.0, 10.15.0, 11.6.0
      * @multiple false
      *
      * @param string $version
      */
-    public function installNodeJs($version = '10.13.0')
+    public function installNodeJs($version = '10.15.0')
     {
         $this->connectToServer();
 
