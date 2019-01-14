b009619a7148aa9def410420c421b2391b31b440
Luke Policinski <luke@lukepolo.com>
2019-01-13 22:55:16 -0500

    removing old assets

--- resources/assets/js/app/servers/pages/index.js
+++
@@ -1,8 +0,0 @@
-export const Servers = require("./Servers");
-export const ServerInfo = require("./ServerInfo");
-export const ServerForm = require("./ServerForm");
-export const ServerBuoys = require("./ServerBuoys");
-export const ServerFiles = require("./ServerFiles");
-export const ServerSites = require("./ServerSites");
-export const ServerMonitoring = require("./ServerMonitoring");
-export const ServerPrivateIpAddresses = require("./ServerPrivateIpAddresses");
