3176c20caf7449d4993686f5213a17d7525c1d34
Mike Bronner <mike@genealabs.com>
2019-03-26 13:09:52 -0700

    Update Nginx Site Configurations To Be Stored In `sites-available`. (#1321)
    * WIP - initial code updates
    
    * Add info logging
    
    * Fix SSH Connection Check timeout
    
    * Add command to create symlinks, prevent duplication of existing lines
    
    * update ssh timeout to 30secs, fix reference to server variable
    
    * Remove setTimeout entirely, as we don't need a timeout
    
    * Move timeout to before login
    
    * Update WebService.php
    
    * File should be be commited

--- app/Services/Systems/Ubuntu/V_18_04/Languages/Ruby/Ruby.php
+++ app/Services/Systems/Ubuntu/V_18_04/Languages/Ruby/Ruby.php
@@ -75,7 +75,7 @@ class Ruby
         $this->remoteTaskService->run("sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger yakkety main > /etc/apt/sources.list.d/passenger.list'");
         $this->remoteTaskService->run('DEBIAN_FRONTEND=noninteractive apt-get install -y nginx-extras passenger');
 
-        $this->remoteTaskService->findTextAndAppend('/etc/nginx/nginx.conf', 'include /etc/nginx/sites-enabled/*;', '## Passenger');
+        $this->remoteTaskService->findTextAndAppend('/etc/nginx/nginx.conf', 'include /etc/nginx/sites-available/*;', '## Passenger');
         $this->remoteTaskService->findTextAndAppend('/etc/nginx/nginx.conf', '## Passenger', 'include /etc/nginx/passenger.conf;');
         $this->remoteTaskService->appendTextToFile('~/.bashrc', 'source ~/.rvm/scripts/rvm');
 
