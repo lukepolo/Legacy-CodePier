073f3de6f846a53697956342bcff4a7238392b5a
Luke Policinski <luke.policinski@egov.com>
2020-04-20 13:12:16 -0400

    Adding proxy to get around some issues
    adding proxy application
    
    adding to 18.04
    
    Wow what a day
    
    damn fail
    
    fuck off

---
+++ app/Services/Systems/Ubuntu/V_18_04/Languages/HTML/Frameworks/ProxyedApplication.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services\Systems\Ubuntu\V_18_04\Languages\HTML\Frameworks;
+
+use App\Models\Site\Site;
+use App\Services\Systems\ServiceConstructorTrait;
+use App\Services\Systems\Ubuntu\V_18_04\WebService;
+
+class ProxyedApplication
+{
+    use ServiceConstructorTrait;
+
+    public static $files = [
+
+    ];
+
+    public $suggestedFeatures = [
+
+    ];
+
+    public static $cronJobs = [
+
+    ];
+
+    public function getNginxConfig(Site $site)
+    {
+        return '
+    location / {
+        include '.WebService::NGINX_SERVER_FILES.'/'.$site->domain.'/root-location/*;
+    }
+        ';
+    }
+}
